5a45a8b5d921e598e70dbc19b6473e0efde56e1d
Viktor Benei <viktor.benei@gmail.com>
2016-10-27 12:49:04 +0200

    Feature/go mini server (#3)
    * minimal bitrise.yml file server (load & save)
    
    * .json versions of load & save
    
    * gows init and bitrise.yml for the go code
    
    * go deps update

---
+++ vendor/github.com/bitrise-io/go-utils/parseutil/parseutil.go
@@ -0,0 +1,72 @@
+package parseutil
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/bitrise-io/go-utils/pointers"
+)
+
+// ParseBool ...
+func ParseBool(userInputStr string) (bool, error) {
+	if userInputStr == "" {
+		return false, errors.New("No string to parse")
+	}
+	userInputStr = strings.TrimSpace(userInputStr)
+
+	lowercased := strings.ToLower(userInputStr)
+	if lowercased == "yes" || lowercased == "y" {
+		return true, nil
+	}
+	if lowercased == "no" || lowercased == "n" {
+		return false, nil
+	}
+	return strconv.ParseBool(lowercased)
+}
+
+// CastToString ...
+func CastToString(value interface{}) string {
+	casted, ok := value.(string)
+
+	if !ok {
+		castedStr := fmt.Sprintf("%v", value)
+		casted = castedStr
+	}
+
+	return casted
+}
+
+// CastToStringPtr ...
+func CastToStringPtr(value interface{}) *string {
+	castedValue := CastToString(value)
+	return pointers.NewStringPtr(castedValue)
+}
+
+// CastToBool ...
+func CastToBool(value interface{}) (bool, bool) {
+	casted, ok := value.(bool)
+
+	if !ok {
+		castedStr := CastToString(value)
+
+		castedBool, err := ParseBool(castedStr)
+		if err != nil {
+			return false, false
+		}
+
+		casted = castedBool
+	}
+
+	return casted, true
+}
+
+// CastToBoolPtr ...
+func CastToBoolPtr(value interface{}) (*bool, bool) {
+	castedValue, ok := CastToBool(value)
+	if !ok {
+		return nil, false
+	}
+	return pointers.NewBoolPtr(castedValue), true
+}
