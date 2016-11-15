92ad8bc61993215890eb83fb1be53d62cf79122f
erosdome <erosdome@gmail.com>
2016-11-15 13:17:19 +0100

    trigger - create from config - spec added

--- spec/javascripts/TriggerSpec.js
+++ spec/javascripts/TriggerSpec.js
@@ -1,100 +1,179 @@
 describe("Trigger", function() {
 
     var Trigger;
+    var Workflow;
 
     beforeEach(module("BitriseWorkflowEditor"));
-    beforeEach(inject(function(_Trigger_) {
+    beforeEach(inject(function(_Trigger_, _Workflow_) {
         Trigger = _Trigger_;
+        Workflow = _Workflow_;
     }));
+
+    var workflows;
+    beforeEach(function() {
+        workflows = [
+            new Workflow("red-workflow"),
+            new Workflow("green-workflow"),
+            new Workflow("blue-workflow")
+        ];
+    });
 
     describe("validateTriggerConfig", function() {
 
         it("should not throw error", function() {
             expect(function() {
                 Trigger.validateTriggerConfig({
+                    workflow: "red-workflow",
                     push_branch: "red-branch"
-                });
+                }, workflows);
             }).not.toThrow();
 
             expect(function() {
                 Trigger.validateTriggerConfig({
+                    workflow: "red-workflow",
                     pull_request_source_branch: "red-branch",
                     pull_request_target_branch: "blue-branch"
-                });
+                }, workflows);
             }).not.toThrow();
 
             expect(function() {
                 Trigger.validateTriggerConfig({
+                    workflow: "red-workflow",
                     tag: "red-tag"
-                });
+                }, workflows);
             }).not.toThrow();
         });
 
         it("should throw error", function() {
             expect(function() {
-                Trigger.validateTriggerConfig(null);
+                Trigger.validateTriggerConfig(null, workflows);
             }).toThrow();
 
             expect(function() {
-                Trigger.validateTriggerConfig(undefined);
+                Trigger.validateTriggerConfig(undefined, workflows);
             }).toThrow();
 
             expect(function() {
-                Trigger.validateTriggerConfig(true);
+                Trigger.validateTriggerConfig(true, workflows);
             }).toThrow();
 
             expect(function() {
-                Trigger.validateTriggerConfig(1);
+                Trigger.validateTriggerConfig(1, workflows);
             }).toThrow();
 
             expect(function() {
-                Trigger.validateTriggerConfig("invalid");
+                Trigger.validateTriggerConfig("invalid", workflows);
             }).toThrow();
 
             expect(function() {
-                Trigger.validateTriggerConfig([1, 2]);
+                Trigger.validateTriggerConfig([1, 2], workflows);
             }).toThrow();
 
             expect(function() {
-                Trigger.validateTriggerConfig({})
+                Trigger.validateTriggerConfig({}, workflows);
             }).toThrow();
 
             expect(function() {
                 Trigger.validateTriggerConfig({
+                    workflow: "yellow-workflow",
+                    push_branch: "red-branch"
+                }, workflows);
+            }).toThrow();
+
+            expect(function() {
+                Trigger.validateTriggerConfig({
+                    workflow: "red-workflow"
+                }, workflows);
+            }).toThrow();
+
+            expect(function() {
+                Trigger.validateTriggerConfig({
+                    workflow: "red-workflow",
                     unknown_key: "unknown_value"
-                });
+                }, workflows);
             }).toThrow();
 
             expect(function() {
                 Trigger.validateTriggerConfig({
+                    workflow: "red-workflow",
                     push_branch: 1
-                });
+                }, workflows);
             }).toThrow();
 
             expect(function() {
                 Trigger.validateTriggerConfig({
+                    workflow: "red-workflow",
                     tag: true
-                });
+                }, workflows);
             }).toThrow();
 
             expect(function() {
                 Trigger.validateTriggerConfig({
+                    workflow: "red-workflow",
                     pull_request_source_branch: "red-branch",
                     pull_request_target_branch: 2
-                });
+                }, workflows);
             }).toThrow();
 
             expect(function() {
                 Trigger.validateTriggerConfig({
+                    workflow: "red-workflow",
                     pull_request_source_branch: "red-branch"
-                });
+                }, workflows);
             }).toThrow();
 
             expect(function() {
                 Trigger.validateTriggerConfig({
+                    workflow: "red-workflow",
                     pull_request_target_branch: "red-branch"
-                });
+                }, workflows);
             }).toThrow();
+        });
+
+    });
+
+    describe("createFromTriggerConfig", function() {
+
+        it("should return push trigger", function() {
+            var trigger = Trigger.createFromTriggerConfig({
+                workflow: "red-workflow",
+                push_branch: "red-branch"
+            }, workflows);
+
+            expect(trigger).toEqual(jasmine.any(Trigger));
+            expect(trigger.type).toBe("push");
+            expect(trigger.pushBranchPattern).toBe("red-branch");
+            expect(trigger.pullRequestSourceBranchPattern).toBeUndefined();
+            expect(trigger.pullRequestTargetBranchPattern).toBeUndefined();
+            expect(trigger.tagPattern).toBeUndefined();
+            expect(trigger.workflow).toBe(workflows[0]);
+        });
+
+        it("should return pull-request trigger", function() {
+            var trigger = Trigger.createFromTriggerConfig({
+                workflow: "red-workflow",
+                pull_request_source_branch: "red-branch",
+                pull_request_target_branch: "green-branch"
+            }, workflows);
+
+            expect(trigger.type).toBe("pull-request");
+            expect(trigger.pushBranchPattern).toBeUndefined();
+            expect(trigger.pullRequestSourceBranchPattern).toBe("red-branch");
+            expect(trigger.pullRequestTargetBranchPattern).toBe("green-branch");
+            expect(trigger.tagPattern).toBeUndefined();
+        });
+
+        it("should return tag trigger", function() {
+            var trigger = Trigger.createFromTriggerConfig({
+                workflow: "red-workflow",
+                tag: "red-tag"
+            }, workflows);
+
+            expect(trigger.type).toBe("tag");
+            expect(trigger.pushBranchPattern).toBeUndefined();
+            expect(trigger.pullRequestSourceBranchPattern).toBeUndefined();
+            expect(trigger.pullRequestTargetBranchPattern).toBeUndefined();
+            expect(trigger.tagPattern).toBe("red-tag");
         });
 
     });
