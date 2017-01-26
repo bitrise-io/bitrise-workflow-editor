f1232e29c08b7e2f94f8addb8f0b079d5edf6590
Krisztian Godrei <godreikrisztian@gmail.com>
2017-01-26 17:48:33 +0100

    steplib spec endpoint

---
+++ apiserver/service/steplib_spec.go
@@ -0,0 +1,49 @@
+package service
+
+import (
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/log"
+	stepmanModels "github.com/bitrise-io/stepman/models"
+)
+
+// GetSpecHandler ...
+func GetSpecHandler(w http.ResponseWriter, r *http.Request) {
+	// Load steplib spec
+	cmd := command.New("bitrise", "stepman", "collections", "--format", "json")
+	out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+	if err != nil {
+		log.Errorf("Failed to get steplib spec, error: %s", err)
+		RespondWithJSONBadRequestErrorMessage(w, "Failed to get steplib spec, error: %s", err)
+		return
+	}
+
+	type OutputModel struct {
+		Data  *([]stepmanModels.SteplibInfoModel) `json:"data,omitempty" yaml:"data,omitempty"`
+		Error string                              `json:"error,omitempty" yaml:"error,omitempty"`
+	}
+
+	var output OutputModel
+	if err := yaml.Unmarshal([]byte(out), &output); err != nil {
+		log.Errorf("Failed to parse the output of stepman (%s), error: %s", out, err)
+		RespondWithJSONBadRequestErrorMessage(w, "Failed to parse the output of stepman (%s), error: %s", out, err)
+		return
+	}
+
+	if output.Error != "" {
+		log.Errorf("Failed to get steplib spec, error: %s", output.Error)
+		RespondWithJSONBadRequestErrorMessage(w, "Failed to get steplib spec, error: %s", output.Error)
+		return
+	}
+
+	if output.Data == nil {
+		log.Errorf("Missing output data, error: %s", output.Error)
+		RespondWithJSONBadRequestErrorMessage(w, "Missing output data, error: %s", output.Error)
+		return
+	}
+
+	RespondWithJSON(w, 200, *output.Data)
+}
---
+++ apiserver/service/steplib_spec_test.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"strings"
+
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetSpecHandler(t *testing.T) {
+	cmd := command.New("bitrise", "stepman", "setup", "--collection", "https://github.com/bitrise-io/bitrise-steplib.git")
+	require.NoError(t, cmd.Run())
+
+	req, err := http.NewRequest("GET", "/api/spec", nil)
+	require.NoError(t, err)
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetSpecHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	require.Equal(t, http.StatusOK, rr.Code, rr.Body.String())
+	require.Equal(t, true, strings.Contains(rr.Body.String(), "{\"uri\":\"https://github.com/bitrise-io/bitrise-steplib.git\",\"spec_path\":\"/Users/godrei/.stepman/step_collections/1485356810/spec/spec.json\"}"))
+}
