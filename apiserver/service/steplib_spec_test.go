70e754eeea9d2ef2487473a69eda79d0495c58e1
Krisztian Godrei <godreikrisztian@gmail.com>
2017-01-27 12:14:12 +0100

    test update

--- apiserver/service/steplib_spec_test.go
+++ apiserver/service/steplib_spec_test.go
@@ -5,14 +5,15 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"regexp"
+	"strings"
 
 	"github.com/bitrise-io/go-utils/command"
 	"github.com/stretchr/testify/require"
 )
 
 func TestGetSpecHandler(t *testing.T) {
-	cmd := command.New("bitrise", "stepman", "setup", "--collection", "https://github.com/bitrise-io/bitrise-steplib.git")
+	defaultSteplib := "https://github.com/bitrise-io/bitrise-steplib.git"
+	cmd := command.New("bitrise", "stepman", "setup", "--collection", defaultSteplib)
 	require.NoError(t, cmd.Run())
 
 	req, err := http.NewRequest("GET", "/api/spec", nil)
@@ -24,9 +25,5 @@ func TestGetSpecHandler(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	require.Equal(t, http.StatusOK, rr.Code, rr.Body.String())
-
-	pattern := `{"uri":"https://github.com/bitrise-io/bitrise-steplib.git","spec_path":"/Users/.*/.stepman/step_collections/.*/spec/spec.json"}`
-	re := regexp.MustCompile(pattern)
-	match := re.FindString(rr.Body.String())
-	require.NotEmpty(t, match, rr.Body.String())
+	require.Equal(t, true, strings.Contains(rr.Body.String(), defaultSteplib))
 }
