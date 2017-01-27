e0732737967114bd03a3274ded95a0bcdc28f0cb
Krisztian Godrei <godreikrisztian@gmail.com>
2017-01-27 11:50:58 +0100

    print output if test fails

--- apiserver/service/steplib_spec_test.go
+++ apiserver/service/steplib_spec_test.go
@@ -24,5 +24,5 @@ func TestGetSpecHandler(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	require.Equal(t, http.StatusOK, rr.Code, rr.Body.String())
-	require.Equal(t, true, strings.Contains(rr.Body.String(), "{\"uri\":\"https://github.com/bitrise-io/bitrise-steplib.git\",\"spec_path\":\"/Users/godrei/.stepman/step_collections/1485356810/spec/spec.json\"}"))
+	require.Equal(t, true, strings.Contains(rr.Body.String(), "{\"uri\":\"https://github.com/bitrise-io/bitrise-steplib.git\",\"spec_path\":\"/Users/godrei/.stepman/step_collections/1485356810/spec/spec.json\"}"), rr.Body.String())
 }
