89268606e80a80e7046a40bd12f56ba35bfdef2d
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-01-30 17:44:21 +0100

    version command and prepare for release (#30)
    * version command and prepare for release
    
    * deps update
    
    * PR fix

--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"log"
-	"os"
-
-	"github.com/bitrise-io/bitrise-workflow-editor/apiserver"
-)
+import "github.com/bitrise-io/bitrise-workflow-editor/cli"
 
 func main() {
-	if err := apiserver.LaunchServer(); err != nil {
-		log.Printf(" [!] API Server failed, error: %s", err)
-		os.Exit(1)
-	}
+	cli.Run()
 }
