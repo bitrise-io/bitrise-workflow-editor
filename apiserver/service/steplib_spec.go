2c9e4ee47253f25c7fe7270c09bc4dfeba66f328
Krisztian Godrei <godreikrisztian@gmail.com>
2017-01-30 15:16:48 +0100

    spec endpoint updates

--- apiserver/service/steplib_spec.go
+++ apiserver/service/steplib_spec.go
@@ -5,7 +5,11 @@ import (
 
 	"gopkg.in/yaml.v2"
 
+	"encoding/json"
+
+	"github.com/bitrise-io/depman/pathutil"
 	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/fileutil"
 	"github.com/bitrise-io/go-utils/log"
 	stepmanModels "github.com/bitrise-io/stepman/models"
 )
@@ -44,5 +48,44 @@ func GetSpecHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	RespondWithJSON(w, 200, *output.Data)
+	type ResponseItemModel struct {
+		URI  string                            `json:"uri,omitempty"`
+		Spec stepmanModels.StepCollectionModel `json:"spec,omitempty"`
+	}
+
+	response := []ResponseItemModel{}
+
+	steplibInfos := *output.Data
+	for _, steplibInfo := range steplibInfos {
+		if exist, err := pathutil.IsPathExists(steplibInfo.SpecPath); err != nil {
+			log.Errorf("Failed to check if spec exists at: %s, error: %s", steplibInfo.SpecPath, err)
+			RespondWithJSONBadRequestErrorMessage(w, "Failed to check if spec exists at: %s, error: %s", steplibInfo.SpecPath, err)
+			return
+		} else if !exist {
+			log.Errorf("Spec not exists at: %s", steplibInfo.SpecPath)
+			RespondWithJSONBadRequestErrorMessage(w, "Spec not exists at: %s", steplibInfo.SpecPath)
+			return
+		}
+
+		specBytes, err := fileutil.ReadBytesFromFile(steplibInfo.SpecPath)
+		if err != nil {
+			log.Errorf("Failed to read spec at: %s, error: %s", steplibInfo.SpecPath, err)
+			RespondWithJSONBadRequestErrorMessage(w, "Failed to read spec at: %s, error: %s", steplibInfo.SpecPath, err)
+			return
+		}
+
+		var spec stepmanModels.StepCollectionModel
+		if err := json.Unmarshal(specBytes, &spec); err != nil {
+			log.Errorf("Failed to read serialize spec, error: %s", err)
+			RespondWithJSONBadRequestErrorMessage(w, "Failed to read serialize spec, error: %s", err)
+			return
+		}
+
+		response = append(response, ResponseItemModel{
+			URI:  steplibInfo.URI,
+			Spec: spec,
+		})
+	}
+
+	RespondWithJSON(w, http.StatusOK, response)
 }
