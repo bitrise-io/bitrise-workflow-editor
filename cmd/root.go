7c73a15b4aaf987c5c311b1ced58362882015298
Krisztian Godrei <godreikrisztian@gmail.com>
2017-02-03 14:16:34 +0100

    start api server as default command

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,20 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"os"
+
+	"github.com/bitrise-io/bitrise-workflow-editor/apiserver"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/spf13/cobra"
+)
 
 // RootCmd ...
 var RootCmd = &cobra.Command{
 	Use: "workflow-editor",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := apiserver.LaunchServer(); err != nil {
+			log.Errorf("Failed to start server, error: %s", err)
+			os.Exit(1)
+		}
+	},
 }
