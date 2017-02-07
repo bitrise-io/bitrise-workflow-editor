2c3c82a812fc31c880e24f3ba3595aefc071fe30
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-02-07 13:12:18 +0100

    Release automation (#47)
    * release automation
    
    * revert previous release
    
    * may test the Linux binary

--- README.md
+++ README.md
@@ -8,3 +8,17 @@
 ```
 bitrise run go-install
 ```
+
+## New version release
+
+- Ensure clean git
+- Bump `RELEASE_VERSION` in `bitrise.yml`
+- Push changes
+- Call `bitrise run create-release`
+- Update & push changelog in `CHANGELOG.md`
+- On GitHub, create new release with title and tag `X.X.X`, description from changelog, starting with *Release Notes*, up to but not including *Release Commits*
+- Wait for the `create-release` workflow to finish successfully on Bitrise
+- Download the generated artifacts from Bitrise
+- In terminal, run `chmod +x <path to generated binary> && <path to generated binary> version --full`
+- After finish and double-checking build number and commit hash on Bitrise, run <path to generated binary> to check if binary is working
+- On GitHub, attach the binaries, then select *Publish release*
--- _scripts/set_version.sh
+++ _scripts/set_version.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 set -x
 
+version_number=$next_version
+if [[ "$version_number" == "" ]] ; then
+  echo " [!] version_number not provided"
+  exit 1
+fi
+
 version_file_path="$1"
 if [ ! -f "$version_file_path" ] ; then
   echo " [!] version_file_path not provided, or file doesn't exist at path: $version_file_path"
   exit 1
 fi
-versionNumber=$next_version
-if [[ "$versionNumber" == "" ]] ; then
-  echo " [!] versionNumber not provided"
+
+plugin_definition_file_path="$2"
+if [ ! -f "$plugin_definition_file_path" ] ; then
+  echo " [!] plugin_definition_file_path not provided, or file doesn't exist at path: $plugin_definition_file_path"
   exit 1
 fi
 
-cat >"${version_file_path}" <<EOL
-package version
 
-// VERSION ...
-const VERSION = "${versionNumber}"
-EOL
+BITRISE_WORKFLOW_EDITOR_VERSION="$version_number" \
+  BITRISE_WORKFLOW_EDITOR_VERSION_FILE="$version_file_path" \
+  BITRISE_WORKFLOW_EDITOR_PLUGIN_DEFINITION_FILE="$plugin_definition_file_path" \
+  go run _scripts/set_version.go
