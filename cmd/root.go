8c5965049106f250271f6b014d257a35bbe31c6d
Krisztian Godrei <godreikrisztian@gmail.com>
2017-07-04 17:56:24 +0200

    Revert "check if bitrise.yml exist in the current directory"
    This reverts commit 980b289e20e4daabac4792cdfe5b2ce56e49ef63.

--- cmd/root.go
+++ cmd/root.go
@@ -2,37 +2,19 @@ package cmd
 
 import (
 	"os"
-	"path/filepath"
 
 	"github.com/bitrise-io/bitrise-workflow-editor/apiserver"
 	"github.com/bitrise-io/go-utils/log"
-	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/spf13/cobra"
 )
-
-func failf(format string, v ...interface{}) {
-	log.Errorf(format, v...)
-	os.Exit(1)
-}
 
 // RootCmd ...
 var RootCmd = &cobra.Command{
 	Use: "workflow-editor",
 	Run: func(cmd *cobra.Command, args []string) {
-		currentDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-		if err != nil {
-			failf("Failed to get current dir, error: %s", err)
-		}
-
-		bitriseConfigPth := filepath.Join(currentDir, "bitrise.yml")
-		if exist, err := pathutil.IsPathExists(bitriseConfigPth); err != nil {
-			failf("Failed to check is bitrise.yml exist, error: %s", err)
-		} else if !exist {
-			failf("No bitrise config (bitrise.yml) found in the current directory")
-		}
-
 		if err := apiserver.LaunchServer(); err != nil {
-			failf("Failed to start server, error: %s", err)
+			log.Errorf("Failed to start server, error: %s", err)
+			os.Exit(1)
 		}
 	},
 }
