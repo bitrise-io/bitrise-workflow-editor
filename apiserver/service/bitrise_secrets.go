3187f817ad67c74cef0a4166efcbe5af07947054
erosdome <jozsef.eros@bitrise.io>
2018-01-25 15:32:38 +0100

    Use both namespaced and non-namespaced handling for secrets' "is expose" flag - temporarily (#241)
    * Create factory for Meta, with getter setter method for specified key in bundle
    
    * env meta field json marshal fix
    
    * Update Variable factory to use Meta factory
    
    * Use both namespaced and non-namespaced handling for “is expose” flag - temporary solution
    
    * Change argument order of Meta value getter-setter
    
    * Move meta bundle IDs to constants YML
    
    * Update with requested change

--- apiserver/service/bitrise_secrets.go
+++ apiserver/service/bitrise_secrets.go
@@ -14,6 +14,28 @@ import (
 	"github.com/bitrise-io/go-utils/pathutil"
 )
 
+func ymlToJSONKeyTypeConversion(i interface{}) interface{} {
+	switch x := i.(type) {
+	case map[string]interface{}:
+		m2 := map[string]interface{}{}
+		for k, v := range x {
+			m2[k] = ymlToJSONKeyTypeConversion(v)
+		}
+		return m2
+	case map[interface{}]interface{}:
+		m2 := map[string]interface{}{}
+		for k, v := range x {
+			m2[k.(string)] = ymlToJSONKeyTypeConversion(v)
+		}
+		return m2
+	case []interface{}:
+		for i, v := range x {
+			x[i] = ymlToJSONKeyTypeConversion(v)
+		}
+	}
+	return i
+}
+
 // GetSecretsAsJSONHandler ...
 func GetSecretsAsJSONHandler(w http.ResponseWriter, r *http.Request) {
 	secretsYMLPth := config.SecretsYMLPath.Get()
@@ -40,20 +62,42 @@ func GetSecretsAsJSONHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var respObj envmanModels.EnvsSerializeModel
-	if err := yaml.Unmarshal(contBytes, &respObj); err != nil {
+	var envsSerializeModel envmanModels.EnvsSerializeModel
+	if err := yaml.Unmarshal(contBytes, &envsSerializeModel); err != nil {
 		log.Errorf("Failed to parse the content of .bitrise.secrets.yml file (invalid YML), error: %s", err)
 		RespondWithJSONBadRequestErrorMessage(w, "Failed to parse the content of .bitrise.secrets.yml file (invalid YML), error: %s", err)
 		return
 	}
 
-	if err := respObj.Normalize(); err != nil {
+	if err := envsSerializeModel.Normalize(); err != nil {
 		log.Errorf("Failed to normalize the content of .bitrise.secrets.yml file (invalid YML), error: %s", err)
 		RespondWithJSONBadRequestErrorMessage(w, "Failed to normalize the content of .bitrise.secrets.yml file (invalid YML), error: %s", err)
 		return
 	}
 
-	RespondWithJSON(w, 200, respObj)
+	for i, env := range envsSerializeModel.Envs {
+		opts, err := env.GetOptions()
+		if err != nil {
+			log.Errorf("Failed to get options of env: %v, error: %s", env, err)
+			RespondWithJSONBadRequestErrorMessage(w, "Failed to get options of env: %v, error: %s", env, err)
+			return
+		}
+		if len(opts.Meta) > 0 {
+			meta := ymlToJSONKeyTypeConversion(opts.Meta)
+			casted, ok := meta.(map[string]interface{})
+			if ok {
+				opts.Meta = casted
+			} else {
+				log.Errorf("Failed to cast env mate property: %v, error: %s", opts.Meta, err)
+				RespondWithJSONBadRequestErrorMessage(w, "Failed to cast env mate property: %v, error: %s", opts.Meta, err)
+				return
+			}
+		}
+		env[envmanModels.OptionsKey] = opts
+		envsSerializeModel.Envs[i] = env
+	}
+
+	RespondWithJSON(w, 200, envsSerializeModel)
 }
 
 // PostSecretsYMLFromJSONHandler ...
