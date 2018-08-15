22ca935bb626c8cf8929b960e027180d75626c9d
David Breuer <dbreuer83@gmail.com>
2018-08-15 11:24:04 +0200

    Move monaco editor source into Javascripts folder and include in vendor.js

--- source/javascripts/vendor.js
+++ source/javascripts/vendor.js
@@ -3,3 +3,6 @@
 //= require _markdown
 //= require _esprima.js
 //= require _js-yaml.min.js
+//= require monaco-editor/min/vs/loader.js
+//= require monaco-editor/min/vs/editor/editor.main.nls.js
+//= require monaco-editor/min/vs/editor/editor.main.js
