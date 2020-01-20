f8adef8e9239a5cc90526f93b7b0d0dc693cefdd
Adam Pelle <adam.pelle@bitrise.io>
2020-01-20 11:43:43 +0100

    Added UI logic for version selector versions

--- spec/javascripts/filters/versionFilter.spec.js
+++ spec/javascripts/filters/versionFilter.spec.js
@@ -14,34 +14,34 @@ describe("VersionFilter", () => {
         });
 
         mockStep = {
-            version: '1.2.3',
+            version: "1.2.3",
             isLibraryStep: () => true,
         };
     });
 
-    beforeEach(inject((_wildcardVersionsFilter_) => {
-        versionFilter = _wildcardVersionsFilter_;
+    beforeEach(inject((_stepVersionsFilter_) => {
+        versionFilter = _stepVersionsFilter_;
     }));
 
-    it('should use stepSource service to calculate wildcard versions', () => {
+    it("should use stepSource service to calculate wildcard versions", () => {
         versionFilter(mockStep);
         expect(mockStepSourceService.versionsOfStep).toHaveBeenCalledWith(mockStep);
     });
 
-    it('should include latest version and own version as well in the list if the step is a library step', () => {
-        mockStepSourceService.versionsOfStep.and.returnValue(['1.2.x']);
+    it("should include latest version and own version as well in the list if the step is a library step", () => {
+        mockStepSourceService.versionsOfStep.and.returnValue(["1.2.x"]);
 
         var versions = versionFilter(mockStep);
 
-        expect(versions).toEqual([null, '1.2.3', '1.2.x']);
+        expect(versions).toEqual([null, "1.2.3", "1.2.x"]);
     });
 
-    it('should not include latest version in the list if the step is not library step', () => {
+    it("should not include latest version in the list if the step is not library step", () => {
         mockStep.isLibraryStep = () => false;
-        mockStepSourceService.versionsOfStep.and.returnValue(['1.2.x']);
+        mockStepSourceService.versionsOfStep.and.returnValue(["1.2.x"]);
 
         var versions = versionFilter(mockStep);
 
-        expect(versions).toEqual(['1.2.3', '1.2.x']);
+        expect(versions).toEqual(["1.2.3", "1.2.x"]);
     });
 });
