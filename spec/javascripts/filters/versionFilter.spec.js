f1b53d252d1a8852c3909f2005bd3ec21eed9080
Adam Pelle <adam.pelle@bitrise.io>
2020-01-20 12:26:10 +0100

    Disabling non wildcard versions through version filter

--- spec/javascripts/filters/versionFilter.spec.js
+++ spec/javascripts/filters/versionFilter.spec.js
@@ -1,47 +1,76 @@
-describe("VersionFilter", () => {
-    var versionFilter;
+describe("VersionsFilter", () => {
+    var stepVersionsFilter;
+    var versionLabelFilter;
     var mockStepSourceService;
-    var mockStep;
 
     beforeEach(() => {
         mockStepSourceService = {
-			versionsOfStep: jasmine.createSpy("extractWildcardVersions"),
-		};
-
-		module("BitriseWorkflowEditor");
-		module(($provide) => {
-			$provide.value("stepSourceService", mockStepSourceService);
-        });
-
-        mockStep = {
-            version: "1.2.3",
-            isLibraryStep: () => true,
+            versionsOfStep: jasmine.createSpy("extractWildcardVersions"),
         };
+
+        module("BitriseWorkflowEditor");
+        module(($provide) => {
+            $provide.value("stepSourceService", mockStepSourceService);
+        });
     });
 
-    beforeEach(inject((_stepVersionsFilter_) => {
-        versionFilter = _stepVersionsFilter_;
+    beforeEach(inject((_stepVersionsFilter_, _versionLabelFilter_) => {
+        stepVersionsFilter = _stepVersionsFilter_;
+        versionLabelFilter = _versionLabelFilter_;
     }));
 
-    it("should use stepSource service to calculate wildcard versions", () => {
-        versionFilter(mockStep);
-        expect(mockStepSourceService.versionsOfStep).toHaveBeenCalledWith(mockStep);
+    describe("stepVersions filter", () => {
+        it("should not duplicate latest version even if the step version is latest", () => {
+            mockStepSourceService.versionsOfStep.and.returnValue(["1.2.x"]);
+
+            var versions = stepVersionsFilter({
+                version: null,
+                isLibraryStep: () => true,
+            });
+
+            expect(versions).toEqual([
+                { label: null, disabled: true },
+                { label: "1.2.x", disabled: false },
+            ]);
+        });
+
+        it("should include latest version and own version as well in the list if the step is a library step", () => {
+            mockStepSourceService.versionsOfStep.and.returnValue(["1.2.x"]);
+
+            var versions = stepVersionsFilter({
+                version: "1.2.3",
+                isLibraryStep: () => true,
+            });
+
+            expect(versions).toEqual([
+                { label: null, disabled: true },
+                { label: "1.2.3", disabled: true },
+                { label: "1.2.x", disabled: false },
+            ]);
+        });
+
+        it("should not include latest version in the list if the step is not library step", () => {
+            mockStepSourceService.versionsOfStep.and.returnValue(["1.2.x"]);
+
+            var versions = stepVersionsFilter({
+                version: "1.2.3",
+                isLibraryStep: () => false,
+            });
+
+            expect(versions).toEqual([
+                { label: "1.2.3", disabled: true },
+                { label: "1.2.x", disabled: false },
+            ]);
+        });
     });
 
-    it("should include latest version and own version as well in the list if the step is a library step", () => {
-        mockStepSourceService.versionsOfStep.and.returnValue(["1.2.x"]);
+    describe("versionLabel filter", () => {
+        it("should use the specified version if given", () => {
+            expect(versionLabelFilter({ label: "1.2.x" })).toEqual("1.2.x");
+        });
 
-        var versions = versionFilter(mockStep);
-
-        expect(versions).toEqual([null, "1.2.3", "1.2.x"]);
-    });
-
-    it("should not include latest version in the list if the step is not library step", () => {
-        mockStep.isLibraryStep = () => false;
-        mockStepSourceService.versionsOfStep.and.returnValue(["1.2.x"]);
-
-        var versions = versionFilter(mockStep);
-
-        expect(versions).toEqual(["1.2.3", "1.2.x"]);
+        it("should use a text indicating latest update if version is not given", () => {
+            expect(versionLabelFilter({ label: null })).toContain("latest");
+        });
     });
 });
