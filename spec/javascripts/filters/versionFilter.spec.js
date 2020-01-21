44fb12cd387966cb22fad2c5ad02732a5a5d4ce3
Adam Pelle <adam.pelle@bitrise.io>
2020-01-21 13:04:38 +0100

    added tests for filter

--- spec/javascripts/filters/versionFilter.spec.js
+++ spec/javascripts/filters/versionFilter.spec.js
@@ -1,28 +1,38 @@
 describe("VersionFilters", () => {
-    let stepVersions;
+    let stepVersionsFilter;
+    let versionLabelFilter;
+    let isVersionDisabledFilter;
     let mockStepSourceService;
+    let mockSemverService;
     let mockStep;
 
     beforeEach(() => {
         mockStepSourceService = {
-			versionsOfStep: jasmine.createSpy("extractWildcardVersions"),
-		};
+            versionsOfStep: jasmine.createSpy("extractWildcardVersions"),
+        };
 
-		module("BitriseWorkflowEditor");
-		module(($provide) => {
-			$provide.value("stepSourceService", mockStepSourceService);
+        mockSemverService = {
+            isVersionWildcard: jasmine.createSpy("isVersionWildcard"),
+        };
+
+        module("BitriseWorkflowEditor");
+        module(($provide) => {
+            $provide.value("stepSourceService", mockStepSourceService);
+            $provide.value("semverService", mockSemverService);
         });
 
         mockStep = { isLibraryStep: () => true };
     });
 
-    beforeEach(inject((_stepVersionsFilter_) => {
-        stepVersions = _stepVersionsFilter_;
+    beforeEach(inject((_stepVersionsFilter_, _versionLabelFilter_, _isVersionDisabledFilter_) => {
+        stepVersionsFilter = _stepVersionsFilter_;
+        versionLabelFilter = _versionLabelFilter_;
+        isVersionDisabledFilter = _isVersionDisabledFilter_;
     }));
 
     describe("stepVersionsFilter", () => {
         it("should use stepSource service to calculate wildcard versions", () => {
-            stepVersions(mockStep);
+            stepVersionsFilter(mockStep);
             expect(mockStepSourceService.versionsOfStep).toHaveBeenCalledWith(mockStep);
         });
 
@@ -30,9 +40,29 @@ describe("VersionFilters", () => {
             mockStep.isLibraryStep = () => false;
             mockStepSourceService.versionsOfStep.and.returnValue(["1.2.x"]);
 
-            const versions = stepVersions(mockStep);
+            const versions = stepVersionsFilter(mockStep);
 
             expect(versions).toEqual(["1.2.x"]);
+        });
+    });
+
+    describe("versionLabel", () => {
+        it("should display version str if set", () => {
+            expect(versionLabelFilter("test-version")).toEqual("test-version");
+        });
+
+        it("should display latest version if version descriptor not set", () => {
+            expect(versionLabelFilter(null)).toContain("latest");
+        });
+    });
+
+    describe("isVersionDisabled", () => {
+        it("should return disable if version is not wildcard", () => {
+            mockSemverService.isVersionWildcard.and.returnValue(false);
+            expect(isVersionDisabledFilter("test-version-non-wildcard")).toBeTruthy();
+
+            mockSemverService.isVersionWildcard.and.returnValue(true);
+            expect(isVersionDisabledFilter("test-version-wildcard")).toBeFalsy();
         });
     });
 });
