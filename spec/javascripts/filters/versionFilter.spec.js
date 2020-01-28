dc5e1dd257b89fc748cfb4d4760d1ecc23662a68
Adam Pelle <adam.pelle.peter@gmail.com>
2020-01-28 13:17:23 +0100

    Revert "[WEB-3337] disable non wildcard versions in version selector (#416)"
    This reverts commit b14611503335983c8e112365b766901614de720e.

--- spec/javascripts/filters/versionFilter.spec.js
+++ spec/javascripts/filters/versionFilter.spec.js
@@ -1,68 +1,46 @@
 describe("VersionFilters", () => {
-    let stepVersionsFilter;
-    let versionLabelFilter;
-    let isVersionDisabledFilter;
+    let stepVersions;
     let mockStepSourceService;
-    let mockSemverService;
     let mockStep;
 
     beforeEach(() => {
         mockStepSourceService = {
-            versionsOfStep: jasmine.createSpy("extractWildcardVersions"),
-        };
+			versionsOfStep: jasmine.createSpy("extractWildcardVersions"),
+		};
 
-        mockSemverService = {
-            isVersionWildcard: jasmine.createSpy("isVersionWildcard"),
-        };
-
-        module("BitriseWorkflowEditor");
-        module(($provide) => {
-            $provide.value("stepSourceService", mockStepSourceService);
-            $provide.value("semverService", mockSemverService);
+		module("BitriseWorkflowEditor");
+		module(($provide) => {
+			$provide.value("stepSourceService", mockStepSourceService);
         });
 
         mockStep = { isLibraryStep: () => true };
     });
 
-    beforeEach(inject((_stepVersionsFilter_, _versionLabelFilter_, _isVersionDisabledFilter_) => {
-        stepVersionsFilter = _stepVersionsFilter_;
-        versionLabelFilter = _versionLabelFilter_;
-        isVersionDisabledFilter = _isVersionDisabledFilter_;
+    beforeEach(inject((_stepVersionsFilter_) => {
+        stepVersions = _stepVersionsFilter_;
     }));
 
     describe("stepVersionsFilter", () => {
         it("should use stepSource service to calculate wildcard versions", () => {
-            stepVersionsFilter(mockStep);
+            stepVersions(mockStep);
             expect(mockStepSourceService.versionsOfStep).toHaveBeenCalledWith(mockStep);
+        });
+
+        it("should include latest version and own version as well in the list if the step is a library step", () => {
+            mockStepSourceService.versionsOfStep.and.returnValue(["2.0.x", "1.2.x", "1.1.1"]);
+
+            const versions = stepVersions(mockStep);
+
+            expect(versions).toEqual([null, "2.0.x", "1.2.x", "1.1.1"]);
         });
 
         it("should not include latest version in the list if the step is not library step", () => {
             mockStep.isLibraryStep = () => false;
             mockStepSourceService.versionsOfStep.and.returnValue(["1.2.x"]);
 
-            const versions = stepVersionsFilter(mockStep);
+            const versions = stepVersions(mockStep);
 
             expect(versions).toEqual(["1.2.x"]);
-        });
-    });
-
-    describe("versionLabel", () => {
-        it("should display version str if set", () => {
-            expect(versionLabelFilter("test-version")).toEqual("test-version");
-        });
-
-        it("should display latest version if version descriptor not set", () => {
-            expect(versionLabelFilter(null)).toContain("latest");
-        });
-    });
-
-    describe("isVersionDisabled", () => {
-        it("should return disable if version is not wildcard", () => {
-            mockSemverService.isVersionWildcard.and.returnValue(false);
-            expect(isVersionDisabledFilter("test-version-non-wildcard")).toBeTruthy();
-
-            mockSemverService.isVersionWildcard.and.returnValue(true);
-            expect(isVersionDisabledFilter("test-version-wildcard")).toBeFalsy();
         });
     });
 });
