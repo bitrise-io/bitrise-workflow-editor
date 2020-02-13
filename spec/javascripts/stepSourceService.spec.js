991d687a088b4b2e769bad4404ec1fbae857a3cc
Simon Márton <simon.marton@bitrise.io>
2020-02-13 17:08:59 +0100

    formatting

--- spec/javascripts/stepSourceService.spec.js
+++ spec/javascripts/stepSourceService.spec.js
@@ -1,45 +1,53 @@
 describe("stepSourceService", function() {
-
 	var stepSourceService;
 	var TEST_STEP_ID = "mockStep";
 	var TEST_LIB_URL = "http://tempuri.org";
 
 	beforeEach(module("BitriseWorkflowEditor"));
 
-	beforeEach(inject((_stepSourceService_) => {
+	beforeEach(inject(_stepSourceService_ => {
 		stepSourceService = _stepSourceService_;
 		stepSourceService.defaultLibraryURL = TEST_LIB_URL;
 
-		stepSourceService.libraries = [{
-			url: TEST_LIB_URL,
-			steps: {
-				[TEST_STEP_ID]: {
-					"2.2.1": { defaultStepConfig: { asset_urls: "test_urls", name: "2.2.1 config" }},
-					"1.2.1": { defaultStepConfig: "1.2.1 config" },
-					"1.1.1": { defaultStepConfig: "1.1.1 config" },
-					"1.0.0": { defaultStepConfig: "1.0.0 config" },
+		stepSourceService.libraries = [
+			{
+				url: TEST_LIB_URL,
+				steps: {
+					[TEST_STEP_ID]: {
+						"2.2.1": { defaultStepConfig: { asset_urls: "test_urls", name: "2.2.1 config" } },
+						"1.2.1": { defaultStepConfig: "1.2.1 config" },
+						"1.1.1": { defaultStepConfig: "1.1.1 config" },
+						"1.0.0": { defaultStepConfig: "1.0.0 config" }
+					}
+				},
+				latestStepVersions: {
+					[TEST_STEP_ID]: "2.2.1"
 				}
-			},
-			latestStepVersions: {
-				[TEST_STEP_ID]: "2.2.1"
 			}
-		}];
+		];
 	}));
 
 	describe("stepFromCVS", function() {
-
 		it("should return local step", function() {
 			expect(stepSourceService.stepFromCVS("path::path/to/step").localPath).not.toBeUndefined();
 			expect(stepSourceService.stepFromCVS("path::path/to/step").localPath).toBe("path/to/step");
-			expect(stepSourceService.stepFromCVS("path::path/to/step::another/path/to/step@1.0@2.0").localPath).toBe("path/to/step::another/path/to/step@1.0");
+			expect(stepSourceService.stepFromCVS("path::path/to/step::another/path/to/step@1.0@2.0").localPath).toBe(
+				"path/to/step::another/path/to/step@1.0"
+			);
 			expect(stepSourceService.stepFromCVS("path::::").localPath).toBe("::");
 			expect(stepSourceService.stepFromCVS("path::::@1.0").localPath).toBe("::");
 		});
 
 		it("should raise error on invalid local step CVS", function() {
-			expect(function() { stepSourceService.stepFromCVS("path::"); }).toThrow();
-			expect(function() { stepSourceService.stepFromCVS("path::@"); }).toThrow();
-			expect(function() { stepSourceService.stepFromCVS("path::@1.0"); }).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("path::");
+			}).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("path::@");
+			}).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("path::@1.0");
+			}).toThrow();
 		});
 
 		it("should return git step", function() {
@@ -52,13 +60,21 @@ describe("stepSourceService", function() {
 			expect(stepSourceService.stepFromCVS("git::git-url.git@1.0@").version).toBeNull();
 			expect(stepSourceService.stepFromCVS("git::git-url.git@1.0@2.0").gitURL).toBe("git-url.git@1.0");
 			expect(stepSourceService.stepFromCVS("git::git-url.git@1.0@2.0").version).toBe("2.0");
-			expect(stepSourceService.stepFromCVS("git::git-url.git::another-git-url.git@1.0@2.0").gitURL).toBe("git-url.git::another-git-url.git@1.0");
+			expect(stepSourceService.stepFromCVS("git::git-url.git::another-git-url.git@1.0@2.0").gitURL).toBe(
+				"git-url.git::another-git-url.git@1.0"
+			);
 		});
 
 		it("should raise error on invalid git step CVS", function() {
-			expect(function() { stepSourceService.stepFromCVS("git::"); }).toThrow();
-			expect(function() { stepSourceService.stepFromCVS("git::@"); }).toThrow();
-			expect(function() { stepSourceService.stepFromCVS("git::@1.0"); }).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("git::");
+			}).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("git::@");
+			}).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("git::@1.0");
+			}).toThrow();
 		});
 
 		it("should return library step", function() {
@@ -66,7 +82,9 @@ describe("stepSourceService", function() {
 			expect(stepSourceService.stepFromCVS(`library-url.git::${TEST_STEP_ID}@1.0`).libraryURL).toBe("library-url.git");
 			expect(stepSourceService.stepFromCVS(`library-url.git::${TEST_STEP_ID}@1.0`).id).toBe(TEST_STEP_ID);
 			expect(stepSourceService.stepFromCVS(`library-url.git::${TEST_STEP_ID}@1.0`).version).toBe("1.0");
-			expect(stepSourceService.stepFromCVS(`git@library-url.git::${TEST_STEP_ID}@1.0`).libraryURL).toBe("git@library-url.git");
+			expect(stepSourceService.stepFromCVS(`git@library-url.git::${TEST_STEP_ID}@1.0`).libraryURL).toBe(
+				"git@library-url.git"
+			);
 			expect(stepSourceService.stepFromCVS(`git@library-url.git::${TEST_STEP_ID}@1.0`).id).toBe(TEST_STEP_ID);
 			expect(stepSourceService.stepFromCVS(`git@library-url.git::${TEST_STEP_ID}@1.0`).version).toBe("1.0");
 			expect(stepSourceService.stepFromCVS(`git@library-url.git::${TEST_STEP_ID}`).version).toBeNull();
@@ -74,27 +92,49 @@ describe("stepSourceService", function() {
 			expect(stepSourceService.stepFromCVS(`::${TEST_STEP_ID}@1.0.0`).libraryURL).toBe(TEST_LIB_URL);
 			expect(stepSourceService.stepFromCVS(`library-url.git::${TEST_STEP_ID}`).version).toBeNull();
 			expect(stepSourceService.stepFromCVS(`library-url.git::${TEST_STEP_ID}@`).version).toBeNull();
-			expect(stepSourceService.stepFromCVS(`library-url.git::another-library-url.git::${TEST_STEP_ID}@1.0@2.0`).libraryURL).toBe("library-url.git");
-			expect(stepSourceService.stepFromCVS(`library-url.git::another-library-url.git::${TEST_STEP_ID}@1.0@2.0`).id).toBe(`another-library-url.git::${TEST_STEP_ID}@1.0`);
-			expect(stepSourceService.stepFromCVS(`library-url.git::another-library-url.git::${TEST_STEP_ID}@1.0@2.0`).version).toBe("2.0");
+			expect(
+				stepSourceService.stepFromCVS(`library-url.git::another-library-url.git::${TEST_STEP_ID}@1.0@2.0`).libraryURL
+			).toBe("library-url.git");
+			expect(
+				stepSourceService.stepFromCVS(`library-url.git::another-library-url.git::${TEST_STEP_ID}@1.0@2.0`).id
+			).toBe(`another-library-url.git::${TEST_STEP_ID}@1.0`);
+			expect(
+				stepSourceService.stepFromCVS(`library-url.git::another-library-url.git::${TEST_STEP_ID}@1.0@2.0`).version
+			).toBe("2.0");
 			expect(stepSourceService.stepFromCVS(`library-url.git::@1.0@2.0`).id).toBe("@1.0");
 			expect(stepSourceService.stepFromCVS(`library-url.git::@1.0@2.0`).version).toBe("2.0");
 		});
 
 		it("should raise error on invalid library step CVS", function() {
-			expect(function() { stepSourceService.stepFromCVS("::"); }).toThrow();
-			expect(function() { stepSourceService.stepFromCVS("::@"); }).toThrow();
-			expect(function() { stepSourceService.stepFromCVS("::@1.0"); }).toThrow();
-			expect(function() { stepSourceService.stepFromCVS("library-url::"); }).toThrow();
-			expect(function() { stepSourceService.stepFromCVS("library-url::@"); }).toThrow();
-			expect(function() { stepSourceService.stepFromCVS("library-url::@1.0"); }).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("::");
+			}).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("::@");
+			}).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("::@1.0");
+			}).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("library-url::");
+			}).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("library-url::@");
+			}).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("library-url::@1.0");
+			}).toThrow();
 		});
 
 		it("should raise error if library step has no library specified nor is default library specified", function() {
 			stepSourceService.defaultLibraryURL = null;
 
-			expect(function() { stepSourceService.stepFromCVS("step-id@1.0"); }).toThrow();
-			expect(function() { stepSourceService.stepFromCVS("::step-id@1.0"); }).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("step-id@1.0");
+			}).toThrow();
+			expect(function() {
+				stepSourceService.stepFromCVS("::step-id@1.0");
+			}).toThrow();
 		});
 
 		it("should only configure asset path for steps with invalid version", () => {
@@ -115,7 +155,7 @@ describe("stepSourceService", function() {
 				version: "1.1.1",
 				defaultStepConfig: "1.1.1 config",
 				libraryURL: "http://tempuri.org",
-				isLibraryStep: () => true,
+				isLibraryStep: () => true
 			};
 		});
 
@@ -134,7 +174,7 @@ describe("stepSourceService", function() {
 
 			var versions = stepSourceService.versionsOfStep(MOCK_STEP);
 
-			expect(versions).toEqual(["1.1.1"])
+			expect(versions).toEqual(["1.1.1"]);
 		});
 
 		it("should return null if the step is pointing to a wrong library", () => {
@@ -154,5 +194,4 @@ describe("stepSourceService", function() {
 			expect(versions).toEqual(["fake_version", "2.2.1", "1.2.1", "1.1.1", "1.0.0"]);
 		});
 	});
-
 });
