cb964c108417a5dc51b2675ccb7d488274cbc72a
Simon Márton <simon.marton@bitrise.io>
2020-02-20 12:55:23 +0100

    [WEB-3399] Use steplib search service for fuzzy search  (#443)
    * Added fuzzySearch method
    
    * Use fuzzy steplib search in the step sidebar
    
    * formatting
    
    * Added ellipsis to step name
    
    * Fix minor step editing bugs
    
    * Add error handlig to search
    
    * Pass calculated step depricitaion and verified info

--- source/javascripts/components/StepItem/StepItem.tsx
+++ source/javascripts/components/StepItem/StepItem.tsx
@@ -12,60 +12,74 @@ import verifiedIcon from "../../../images/step/badge-verified.svg";
 import deprecatedIcon from "../../../images/step/badge-deprecated.svg";
 
 type StringProps = {
-    alwaysLatest: string
-}
-
-type StepItemProps = {
-  step: Step,
-  strings: StringProps,
-  selected: boolean,
-  highlightVersionUpdate: boolean,
-  stepIndex: number,
-  onSelected: (step: Step, index: number) => void
-}
-
-export const normalizeIconUrl = (step?: Step): string | undefined => {
-  if (!step) {
-    return;
-  }
-
-  const stepIconURL = step.iconURL();
-  return stepIconURL || defaultStepIcon;
+	alwaysLatest: string;
 };
 
-const tabIndex = (selected: boolean): number => selected ? -1 : 0;
+type StepItemProps = {
+	step: Step;
+	title: string;
+	version: string;
+	isDeprecated: boolean;
+	isVerified: boolean;
+	strings: StringProps;
+	selected: boolean;
+	highlightVersionUpdate: boolean;
+	stepIndex: number;
+	onSelected: (step: Step, index: number) => void;
+};
 
-const stepVersion = (step: Step, highlightVersionUpdate: boolean) => (highlightVersionUpdate
-  ? <Text>{step.version}</Text>
-  : <Badge backgroundColor="red-3" color="white">
-      <Icon name="ArrowUp" />
-      {step.version}
-    </Badge>
-);
+export const normalizeIconUrl = (step?: Step): string | undefined => {
+	if (!step) {
+		return;
+	}
+
+	const stepIconURL = step.iconURL();
+	return stepIconURL || defaultStepIcon;
+};
+
+const tabIndex = (selected: boolean): number => (selected ? -1 : 0);
+
+const stepVersion = (step: Step, highlightVersionUpdate: boolean) =>
+	highlightVersionUpdate ? (
+		<Text>{step.version}</Text>
+	) : (
+		<Badge backgroundColor="red-3" color="white">
+			<Icon name="ArrowUp" />
+			{step.version}
+		</Badge>
+	);
 
 const StepItem: FunctionComponent<StepItemProps> = ({
-  step,
-  strings,
-  selected,
-  highlightVersionUpdate,
-  stepIndex,
-  onSelected
-} : StepItemProps) => (
-  <button className="step" tabIndex={tabIndex(selected)} onClick={() => onSelected(step, stepIndex)}>
-    <img className="icon" src={normalizeIconUrl(step)} />
-    <span className="info">
-      <strong>
-        <Text className="title">{step.displayName()}</Text>
-        {step.isVerified() && <img className="verified" src={verifiedIcon} />}
-        {step.isDeprecated() && <img className="deprecated" src={deprecatedIcon} />}
-      </strong>
-      <em className="version">
-        {step.requestedVersion()
-            ? stepVersion(step, highlightVersionUpdate)
-            : <Text>{strings.alwaysLatest + (step.version && ` (${step.version})`)}</Text>}
-      </em>
-    </span>
-  </button>
+	step,
+	title,
+	version,
+	isDeprecated,
+	isVerified,
+	strings,
+	selected,
+	highlightVersionUpdate,
+	stepIndex,
+	onSelected
+}: StepItemProps) => (
+	<button className="step" tabIndex={tabIndex(selected)} onClick={() => onSelected(step, stepIndex)}>
+		<img className="icon" src={normalizeIconUrl(step)} />
+		<span className="info">
+			<strong>
+				<Text className="title" ellipsis>
+					{title}
+				</Text>
+				{isVerified && <img className="verified" src={verifiedIcon} />}
+				{isDeprecated && <img className="deprecated" src={deprecatedIcon} />}
+			</strong>
+			<em className="version">
+				{version ? (
+					stepVersion(step, highlightVersionUpdate)
+				) : (
+					<Text>{strings.alwaysLatest + (version ? ` (${version})` : "")}</Text>
+				)}
+			</em>
+		</span>
+	</button>
 );
 
 export default StepItem;
