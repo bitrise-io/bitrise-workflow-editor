37824fd4c41673474e1d5e3d2617d9ce717becb4
Adam Pelle <adam.pelle@bitrise.io>
2020-03-10 09:54:23 +0100

    refactored common cypress utils

--- spec/integration/common.js
+++ spec/integration/common.js
@@ -1,26 +1,22 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
-import $ from "./elements";
+import $, { selector } from "./elements";
 import { version } from '../../package.json';
 
-const PORT = Cypress.env('PORT')
+const PORT = Cypress.env('PORT');
 
-Given('editor is open', () => {
-  cy.visit(`http://localhost:${PORT}/${version}/#!/workflows`);
-});
-
-When('I click on {string}', (element) => {
+export const click = (element) => {
   $(element).click();
-});
+};
 
-When('I select {string} from {string}', (value, element) => {
+export const select = (value, element) => {
   $(element).select(value);
-});
+};
 
-When('I type {string} in {string}', (text, element) => {
+export const type = (text, element) => {
   $(element).type(text, { force: true }).trigger('input');
-});
+};
 
-When('I confirm on {string} popup', (type) => {
+export const popupConfirm = (type) => {
   let selector = '#default-confirm-popup-body .yes';
 
   if (type === 'alert') {
@@ -28,33 +24,42 @@ When('I confirm on {string} popup', (type) => {
   }
 
   cy.get(selector).click();
-});
+};
 
-Then('I should see {string} in {string}', (text, element) => {
+export const assertInputValueEQ = (value, element) => {
   $(element).should($el => {
-    expect($el).to.contain(text);
+    expect($el).to.contain(value);
   });
-});
+};
 
-Then('I should not see {string} in {string}', (text, element) => {
+export const assertNotInputValueNotEQ = (text, element) => {
   $(element).should($el => {
     expect($el).not.to.contain(text);
   })
+};
+
+export const save = () => {
+  cy.get('button.save.rebo').then(btn => {
+    if (!btn.is(':disabled')) {
+      btn.click();
+    }
+  });
+};
+
+Given('editor is open', () => {
+  cy.visit(`http://localhost:${PORT}/${version}/#!/workflows`);
 });
+
+When('I click on {string}', click);
+When('I select {string} from {string}', select);
+When('I type {string} in {string}', type);
+When('I confirm on {string} popup', popupConfirm);
+Then('I should see {string} in {string}', assertInputValueEQ);
+Then('I should not see {string} in {string}', assertNotInputValueNotEQ);
 
 Then('{string} should {string}', (element, expectation) => {
   const cExpectation = expectation.replace(/\s/g, '.');
   $(element).should(cExpectation);
 });
 
-Then('I should not see {string}', (element) => {
-  $(element).should('not.be.visible');
-});
-
-Then('I save', () => {
-  cy.get('button.save.rebo').then(btn => {
-    if (!btn.is(':disabled')) {
-      btn.click();
-    }
-  });
-});
+Then('I save', save);
--- spec/integration/workflows/steps.js
+++ spec/integration/workflows/steps.js
@@ -1,6 +1,6 @@
-import { Then } from "cypress-cucumber-preprocessor/steps";
+import { Given, Then } from "cypress-cucumber-preprocessor/steps";
 import $el, { elements } from '../elements';
-import "../common";
+import { click, select, type } from "../common";
 
 afterEach(() => {
   $el(elements["Discard Button"]).then(btn => {
@@ -8,6 +8,13 @@ afterEach(() => {
       btn.click();
     }
   });
+});
+
+Given('Workflow with name {string}', (name) => {
+  click("Add Workflow Button");
+  select("Empty workflow", "Workflow Type Dropdown");
+  type(name, "Workflow Name");
+  click("Workflow Add Button");
 });
 
 Then('Workflow appeared with name {string}', (name) => {
