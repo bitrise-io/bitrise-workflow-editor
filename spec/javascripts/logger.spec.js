227bc41136bd6f21ef705c20aea246a50eab616e
Simon Márton <simon.marton@bitrise.io>
2020-03-16 09:51:05 +0100

    Added debug level and object ctx

--- spec/javascripts/logger.spec.js
+++ spec/javascripts/logger.spec.js
@@ -1,48 +1,54 @@
 describe("DataDogLoggerService", () => {
-  const testApiKey = "some-api-key";
+	const testApiKey = "some-api-key";
 
-  let logger;
-  let mockDatadogLogs = {};
+	let logger;
+	let mockDatadogLogs = {};
 
 	beforeEach(() => {
-    window.datadogLogs = mockDatadogLogs;
+		window.datadogLogs = mockDatadogLogs;
 
-    module("BitriseWorkflowEditor");
-    module(($provide) => {
+		module("BitriseWorkflowEditor");
+		module($provide => {
 			$provide.constant("DATADOG_API_KEY", testApiKey);
-    });
+		});
 
-    mockDatadogLogs.init = jasmine.createSpy("init");
-    mockDatadogLogs.logger = {
-      info: jasmine.createSpy("info"),
-      error: jasmine.createSpy("error"),
-      warn: jasmine.createSpy("warn"),
-    };
-  });
+		mockDatadogLogs.init = jasmine.createSpy("init");
+		mockDatadogLogs.logger = {
+			debug: jasmine.createSpy("debug"),
+			info: jasmine.createSpy("info"),
+			error: jasmine.createSpy("error"),
+			warn: jasmine.createSpy("warn")
+		};
+	});
 
-	beforeEach(inject((_logger_) => {
+	beforeEach(inject(_logger_ => {
 		logger = _logger_;
-  }));
+	}));
 
 	it("initialized correctly", () => {
 		expect(mockDatadogLogs.init).toHaveBeenCalledWith({
-      clientToken: testApiKey,
-      forwardErrorsToLogs: false
-    });
-  });
+			clientToken: testApiKey,
+			forwardErrorsToLogs: false
+		});
+	});
 
-  it("should use datadog info logging", () => {
-    logger.info("test", "test-ctx");
-    expect(mockDatadogLogs.logger.info).toHaveBeenCalledWith("test", "test-ctx");
-  });
+	it("should use datadog debug logging", () => {
+		logger.debug("test", { test: "ctx" });
+		expect(mockDatadogLogs.logger.debug).toHaveBeenCalledWith("test", { test: "ctx" });
+	});
 
-  it("should use datadog error logging", () => {
-    logger.error("test", "test-ctx");
-    expect(mockDatadogLogs.logger.error).toHaveBeenCalledWith("test", "test-ctx");
-  });
+	it("should use datadog info logging", () => {
+		logger.info("test", { test: "ctx" });
+		expect(mockDatadogLogs.logger.info).toHaveBeenCalledWith("test", { test: "ctx" });
+	});
 
-  it("should use datadog warning logging", () => {
-    logger.warn("test", "test-ctx");
-    expect(mockDatadogLogs.logger.warn).toHaveBeenCalledWith("test", "test-ctx");
-  });
+	it("should use datadog error logging", () => {
+		logger.error("test", { test: "ctx" });
+		expect(mockDatadogLogs.logger.error).toHaveBeenCalledWith("test", { test: "ctx" });
+	});
+
+	it("should use datadog warning logging", () => {
+		logger.warn("test", { test: "ctx" });
+		expect(mockDatadogLogs.logger.warn).toHaveBeenCalledWith("test", { test: "ctx" });
+	});
 });
