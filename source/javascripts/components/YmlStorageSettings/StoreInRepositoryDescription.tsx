d2a458849979f4e12b036a7085e0160042be8140
David Kiss <david.kiss@bitrise.io>
2020-08-07 18:06:30 +0200

    Create StoreInRepositoryDescription component

---
+++ source/javascripts/components/YmlStorageSettings/StoreInRepositoryDescription.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+import { Flex, Text } from "@bitrise/bitkit";
+import InlineLink from "../common/InlineLink";
+
+type Props = {
+	title: string;
+	description: string;
+};
+
+const StoreInRepositoryDescription: FC<Props> = ({ title, description }: Props) => {
+	return (
+		<Flex direction="vertical" gap="x4">
+			<Text config="5" textColor="gray-8">
+				{title}
+			</Text>
+			<Text textColor="gray-7" config="7">
+				<Text inline>{description}</Text>
+				<Text inline>
+					Read more in <InlineLink text="our documentation" url="REPLACE URL" />
+				</Text>
+			</Text>
+		</Flex>
+	);
+};
+
+export default StoreInRepositoryDescription;
