f02f31f4a910de68a58b5fb7d11dc5d308f01e91
mate.nadasdi <mate.nadasdi@mate.nadasdi-mbp>
2021-07-02 17:15:51 +0200

    [PLANG-110] Add custom rum timing to WFE page loads

---
+++ source/javascripts/utils/datadogCustomRumTiming.ts
@@ -0,0 +1,17 @@
+import { datadogRum } from "@datadog/browser-rum";
+
+const reportedTimings: Set<string> = new Set();
+
+export default (page: string, name: string, onloadOnly = true): void => {
+  const reportName = `${page}_${name}`;
+
+  if (onloadOnly) {
+    if (reportedTimings.has(reportName)) {
+      return;
+    }
+
+    reportedTimings.add(reportName);
+  }
+
+  datadogRum.addTiming(reportName);
+};
