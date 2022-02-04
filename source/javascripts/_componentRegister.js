1b55f205b1ff252f5ce7086e8e4ec74e474d4938
Zoltan Szabo <63643463+zoltanszabo-bitrise@users.noreply.github.com>
2022-02-04 16:50:39 +0100

    feat: Create workflow recipe link component [OX-659] (#608)

--- source/javascripts/_componentRegister.js
+++ source/javascripts/_componentRegister.js
@@ -6,6 +6,7 @@ import StepVersionDetails from "./components/StepVersionDetails/StepVersionDetai
 import YmlStorageSettings from "./components/YmlStorageSettings/YmlStorageSettings";
 import UpdateYmlInRepositoryModal from "./components/UpdateYmlInRepositoryModal/UpdateYmlInRepositoryModal";
 import WorkflowSelector from "./components/WorkflowSelector/WorkflowSelector";
+import WorkflowRecipesLink from "./components/workflow-recipes/WorkflowRecipesLink/WorkflowRecipesLink";
 
 var register = react2angular;
 
@@ -42,4 +43,8 @@ angular
 	.component(
 		"rWorkflowSelector",
 		register(WorkflowSelector, ["selectedWorkflowId", "workflows", "selectWorkflow", "renameWorkflowConfirmed"])
+	)
+	.component(
+		"rWorkflowRecipesLink",
+		register(WorkflowRecipesLink, ["id"])
 	);
---
+++ source/javascripts/components/workflow-recipes/WorkflowRecipesLink/WorkflowRecipesLink.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Flex, Icon, Link, Text, Tooltip } from "@bitrise/bitkit";
+
+type WorkflowRecipesLinkProps = {
+	id: string;
+}
+
+const WorkflowRecipesLink = ({ id }: WorkflowRecipesLinkProps): JSX.Element => (
+	<Flex direction='horizontal' gap='x2' alignChildrenVertical='middle'>
+		<Link id={id} href='https://github.com/bitrise-io/workflow-recipes' target='_blank' color='grape-3'>
+			<Flex direction='horizontal' gap='x0'>
+				<Icon name='OpenInBrowser' size='20px' />
+				<Text size='x3'>Workflow Recipes</Text>
+			</Flex>
+		</Link>
+		<Tooltip title='Workflow Recipes provide ready-made solutions for common Workflow tasks.'>
+			{({ ref, ...rest }) => (
+				<Icon innerRef={ref} name='Support' size='20px' textColor='gray-5' {...rest} />
+			)}
+		</Tooltip>
+	</Flex>
+);
+
+export default WorkflowRecipesLink;
