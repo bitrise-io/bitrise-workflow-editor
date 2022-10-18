269369f4cfed9b31b351bc3a9403fba3673b8a7d
Dániel Décsi <97228168+danieldecsi-bitrise@users.noreply.github.com>
2022-10-18 14:37:56 +0200

    Stop exposing environment variables (#708)

--- webpack.config.js
+++ webpack.config.js
@@ -191,9 +191,7 @@ module.exports = {
 		]
 	},
 	plugins: [
-		new webpack.DefinePlugin({
-			"process.env": JSON.stringify(process.env)
-		}),
+		new webpack.EnvironmentPlugin(["MODE"]),
 		new MiniCssExtractPlugin({
 			filename: "stylesheets/[name].css"
 		}),
