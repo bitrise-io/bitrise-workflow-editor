337577ff995004e27c506959dfbeaafe63c921e2
avivenzio-bitrise <alex.vivenzio@bitrise.io>
2022-10-18 10:10:03 -0400

    use default text size

--- source/javascripts/components/GuidedOnboarding/GuidedOnboardingHeader.tsx
+++ source/javascripts/components/GuidedOnboarding/GuidedOnboardingHeader.tsx
@@ -3,7 +3,7 @@ import { Box, Icon, Text } from "@bitrise/bitkit";
 export const GuidedOnboardingHeader = (): JSX.Element => (
     <Box display="flex" padding="0 18px">
       <Icon name="Info" />{" "}
-      <Text size="3" fontWeight="bold" paddingLeft="10px">
+      <Text fontWeight="bold" paddingLeft="10px">
         Getting Started Guide
       </Text>
     </Box>
