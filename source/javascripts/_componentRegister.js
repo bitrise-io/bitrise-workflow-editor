2767a6e8bc66f6f118987c40cb395e281a9e67fc
cdhenley219-bit <chanel.henley@bitrise.io>
2022-11-01 19:45:27 -0400

    Took out references to lastWorkflowEditedApp

--- source/javascripts/_componentRegister.js
+++ source/javascripts/_componentRegister.js
@@ -74,4 +74,4 @@ angular
 	)
 	.component("rWorkflowRecipesInfoBanner", register(WorkflowRecipesInfoBanner, []))
 	.component("rProductTour", register(ProductTour, ["menuIds", "currentUser", "productTourShown", "onDismiss"]))
-	.component("rGuidedOnboarding", register(GuidedOnboarding, ["isEnabled", "isOpen", "onTurnOff", "buildStatus", "lastWorkflowEditedApp", "lastWorkflowEditedDate", "lastRunningBuildFinishDate"]));
+	.component("rGuidedOnboarding", register(GuidedOnboarding, ["isEnabled", "isOpen", "onTurnOff", "buildStatus", "lastWorkflowEditedDate", "lastRunningBuildFinishDate"]));
