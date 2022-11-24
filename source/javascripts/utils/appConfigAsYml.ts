65340cd536d9b72f83bdfdb0ce373175779a4386
Bálint <balint.morasz@bitrise.io>
2022-11-24 15:07:45 +0100

    Revert "fix: standardize yml format (#720)" (#722)
    This reverts commit fa3bf5a8b5f32748d9096e866e393cee7ff556b8.

--- source/javascripts/utils/appConfigAsYml.ts
+++ source/javascripts/utils/appConfigAsYml.ts
@@ -1,4 +1,4 @@
-import { dump, JSON_SCHEMA } from "js-yaml";
+import { safeDump } from "js-yaml";
 import { AppConfig } from "../models/AppConfig";
 
 export default function appConfigAsYml(appConfig: AppConfig | undefined): string {
@@ -10,5 +10,5 @@ export default function appConfigAsYml(appConfig: AppConfig | undefined): string
 		return appConfig;
 	}
 
-	return dump(appConfig, { noArrayIndent: true, schema: JSON_SCHEMA , sortKeys: true });
+	return `---\n${safeDump(appConfig, { noArrayIndent: true })}`;
 }
