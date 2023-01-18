a0618e938db70d9960c4f7fb930a1894b2291435
erosdome <jozsef.eros@bitrise.io>
2023-01-18 15:27:31 +0100

    Handle app UUID slugs [SWAT-264] (#729)

--- spec/javascripts/appService.spec.js
+++ spec/javascripts/appService.spec.js
@@ -1,30 +1,46 @@
 describe("AppService", () => {
-  let appService;
+	let appService;
 
 	beforeEach(() => {
-    module("BitriseWorkflowEditor");
-    inject((_appServiceUtil_) => {
-      appService = _appServiceUtil_;
-    });
-  });
+		module("BitriseWorkflowEditor");
+		inject(_appServiceUtil_ => {
+			appService = _appServiceUtil_;
+		});
+	});
 
-  const runWithUrl = (url, fn) => {
-    with({ document: { location: { href: url } }}) fn;
-  };
+	const runWithUrl = (url, fn) => {
+		with ({ document: { location: { href: url } } }) fn;
+	};
 
-  describe("getAppSlug", () => {
-    it("should return app slug from the path", () => {
-      const testSlug = "testId01"
+	describe("getAppSlug", () => {
+		it("should return app slug from the path if it is in hex format", () => {
+			runWithUrl("https://app.bitrise.io/app/abcdefgh12345678/workflow_editor#!/workflows?workflow_id=primary", () => {
+				expect(appService.getAppSlug()).toEqual("abcdefgh12345678");
+			});
+		});
 
-      runWithUrl(`https://app.bitrise.io/app/${testSlug}/workflow_editor#!/workflows?workflow_id=primary`, () => {
-        expect(appService.getAppSlug()).toEqual(testSlug);
-      });
-    });
+		it("should return app slug from the path if it is in UUID format", () => {
+			runWithUrl(
+				"https://app.bitrise.io/app/abcd1234-ef56-7890-ab12-cd34567890ab/workflow_editor#!/workflows?workflow_id=primary",
+				() => {
+					expect(appService.getAppSlug()).toEqual("abcd1234-ef56-7890-ab12-cd34567890ab");
+				}
+			);
+		});
 
-    it("should return null if url not in expected format", () => {
-      const testSlug = "testId01"
+		it("should return null if url not in expected format", () => {
+			runWithUrl("https://app.bitrise.io/zzzzzzzz12345678/something", () => {
+				expect(appService.getAppSlug()).toBeNull();
+			});
 
-			runWithUrl(`https://app.bitrise.io/test${testSlug}/something`, () => {
+			runWithUrl(
+				"https://app.bitrise.io/app/zzzzzzzz-ef56-7890-ab12-cd34567890ab/workflow_editor#!/workflows?workflow_id=primary",
+				() => {
+					expect(appService.getAppSlug()).toBeNull();
+				}
+			);
+
+			runWithUrl("https://app.bitrise.io/testabcdefgh12345678/something", () => {
 				expect(appService.getAppSlug()).toBeNull();
 			});
 		});
@@ -33,16 +49,14 @@ describe("AppService", () => {
 	describe("handleSecretAfterSave", () => {
 		let Variable;
 
-		beforeEach(inject(function (_Variable_) {
+		beforeEach(inject(function(_Variable_) {
 			Variable = _Variable_;
 		}));
 
 		it("should set value to null if secret is protected", () => {
-			const secret = new Variable(
-				{
-					"TEST_KEY": "test value"
-				}
-			);
+			const secret = new Variable({
+				TEST_KEY: "test value"
+			});
 			secret.isProtected(true);
 
 			appService.handleSecretAfterSave(secret);
@@ -54,11 +68,9 @@ describe("AppService", () => {
 
 		it("should leave the value untouched if secret is not protected", () => {
 			const testValue = "test value";
-			const secret = new Variable(
-				{
-					"TEST_KEY": testValue
-				}
-			);
+			const secret = new Variable({
+				TEST_KEY: testValue
+			});
 			secret.isProtected(false);
 
 			appService.handleSecretAfterSave(secret);
