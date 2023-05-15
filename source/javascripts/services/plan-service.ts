93e33f6d3a42ea9d432eae43cce17d3dd02866af
avivenzio-bitrise <112978063+avivenzio-bitrise@users.noreply.github.com>
2023-05-15 11:03:15 -0400

    add deprecated plans to modern concurrency (#779)

--- source/javascripts/services/plan-service.ts
+++ source/javascripts/services/plan-service.ts
@@ -1,7 +1,13 @@
 const STARTER_PACKAGE = "teams-package";
 const STARTER_PACKAGE_TRIAL = "starter-package-trial";
+const DEPRECATED_PLANS = [
+	"deprecated-concrete-developer",
+	"deprecated-concrete-pro",
+	"deprecated-organization-elite",
+	"deprecated-enterprise-elite"
+];
 
-const MODERN_CONCURRENCY_PLANS = [STARTER_PACKAGE, STARTER_PACKAGE_TRIAL];
+const MODERN_CONCURRENCY_PLANS = [STARTER_PACKAGE, STARTER_PACKAGE_TRIAL, ...DEPRECATED_PLANS];
 
 export const isModernConcurrencyPlan = (name: string): boolean => {
 	return MODERN_CONCURRENCY_PLANS.includes(name);
