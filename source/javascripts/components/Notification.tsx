3f4957df125c2def2ad026a58df1bf423b844e6e
Gergely Santa <gergely.santa@bitrise.io>
2023-11-30 15:55:39 +0100

    Show notification

---
+++ source/javascripts/components/Notification.tsx
@@ -0,0 +1,26 @@
+import { FunctionComponent } from "react";
+import { Notification as BitkitNotification, NotificationProps } from "@bitrise/bitkit";
+import { Text } from "@bitrise/bitkit";
+
+type Props = {
+	message: string;
+  title?: string;
+  status: NotificationProps['status'];
+};
+
+const Notification: FunctionComponent<Props> = ({ message, status, title }: Props) => {
+  if (title) {
+    return (
+      <BitkitNotification status={status}>
+        <Text fontWeight="bold">{title}</Text>
+        <Text>{message}</Text>
+      </BitkitNotification>
+    )
+  }
+
+  return (
+    <BitkitNotification status={status}>{message}</BitkitNotification>
+  )
+}
+
+export default Notification;
