abeed5d9cd930ab30c6c3b0dc5aae4171a26f4c8
Gergely Santa <gergely.santa@bitrise.io>
2024-01-03 14:28:47 +0100

    Fix lint issues

--- spec/integration/cypress/commands.js
+++ spec/integration/cypress/commands.js
@@ -1,15 +1,3 @@
-// TODO: Cypress does not support request based whitelisting :( https://github.com/cypress-io/cypress/issues/4843
-const mockRequest = (method, url, fn) => ({
-	method,
-	url,
-	onResponse: xhr => {
-		const mockResponse = fn(xhr);
-		if (mockResponse) {
-			xhr.response.body = mockResponse;
-		}
-	}
-});
-
 Cypress.Commands.add("waitForSteps", () => {
 	// TODO: cypress does not support polling :(
 	// https://github.com/cypress-io/cypress/issues/3308
