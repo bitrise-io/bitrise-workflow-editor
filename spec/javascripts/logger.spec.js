4cfae048c756740cec5cb4a16fae6733d771172c
Gergely Santa <gergely.santa@bitrise.io>
2024-01-03 14:38:23 +0100

    Fix test

--- spec/javascripts/logger.spec.js
+++ spec/javascripts/logger.spec.js
@@ -51,8 +51,8 @@ describe("DataDogLoggerService", () => {
 	it("setTags", () => {
 		logger.setTags(mockContext);
 
-		expect(mockInnerLogger.addContext).toHaveBeenCalledWith("test", "test-val");
-		expect(mockInnerLogger.addContext).toHaveBeenCalledWith("test2", "test2-val");
+		expect(mockInnerLogger.setContext).toHaveBeenCalledWith("test", "test-val");
+		expect(mockInnerLogger.setContext).toHaveBeenCalledWith("test2", "test2-val");
 	});
 
 	it("should use datadog debug logging", () => {
