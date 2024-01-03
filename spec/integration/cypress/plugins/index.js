7ca5e67691512e854e4892afc3a60bf670f5f390
Gergely Santa <gergely.santa@bitrise.io>
2024-01-03 16:20:14 +0100

    Remove comment

--- spec/integration/cypress/plugins/index.js
+++ spec/integration/cypress/plugins/index.js
@@ -18,6 +18,5 @@ const cucumber = require("cypress-cucumber-preprocessor").default
  */
 module.exports = (on) => {
   // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
   on("file:preprocessor", cucumber());
 }
