01245eef41feb28a009b839cfffe09d4140cc8bd
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-03-18 17:16:45 +0100

    chore: add bitrise-eslint-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,68 @@
 module.exports = {
-	parser: "@typescript-eslint/parser",
-	root: true,
-	env: {
-		node: true,
-		browser: true,
-		jasmine: true,
-	},
-	plugins: ["@typescript-eslint", "simple-import-sort", "unused-imports", "jasmine", "prettier"],
-	globals: {
-		_: false,
-		cy: false,
-		inject: false,
-		angular: false,
-		Cypress: false,
-		Promise: false,
-	},
-	settings: {
-		react: {
-			version: "detect",
-		},
-	},
-	extends: [
-		"prettier",
-		"eslint:recommended",
-		"plugin:react/recommended",
-		"plugin:react/jsx-runtime",
-		"plugin:storybook/recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:@typescript-eslint/eslint-recommended",
-	],
-	rules: {
-		"@typescript-eslint/ban-types": ["warn", { types: { "{}": false }, extendDefaults: true }],
-		"@typescript-eslint/no-explicit-any": ["warn", { ignoreRestArgs: true }],
-		"@typescript-eslint/no-non-null-assertion": "off",
-		"@typescript-eslint/no-unused-vars": "off",
-		"prefer-const": ["error", { destructuring: "all" }],
-		"prettier/prettier": "warn",
-		"react/no-unescaped-entities": ["error", { forbid: [">", '"', "}"] }],
-		"simple-import-sort/imports": "warn",
-		"unused-imports/no-unused-imports": "warn",
-		"unused-imports/no-unused-vars": ["warn", { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }],
-	},
-	overrides: [
-		{
-			files: ["*.js"],
-			rules: {
-				"@typescript-eslint/no-var-requires": "off",
-				"@typescript-eslint/explicit-function-return-type": "off",
-			},
-		},
-	],
+  root: true,
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.eslint.json"],
+    tsconfigRootDir: __dirname,
+  },
+  env: {
+    node: true,
+    browser: true,
+    jasmine: true,
+  },
+  globals: {
+    _: false,
+    cy: false,
+    inject: false,
+    angular: false,
+    Cypress: false,
+    Promise: false,
+  },
+  extends: ["plugin:@bitrise/config"],
+  plugins: ["jasmine", "lodash"],
+  rules: {
+    /** * Import related rules ** */
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "**/*.stories.tsx",
+          "**/*.spec.{ts,tsx}",
+          "**/*.mocks.ts",
+          "**/*.utils.ts",
+          "**/*.factory.ts",
+          "*rc.js",
+          "*.config.{js,ts}",
+        ],
+      },
+    ],
+    "prettier/prettier": ["error", { useTabs: false }],
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "jsx-a11y/anchor-is-valid": "warn",
+    "@typescript-eslint/no-non-null-assertion": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/ban-types": ["warn", { types: { "{}": false }, extendDefaults: true }],
+    "react/no-unescaped-entities": ["error", { forbid: [">", '"', "}"] }],
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+      },
+    },
+    {
+      files: ["*.stories.*"],
+      rules: {
+        "react-hooks/rules-of-hooks": "warn",
+      },
+    },
+    {
+      files: ["*.spec.{ts,tsx}", "*.stories.tsx", "*.mocks.ts", "*.factory.ts", "*.utils.ts"],
+      rules: {
+        "@typescript-eslint/no-non-null-assertion": "off",
+      },
+    },
+  ],
 };
