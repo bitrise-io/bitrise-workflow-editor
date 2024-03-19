920fc712012318d7fe684ca8f7d1a58eae48b978
Laszlo Moczo <laszlo.moczo@bitrise.io>
2024-03-19 19:45:00 +0100

    Storybook deploy to GCP

--- .storybook/deploy-storybook.js
+++ .storybook/deploy-storybook.js
@@ -34,6 +34,8 @@ async function createComment(path) {
 
 async function uploadFiles() {
   if (!process.env.STORYBOOK_GCP_DEPLOY_BUCKET || !process.env.STORYBOOK_GCP_DEPLOY_KEY) {
+    console.log(!!process.env.STORYBOOK_GCP_DEPLOY_BUCKET);
+    console.log(!!process.env.STORYBOOK_GCP_DEPLOY_KEY);
     throw new Error('Storybook deploy config not found!');
   }
 
