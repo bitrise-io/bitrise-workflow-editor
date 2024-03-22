f243be46fe5af132cc07f7a0db7396fff720bcac
Tamas Papik <trapacska@gmail.com>
2024-03-22 16:16:16 +0100

    fix workflow selection in dropodnws

--- source/javascripts/components/TriggersPage/AddPushTriggerDialog.tsx
+++ source/javascripts/components/TriggersPage/AddPushTriggerDialog.tsx
@@ -1,21 +1,21 @@
 import { ReactNode, useEffect, useMemo, useState } from 'react';
 import {
-  Box,
-  Button,
-  Card,
-  Checkbox,
-  DefinitionTooltip,
-  Dialog,
-  DialogBody,
-  DialogFooter,
-  Divider,
-  Icon,
-  Input,
-  ProgressIndicator,
-  ProgressIndicatorProps,
-  Select,
-  Text,
-  Tooltip,
+	Box,
+	Button,
+	Card,
+	Checkbox,
+	DefinitionTooltip,
+	Dialog,
+	DialogBody,
+	DialogFooter,
+	Divider,
+	Icon,
+	Input,
+	ProgressIndicator,
+	ProgressIndicatorProps,
+	Select,
+	Text,
+	Tooltip,
 } from '@bitrise/bitkit';
 import { Controller, FormProvider, useFieldArray, useForm, useFormContext } from 'react-hook-form';
 
@@ -23,288 +23,288 @@ import { Condition, FormItems, PushConditionType, TriggerItem } from './Triggers
 import { checkIsConditionsUsed } from './TriggersPage.utils';
 
 type DialogProps = {
-  currentTriggers: TriggerItem[];
-  isOpen: boolean;
-  onClose: () => void;
-  pipelines: string[];
-  onSubmit: (action: 'add' | 'edit', trigger: TriggerItem) => void;
-  editedItem?: TriggerItem;
-  workflows: string[];
+	currentTriggers: TriggerItem[];
+	isOpen: boolean;
+	onClose: () => void;
+	pipelines: string[];
+	onSubmit: (action: 'add' | 'edit', trigger: TriggerItem) => void;
+	editedItem?: TriggerItem;
+	workflows: string[];
 };
 
 const LABEL_MAP: Record<PushConditionType, string> = {
-  push_branch: 'Push branch',
-  commit_message: 'Commit message',
-  changed_files: 'Path',
+	push_branch: 'Push branch',
+	commit_message: 'Commit message',
+	changed_files: 'Path',
 };
 
 const getLabelText = (isRegex: boolean, type: PushConditionType): string => {
-  if (isRegex) {
-    return 'Regex pattern';
-  }
-  return LABEL_MAP[type];
+	if (isRegex) {
+		return 'Regex pattern';
+	}
+	return LABEL_MAP[type];
 };
 
 type ConditionCardProps = {
-  children: ReactNode;
-  conditionNumber: number;
+	children: ReactNode;
+	conditionNumber: number;
 };
 
 const OPTIONS_MAP: Record<PushConditionType, string> = {
-  push_branch: 'Push branch',
-  commit_message: 'Commit message',
-  changed_files: 'File change',
+	push_branch: 'Push branch',
+	commit_message: 'Commit message',
+	changed_files: 'File change',
 };
 
 const ConditionCard = (props: ConditionCardProps) => {
-  const { children, conditionNumber } = props;
-  const { register, watch } = useFormContext();
-  const { conditions } = watch();
-  const { isRegex, type } = conditions[conditionNumber] || {};
+	const { children, conditionNumber } = props;
+	const { register, watch } = useFormContext();
+	const { conditions } = watch();
+	const { isRegex, type } = conditions[conditionNumber] || {};
 
-  return (
-    <Card key={conditionNumber} marginBottom="16" padding="16px 16px 24px 16px">
-      <Box display="flex" justifyContent="space-between" alignItems="center" marginBottom="12">
-        <Text textStyle="heading/h5">Condition {conditionNumber + 1}</Text>
-        {children}
-      </Box>
-      <Select
-        marginBottom="16"
-        placeholder="Select a condition type"
-        {...register(`conditions.${conditionNumber}.type`)}
-      >
-        {Object.entries(OPTIONS_MAP).map(([optionType, text]) => {
-          const isConditionTypeUsed = conditions.some((condition: Condition) => condition.type === optionType);
-          const isTypeOfCurrentCard = optionType === conditions[conditionNumber].type;
+	return (
+		<Card key={conditionNumber} marginBottom="16" padding="16px 16px 24px 16px">
+			<Box display="flex" justifyContent="space-between" alignItems="center" marginBottom="12">
+				<Text textStyle="heading/h5">Condition {conditionNumber + 1}</Text>
+				{children}
+			</Box>
+			<Select
+				marginBottom="16"
+				placeholder="Select a condition type"
+				{...register(`conditions.${conditionNumber}.type`)}
+			>
+				{Object.entries(OPTIONS_MAP).map(([optionType, text]) => {
+					const isConditionTypeUsed = conditions.some((condition: Condition) => condition.type === optionType);
+					const isTypeOfCurrentCard = optionType === conditions[conditionNumber].type;
 
-          if (isConditionTypeUsed && !isTypeOfCurrentCard) {
-            return undefined;
-          }
+					if (isConditionTypeUsed && !isTypeOfCurrentCard) {
+						return undefined;
+					}
 
-          return (
-            <option key={optionType} value={optionType}>
-              {text}
-            </option>
-          );
-        })}
-      </Select>
-      {!!type && (
-        <>
-          <Checkbox marginBottom="8" {...register(`conditions.${conditionNumber}.isRegex`)}>
-            Use regex pattern
-          </Checkbox>
-          <Tooltip label="Regular Expression (regex) is a sequence of characters that specifies a match pattern in text.">
-            <Icon name="Info" size="16" marginLeft="5" />
-          </Tooltip>
-          <Controller
-            name={`conditions.${conditionNumber}.value`}
-            render={({ field }) => (
-              <Input
-                {...field}
-                onChange={(e) => field.onChange(e.target.value.trimStart())}
-                label={getLabelText(isRegex, type)}
-                placeholder="*"
-              />
-            )}
-          />
-        </>
-      )}
-    </Card>
-  );
+					return (
+						<option key={optionType} value={optionType}>
+							{text}
+						</option>
+					);
+				})}
+			</Select>
+			{!!type && (
+				<>
+					<Checkbox marginBottom="8" {...register(`conditions.${conditionNumber}.isRegex`)}>
+						Use regex pattern
+					</Checkbox>
+					<Tooltip label="Regular Expression (regex) is a sequence of characters that specifies a match pattern in text.">
+						<Icon name="Info" size="16" marginLeft="5" />
+					</Tooltip>
+					<Controller
+						name={`conditions.${conditionNumber}.value`}
+						render={({ field }) => (
+							<Input
+								{...field}
+								onChange={(e) => field.onChange(e.target.value.trimStart())}
+								label={getLabelText(isRegex, type)}
+								placeholder="*"
+							/>
+						)}
+					/>
+				</>
+			)}
+		</Card>
+	);
 };
 
 const AddPushTriggerDialog = (props: DialogProps) => {
-  const { currentTriggers, isOpen, onClose, pipelines, onSubmit, editedItem, workflows } = props;
-  const [activeStageIndex, setActiveStageIndex] = useState<0 | 1>(0);
+	const { currentTriggers, isOpen, onClose, pipelines, onSubmit, editedItem, workflows } = props;
+	const [activeStageIndex, setActiveStageIndex] = useState<0 | 1>(0);
 
-  const isEditMode = !!editedItem;
+	const isEditMode = !!editedItem;
 
-  const dialogStages: ProgressIndicatorProps['stages'] = [
-    {
-      action: activeStageIndex === 1 ? { onClick: () => setActiveStageIndex(0) } : undefined,
-      label: 'Conditions',
-    },
-    { label: 'Target' },
-  ];
+	const dialogStages: ProgressIndicatorProps['stages'] = [
+		{
+			action: activeStageIndex === 1 ? { onClick: () => setActiveStageIndex(0) } : undefined,
+			label: 'Conditions',
+		},
+		{ label: 'Target' },
+	];
 
-  const defaultValues: FormItems = useMemo(() => {
-    return {
-      conditions: [
-        {
-          isRegex: false,
-          type: 'push_branch',
-          value: '',
-        },
-      ],
-      id: crypto.randomUUID(),
-      pipelineable: '',
-      source: 'push',
-      isActive: true,
-      ...editedItem,
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [editedItem, isOpen]);
+	const defaultValues: FormItems = useMemo(() => {
+		return {
+			conditions: [
+				{
+					isRegex: false,
+					type: 'push_branch',
+					value: '',
+				},
+			],
+			id: crypto.randomUUID(),
+			pipelineable: '',
+			source: 'push',
+			isActive: true,
+			...editedItem,
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [editedItem, isOpen]);
 
-  const formMethods = useForm<FormItems>({
-    defaultValues,
-  });
+	const formMethods = useForm<FormItems>({
+		defaultValues,
+	});
 
-  const { control, register, reset, handleSubmit, watch } = formMethods;
+	const { control, register, reset, handleSubmit, watch } = formMethods;
 
-  useEffect(() => {
-    reset(defaultValues);
-  }, [reset, defaultValues, isOpen, editedItem]);
+	useEffect(() => {
+		reset(defaultValues);
+	}, [reset, defaultValues, isOpen, editedItem]);
 
-  const { append, fields, remove } = useFieldArray({
-    control,
-    name: 'conditions',
-  });
+	const { append, fields, remove } = useFieldArray({
+		control,
+		name: 'conditions',
+	});
 
-  const onFormCancel = () => {
-    onClose();
-    reset(defaultValues);
-    setActiveStageIndex(0);
-  };
+	const onFormCancel = () => {
+		onClose();
+		reset(defaultValues);
+		setActiveStageIndex(0);
+	};
 
-  const onFormSubmit = (data: FormItems) => {
-    const filteredData = data;
-    filteredData.conditions = data.conditions
-      .filter(({ type }) => !!type)
-      .map((condition) => {
-        const newCondition = { ...condition };
-        newCondition.value = newCondition.value.trim();
-        if (newCondition.value === '') {
-          newCondition.value = '*';
-        }
-        return newCondition;
-      });
-    onSubmit(isEditMode ? 'edit' : 'add', filteredData as TriggerItem);
-    onFormCancel();
-  };
+	const onFormSubmit = (data: FormItems) => {
+		const filteredData = data;
+		filteredData.conditions = data.conditions
+			.filter(({ type }) => !!type)
+			.map((condition) => {
+				const newCondition = { ...condition };
+				newCondition.value = newCondition.value.trim();
+				if (newCondition.value === '') {
+					newCondition.value = '*';
+				}
+				return newCondition;
+			});
+		onSubmit(isEditMode ? 'edit' : 'add', filteredData as TriggerItem);
+		onFormCancel();
+	};
 
-  const onAppend = () => {
-    append({
-      isRegex: false,
-      type: undefined,
-      value: '',
-    });
-  };
+	const onAppend = () => {
+		append({
+			isRegex: false,
+			type: undefined,
+			value: '',
+		});
+	};
 
-  const isConditionsUsed = checkIsConditionsUsed(currentTriggers, watch() as TriggerItem);
+	const isConditionsUsed = checkIsConditionsUsed(currentTriggers, watch() as TriggerItem);
 
-  const { pipelineable } = watch();
+	const { pipelineable } = watch();
 
-  const isPipelineableMissing = !pipelineable;
+	const isPipelineableMissing = !pipelineable;
 
-  return (
-    <FormProvider {...formMethods}>
-      <Dialog
-        as="form"
-        isOpen={isOpen}
-        onClose={onFormCancel}
-        title={isEditMode ? 'Edit trigger' : 'Add push trigger'}
-        maxWidth="480"
-        onSubmit={handleSubmit(onFormSubmit)}
-      >
-        <DialogBody>
-          <Box marginBottom="24">
-            <ProgressIndicator variant="horizontal" stages={dialogStages} activeStageIndex={activeStageIndex} />
-          </Box>
-          <Divider marginBottom="24" />
-          {activeStageIndex === 0 ? (
-            <>
-              <Text color="text/primary" textStyle="heading/h3" marginBottom="4">
-                Set up trigger conditions
-              </Text>
-              <Text color="text/secondary" marginBottom="24">
-                Configure the{' '}
-                <DefinitionTooltip label="Configure the conditions that should all be met to execute the targeted Pipeline or Workflow.">
-                  conditions
-                </DefinitionTooltip>{' '}
-                that should all be met to execute the targeted Pipeline or Workflow.
-              </Text>
-              {fields.map((item, index) => {
-                return (
-                  <ConditionCard conditionNumber={index} key={item.id}>
-                    {index > 0 && (
-                      <Button leftIconName="MinusRemove" onClick={() => remove(index)} size="sm" variant="tertiary">
-                        Remove
-                      </Button>
-                    )}
-                  </ConditionCard>
-                );
-              })}
+	return (
+		<FormProvider {...formMethods}>
+			<Dialog
+				as="form"
+				isOpen={isOpen}
+				onClose={onFormCancel}
+				title={isEditMode ? 'Edit trigger' : 'Add push trigger'}
+				maxWidth="480"
+				onSubmit={handleSubmit(onFormSubmit)}
+			>
+				<DialogBody>
+					<Box marginBottom="24">
+						<ProgressIndicator variant="horizontal" stages={dialogStages} activeStageIndex={activeStageIndex} />
+					</Box>
+					<Divider marginBottom="24" />
+					{activeStageIndex === 0 ? (
+						<>
+							<Text color="text/primary" textStyle="heading/h3" marginBottom="4">
+								Set up trigger conditions
+							</Text>
+							<Text color="text/secondary" marginBottom="24">
+								Configure the{' '}
+								<DefinitionTooltip label="Configure the conditions that should all be met to execute the targeted Pipeline or Workflow.">
+									conditions
+								</DefinitionTooltip>{' '}
+								that should all be met to execute the targeted Pipeline or Workflow.
+							</Text>
+							{fields.map((item, index) => {
+								return (
+									<ConditionCard conditionNumber={index} key={item.id}>
+										{index > 0 && (
+											<Button leftIconName="MinusRemove" onClick={() => remove(index)} size="sm" variant="tertiary">
+												Remove
+											</Button>
+										)}
+									</ConditionCard>
+								);
+							})}
 
-              <Button
-                variant="secondary"
-                leftIconName="PlusAdd"
-                width="100%"
-                onClick={onAppend}
-                isDisabled={fields.length >= Object.keys(OPTIONS_MAP).length}
-              >
-                Add condition
-              </Button>
-            </>
-          ) : (
-            <>
-              <Text color="text/primary" textStyle="heading/h3" marginBottom="4">
-                Targeted Pipeline or Workflow
-              </Text>
-              <Text color="text/secondary" marginBottom="24">
-                Select the Pipeline or Workflow you want Bitrise to run when trigger conditions are met.
-              </Text>
-              <Select placeholder="Select a Pipeline or Workflow" {...register('pipelineable')}>
-                {pipelines.length && (
-                  <optgroup label="Pipelines">
-                    {pipelines.map((p) => (
-                      <option key={p} value={`pipeline#${p}`}>
-                        {p}
-                      </option>
-                    ))}
-                  </optgroup>
-                )}
-                {workflows.length && (
-                  <optgroup label="Workflows">
-                    {workflows.map((p) => (
-                      <option key={p} value={`worfklow#${p}`}>
-                        {p}
-                      </option>
-                    ))}
-                  </optgroup>
-                )}
-              </Select>
-            </>
-          )}
-        </DialogBody>
-        <DialogFooter display="flex" justifyContent="flex-end">
-          <Button onClick={onFormCancel} variant="tertiary" marginInlineEnd="auto">
-            Cancel
-          </Button>
-          {activeStageIndex === 0 ? (
-            <Tooltip
-              isDisabled={!isConditionsUsed}
-              label="You previously added the same set of conditions for another trigger. Please check and try again."
-            >
-              <Button isDisabled={isConditionsUsed} rightIconName="ArrowRight" onClick={() => setActiveStageIndex(1)}>
-                Next
-              </Button>
-            </Tooltip>
-          ) : (
-            <>
-              <Button leftIconName="ArrowLeft" variant="secondary" onClick={() => setActiveStageIndex(0)}>
-                Previous
-              </Button>
-              <Tooltip isDisabled={!isPipelineableMissing} label="Please select a pipeline or workflow.">
-                <Button type="submit" isDisabled={isPipelineableMissing}>
-                  {isEditMode ? 'Done' : 'Add trigger'}
-                </Button>
-              </Tooltip>
-            </>
-          )}
-        </DialogFooter>
-      </Dialog>
-    </FormProvider>
-  );
+							<Button
+								variant="secondary"
+								leftIconName="PlusAdd"
+								width="100%"
+								onClick={onAppend}
+								isDisabled={fields.length >= Object.keys(OPTIONS_MAP).length}
+							>
+								Add condition
+							</Button>
+						</>
+					) : (
+						<>
+							<Text color="text/primary" textStyle="heading/h3" marginBottom="4">
+								Targeted Pipeline or Workflow
+							</Text>
+							<Text color="text/secondary" marginBottom="24">
+								Select the Pipeline or Workflow you want Bitrise to run when trigger conditions are met.
+							</Text>
+							<Select placeholder="Select a Pipeline or Workflow" {...register('pipelineable')}>
+								{pipelines.length && (
+									<optgroup label="Pipelines">
+										{pipelines.map((p) => (
+											<option key={p} value={`pipeline#${p}`}>
+												{p}
+											</option>
+										))}
+									</optgroup>
+								)}
+								{workflows.length && (
+									<optgroup label="Workflows">
+										{workflows.map((p) => (
+											<option key={p} value={`workflow#${p}`}>
+												{p}
+											</option>
+										))}
+									</optgroup>
+								)}
+							</Select>
+						</>
+					)}
+				</DialogBody>
+				<DialogFooter display="flex" justifyContent="flex-end">
+					<Button onClick={onFormCancel} variant="tertiary" marginInlineEnd="auto">
+						Cancel
+					</Button>
+					{activeStageIndex === 0 ? (
+						<Tooltip
+							isDisabled={!isConditionsUsed}
+							label="You previously added the same set of conditions for another trigger. Please check and try again."
+						>
+							<Button isDisabled={isConditionsUsed} rightIconName="ArrowRight" onClick={() => setActiveStageIndex(1)}>
+								Next
+							</Button>
+						</Tooltip>
+					) : (
+						<>
+							<Button leftIconName="ArrowLeft" variant="secondary" onClick={() => setActiveStageIndex(0)}>
+								Previous
+							</Button>
+							<Tooltip isDisabled={!isPipelineableMissing} label="Please select a pipeline or workflow.">
+								<Button type="submit" isDisabled={isPipelineableMissing}>
+									{isEditMode ? 'Done' : 'Add trigger'}
+								</Button>
+							</Tooltip>
+						</>
+					)}
+				</DialogFooter>
+			</Dialog>
+		</FormProvider>
+	);
 };
 
 export default AddPushTriggerDialog;
