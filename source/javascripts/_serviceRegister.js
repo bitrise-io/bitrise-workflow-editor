c1be04ba0e0763c1d8281c5564cd22c74288f896
Zoltán Nyikos <zoltan.nyikos@bitrise.io>
2024-04-08 15:10:59 +0200

    only show credit cost if plan is credit based
    remove hardcoded plan codes

--- source/javascripts/_serviceRegister.js
+++ source/javascripts/_serviceRegister.js
@@ -6,48 +6,44 @@ import loggerFactory from "./services/logger";
 import workflowSelectionserviceFactory from "./services/workflows-selection-service";
 import workflowSelectionStore from "./services/workflow-selection-store";
 import * as appService from "./services/app-service";
-import * as planService from "./services/plan-service";
 
 angular
-	.module("BitriseWorkflowEditor")
-	.factory("logger", [
-		"SERVICE_NAME",
-		"DATADOG_API_KEY",
-		"IS_ANALYTICS",
-		function(serviceName, token, isAnalyticsOn) {
-			return loggerFactory({
-				name: serviceName,
-				isAnalyticsOn: isAnalyticsOn,
-				clientToken: token,
-				level: "info"
-			});
-		}
-	])
-	.factory("semverService", function() {
-		return semverService;
-	})
-	.factory("StringService", function() {
-		return StringService;
-	})
-	.factory("RequestService", function(logger) {
-		return new RequestService(logger);
-	})
-	.factory("appServiceUtil", function() {
-		return appService;
-	})
-	.factory("workflowSelectionStore", function() {
-		return workflowSelectionStore;
-	})
-	.factory("workflowSelectionService", [
-		"workflowSelectionStore",
-		"$location",
-		function(selectionStore, $location) {
-			return workflowSelectionserviceFactory(selectionStore, $location);
-		}
-	])
-	.factory("reactCompatService", function() {
-		return { cachedFn: cachedFn };
-	})
-	.factory("planServiceUtil", function() {
-		return planService;
-	});
+  .module("BitriseWorkflowEditor")
+  .factory("logger", [
+    "SERVICE_NAME",
+    "DATADOG_API_KEY",
+    "IS_ANALYTICS",
+    function (serviceName, token, isAnalyticsOn) {
+      return loggerFactory({
+        name: serviceName,
+        isAnalyticsOn: isAnalyticsOn,
+        clientToken: token,
+        level: "info",
+      });
+    },
+  ])
+  .factory("semverService", function () {
+    return semverService;
+  })
+  .factory("StringService", function () {
+    return StringService;
+  })
+  .factory("RequestService", function (logger) {
+    return new RequestService(logger);
+  })
+  .factory("appServiceUtil", function () {
+    return appService;
+  })
+  .factory("workflowSelectionStore", function () {
+    return workflowSelectionStore;
+  })
+  .factory("workflowSelectionService", [
+    "workflowSelectionStore",
+    "$location",
+    function (selectionStore, $location) {
+      return workflowSelectionserviceFactory(selectionStore, $location);
+    },
+  ])
+  .factory("reactCompatService", function () {
+    return { cachedFn: cachedFn };
+  });
