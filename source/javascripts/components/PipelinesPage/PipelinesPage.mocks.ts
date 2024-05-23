ef836be1d213f7c391d2d91c2f8923c46f3553a4
Andras Eszes <andras.eszes@bitrise.io>
2024-05-23 18:04:12 +0200

    chore: remove unused $location mock

--- source/javascripts/components/PipelinesPage/PipelinesPage.mocks.ts
+++ source/javascripts/components/PipelinesPage/PipelinesPage.mocks.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { action } from '@storybook/addon-actions';
 import { BitriseYml } from './PipelinesPage.types';
 
 export const mockYml: BitriseYml = {
@@ -27,26 +25,5 @@ export const mockYml: BitriseYml = {
     wf2: {
       steps: [{ 'save-cache@1.2.0': {} }, { 'script@1.1.5': {} }, { 'restore-cache@2.4.0': {} }],
     },
-  },
-};
-
-export const mockLocation = {
-  data: {} as Record<string, string>,
-  search(key?: string, value?: string) {
-    if (key && value) {
-      mockLocation.data[key] = value;
-    }
-
-    if (!key && !value) {
-      return mockLocation.data;
-    }
-
-    action('$location.search')(key, value);
-
-    return mockLocation;
-  },
-  replace(...args: never[]) {
-    action('$location.replace')(...args);
-    return mockLocation;
   },
 };
