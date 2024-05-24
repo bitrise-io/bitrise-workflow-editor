2f63f324536e1a4e32204e3b26ee9971e21c4bab
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-05-24 15:35:36 +0200

    refactor: bitrise.yml

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,24 +1,23 @@
-import React from "react";
 import type { Preview } from "@storybook/react";
-import {Provider} from '@bitrise/bitkit'
+import { Provider } from "@bitrise/bitkit";
 
 const preview: Preview = {
-	parameters: {
-		actions: { argTypesRegex: "^on[A-Z].*" },
-		controls: {
-			matchers: {
-				color: /(background|color)$/i,
-				date: /Date$/i,
-			},
-		},
-	},
-	decorators: [
-		(Story) => (
-		<Provider>
-			<Story />
-		</Provider>
-		),
-	],
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <Provider>
+        <Story />
+      </Provider>
+    ),
+  ],
 };
 
-export default preview
+export default preview;
