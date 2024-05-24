518bc1bcf704832733e8de7b29db0976b059b51b
Andras Eszes <andras.eszes@bitrise.io>
2024-05-24 11:43:18 +0200

    fix: useSearchParam hook

--- source/javascripts/components/PipelinesPage/hooks/useSearchParams.ts
+++ source/javascripts/components/PipelinesPage/hooks/useSearchParams.ts
@@ -7,10 +7,13 @@ const useSearchParams = () => {
   const setSearchParams = (setter: (prevSearchParams: URLSearchParams) => URLSearchParams) => {
     const prevSearchParams = new URLSearchParams(window.location.hash.split('?')[1] || '');
     const nextSearchParams = setter(prevSearchParams);
+    const basePath = `${window.location.href.replace(window.location.origin, '').replace(`?${prevSearchParams}`, '')}`;
 
-    history.replace(
-      `${window.location.href.replace(window.location.origin, '').replace(prevSearchParams.toString(), nextSearchParams.toString())}`,
-    );
+    if (nextSearchParams.size === 0) {
+      history.replace(basePath);
+    } else {
+      history.replace(`${basePath}?${nextSearchParams}`);
+    }
   };
 
   return [searchParams, setSearchParams] as const;
