3a811ac0fdb9bfdef88e7f158279500ad091d045
Andras Eszes <andras.eszes@bitrise.io>
2024-05-28 11:58:58 +0200

    fix: useFeatureFlag hook

--- source/javascripts/hooks/useFeatureFlag.ts
+++ source/javascripts/hooks/useFeatureFlag.ts
@@ -5,9 +5,10 @@ const defaultValues = {
 type FeatureFlags = typeof defaultValues;
 
 const useFeatureFlag = <K extends keyof FeatureFlags>(key: K): FeatureFlags[K] => {
-  return (window.parent?.globalProps?.featureFlags?.user?.[key] ??
-    window.parent?.globalProps?.featureFlags?.account?.[key] ??
-    defaultValues[key]) as FeatureFlags[K];
+  const accountValue = window.parent?.globalProps?.featureFlags?.account?.[key];
+  const defaultValue = defaultValues[key];
+
+  return (accountValue ?? defaultValue) as FeatureFlags[K];
 };
 
 export default useFeatureFlag;
