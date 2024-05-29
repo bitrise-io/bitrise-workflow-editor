98427f27df741ad6e0d7975c121016cd2ea23631
Andras Eszes <andras.eszes@bitrise.io>
2024-05-29 14:54:08 +0200

    refactor: useSearchParams hook

--- source/javascripts/components/PipelinesPage/PipelinesPage.tsx
+++ source/javascripts/components/PipelinesPage/PipelinesPage.tsx
@@ -2,18 +2,13 @@ import { BitriseYml } from './PipelinesPage.types';
 import PipelinesCanvas from './components/PipelinesCanvas';
 
 import 'reactflow/dist/style.css';
-import SearchParamsProvider from './providers/SearchParamsProvider';
 
 type Props = {
   yml: BitriseYml;
 };
 
 const PipelinesPage = ({ yml }: Props) => {
-  return (
-    <SearchParamsProvider>
-      <PipelinesCanvas pipelines={yml.pipelines} stages={yml.stages} />
-    </SearchParamsProvider>
-  );
+  return <PipelinesCanvas pipelines={yml.pipelines} stages={yml.stages} />;
 };
 
 export default PipelinesPage;
