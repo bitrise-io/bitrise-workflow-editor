5d60c172be60ca7f27a17208dfd1c4630f02f2f4
erosdome <jozsef.eros@bitrise.io>
2024-06-20 14:15:09 +0200

    Readd announcement link to M1 deprecation banner (#1020)

--- source/javascripts/components/NotificationMessageWithLink.tsx
+++ source/javascripts/components/NotificationMessageWithLink.tsx
@@ -6,19 +6,17 @@ type NotificationStatus = 'info' | 'error' | 'success' | 'warning' | 'progress';
 type Props = {
   type: NotificationStatus;
   message: string;
-  linkUrl?: string;
-  linkText?: string;
+  linkUrl: string;
+  linkText: string;
 };
 
 const NotificationMessageWithLink: FunctionComponent<Props> = ({ message, type, linkUrl, linkText }: Props) => (
   <Notification status={type}>
     <>
       {message}{' '}
-      {linkUrl && linkText && (
-        <Link href={linkUrl} target="_blank" rel="noreferrer noopener" isUnderlined>
-          {linkText}
-        </Link>
-      )}
+      <Link href={linkUrl} target="_blank" rel="noreferrer noopener" isUnderlined>
+        {linkText}
+      </Link>
     </>
   </Notification>
 );
