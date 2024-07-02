75c406b4428c50c26afe7d9c75bf7dd5e0816c5c
Luca Kollar <luca.gelencser-kollar@bitrise.io>
2024-07-02 14:05:03 +0200

    Dialog

--- source/javascripts/components/ConfigurationYmlSource/ConfigurationYmlSourceDialog.tsx
+++ source/javascripts/components/ConfigurationYmlSource/ConfigurationYmlSourceDialog.tsx
@@ -1,4 +1,15 @@
-import { Button, Dialog, DialogBody, DialogFooter, Radio, RadioGroup, Text } from '@bitrise/bitkit';
+import { useState } from 'react';
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  Divider,
+  Notification,
+  Radio,
+  RadioGroup,
+  Text,
+} from '@bitrise/bitkit';
 import { Controller, useForm } from 'react-hook-form';
 
 type ConfigurationYmlSourceDialogProps = {
@@ -8,6 +19,8 @@ type ConfigurationYmlSourceDialogProps = {
 
 const ConfigurationYmlSourceDialog = (props: ConfigurationYmlSourceDialogProps) => {
   const { isOpen, onClose } = props;
+
+  const [source, setSource] = useState<string>();
   const { control } = useForm();
 
   return (
@@ -21,7 +34,12 @@ const ConfigurationYmlSourceDialog = (props: ConfigurationYmlSourceDialogProps) 
           name="source"
           render={({ field: { ...rest } }) => {
             return (
-              <RadioGroup {...rest}>
+              <RadioGroup
+                {...rest}
+                onChange={(value) => {
+                  setSource(value);
+                }}
+              >
                 <Radio
                   helperText="Store and manage all your configuration on bitrise.io."
                   marginBlockEnd="12"
@@ -31,9 +49,38 @@ const ConfigurationYmlSourceDialog = (props: ConfigurationYmlSourceDialogProps) 
                 </Radio>
                 <Radio
                   helperText="The configuration is stored, versioned, and maintained in your Git repository."
+                  marginBlockEnd="24"
                   value="git"
                 >
                   Git repository
+                </Radio>
+              </RadioGroup>
+            );
+          }}
+        />
+        {source && (
+          <Notification status="progress">Looking for a configuration file in the app’s repository.</Notification>
+        )}
+        <Divider marginY="24" />
+        <Text textStyle="heading/h3" marginBlockEnd="4">
+          Set configuration file
+        </Text>
+        <Text marginBlockEnd="24">Choose which configuration file should be used on bitrise.io from now.</Text>
+        <Controller
+          control={control}
+          name="configuration-file"
+          render={({ field: { ...rest } }) => {
+            return (
+              <RadioGroup {...rest}>
+                <Radio
+                  helperText="Multiple configuration files will be merged into a single file. Learn more"
+                  marginBlockEnd="12"
+                  value="git"
+                >
+                  Use the configuration file stored in the Git repository
+                </Radio>
+                <Radio helperText="Last updated: May 12, 2024" value="bitrise">
+                  Use the last version you stored on bitrise.io
                 </Radio>
               </RadioGroup>
             );
