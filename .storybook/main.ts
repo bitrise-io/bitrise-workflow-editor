7cae5137d46d9d9b581b73b4ea20ea98d96527e7
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-07-10 20:33:23 +0200

    chore: add path aliasing

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,60 +1,76 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
+const path = require("path");
+
 const config: StorybookConfig = {
-	stories: ["../source/**/*.stories.tsx"],
-	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-onboarding",
-		"@storybook/addon-interactions",
-		"@storybook/addon-webpack5-compiler-swc"
-	],
-	framework: {
-		name: "@storybook/react-webpack5",
-		options: {
-			builder: {
-				fastRefresh: true
-			},
-		},
-	},
-	swc: () => ({
-		jsc: {
-			transform: {
-				react: {
-					runtime: 'automatic',
-				}
-			},
-		},
-	}),
-	docs: {
-		autodocs: false
-	},
-	refs: {
-		'@chakra-ui/react': { disable: true }
-	},
-	webpackFinal: async (config) => {
-		if (config.module?.rules) {
-			config.module.rules.push({
-				test: /.*\/bitkit\/.*tsx?$/,
-				use: [
-					{
-						loader: 'ts-loader',
-						options: { transpileOnly: true, configFile: require.resolve('@bitrise/bitkit/src/tsconfig.json') },
-					},
-				],
-			});
-		}
-		return config;
-	},
-	typescript: {
-		reactDocgen: false,
-	},
-	staticDirs: [
-		{
-			from: './public',
-			to: '/'
-		}
-	]
+  stories: ["../source/**/*.stories.tsx"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    "@storybook/addon-webpack5-compiler-swc",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {
+      builder: {
+        fastRefresh: true,
+      },
+    },
+  },
+  swc: () => ({
+    jsc: {
+      transform: {
+        react: {
+          runtime: "automatic",
+        },
+      },
+    },
+  }),
+  docs: {
+    autodocs: false,
+  },
+  refs: {
+    "@chakra-ui/react": { disable: true },
+  },
+  webpackFinal: async (config) => {
+    if (config.module?.rules) {
+      config.module.rules.push({
+        test: /.*\/bitkit\/.*tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+              configFile: require.resolve("@bitrise/bitkit/src/tsconfig.json"),
+            },
+          },
+        ],
+      });
+    }
+
+    const resolve = config.resolve || {};
+    const alias = resolve.alias || {};
+    config.resolve = {
+      ...resolve,
+      alias: {
+        ...alias,
+        "@": path.resolve(__dirname, "../source/javascripts"),
+      },
+    };
+
+    return config;
+  },
+  typescript: {
+    reactDocgen: false,
+  },
+  staticDirs: [
+    {
+      from: "./public",
+      to: "/",
+    },
+  ],
 };
 
 export default config;
