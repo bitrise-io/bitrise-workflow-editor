0deb9753c7b4ed68b794691f31cbd84bcabf8f54
Zoltan Szabo <63643463+zoltanszabo-bitrise@users.noreply.github.com>
2024-07-12 13:17:31 +0200

    fix: Add Cancel button to Run Workflow dialog (#1068)
    * chore: move pages and their components to their respective directory
    
    * fix: Add Cancel button to Run Workflow Dialog
    
    * fix: component registration to Angular

---
+++ source/javascripts/pages/WorkflowsPage/components/WorkflowConfigPanel/WorkflowConfigPanel.stories.tsx
@@ -0,0 +1,91 @@
+import { Meta, StoryObj } from '@storybook/react';
+import WorkflowConfigPanel from './WorkflowConfigPanel';
+import { mockGetAllStackInfo, mockGetMachineTypeConfigs } from './WorkflowConfigPanel.mswMocks';
+import { mockYml } from '@/pages/PipelinesPage/PipelinesPage.mocks';
+
+export default {
+  component: WorkflowConfigPanel,
+  args: {
+    yml: mockYml,
+    appSlug: 'app-1',
+    defaultValues: {
+      workflowId: 'wf1',
+      defaultStackId: 'osx-xcode-15.0.x',
+      defaultMachineTypeId: 'g2-m1.8core',
+      configuration: {
+        stackId: '',
+        machineTypeId: '',
+      },
+      isMachineTypeSelectorAvailable: true,
+    },
+  },
+  argTypes: {
+    onChange: {
+      type: 'function',
+    },
+  },
+  parameters: {
+    msw: {
+      handlers: [mockGetAllStackInfo(), mockGetMachineTypeConfigs()],
+    },
+  },
+} as Meta<typeof WorkflowConfigPanel>;
+
+type Story = StoryObj<typeof WorkflowConfigPanel>;
+
+export const Default: Story = {};
+
+export const WithOverrides: Story = {
+  args: {
+    defaultValues: {
+      workflowId: 'workflow-1',
+      defaultStackId: 'osx-xcode-15.0.x',
+      defaultMachineTypeId: 'g2-m1.8core',
+      isMachineTypeSelectorAvailable: true,
+      configuration: {
+        stackId: 'linux-docker-android-22.04',
+        machineTypeId: 'elite-xl',
+        envs: [
+          { key: 'FOO', value: 'Bar', isExpand: true },
+          { key: 'HELLO', value: 'World', isExpand: false },
+        ],
+      },
+      properties: {
+        title: 'First Workflow',
+        summary: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente unde consectetur minus.',
+        description:
+          'Ipsum, molestias. Incidunt, accusamus nemo! Totam nulla quia, sapiente ut facere neque doloremque ad, quis velit, debitis dicta! Quidem consequatur commodi eligendi!',
+      },
+    },
+  },
+};
+
+export const WithoutAppSlug: Story = {
+  args: {
+    appSlug: undefined,
+  },
+};
+
+export const WithDedicatedMachine: Story = {
+  args: {
+    appSlug: 'app-1',
+    defaultValues: {
+      workflowId: 'workflow-1',
+      defaultStackId: 'osx-xcode-15.0.x',
+      defaultMachineTypeId: 'g2-m1.8core',
+      isMachineTypeSelectorAvailable: false,
+    },
+  },
+};
+
+export const WithSelfHostedRunner: Story = {
+  args: {
+    appSlug: 'app-1',
+    defaultValues: {
+      workflowId: 'workflow-1',
+      defaultStackId: 'osx-xcode-15.0.x',
+      defaultMachineTypeId: undefined,
+      isMachineTypeSelectorAvailable: true,
+    },
+  },
+};
