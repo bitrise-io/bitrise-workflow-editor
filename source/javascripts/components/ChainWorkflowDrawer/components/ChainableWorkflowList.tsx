428282ec570ccc7aa7edd2c9a22e4643022f5297
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-07-12 09:25:06 +0200

    fix: chain workflow drawer search focus cutoff

--- source/javascripts/components/ChainWorkflowDrawer/components/ChainableWorkflowList.tsx
+++ source/javascripts/components/ChainWorkflowDrawer/components/ChainableWorkflowList.tsx
@@ -50,7 +50,7 @@ const ChainableWorkflowList = ({ id: workflowId, onChainWorkflow }: Props) => {
   }
 
   return (
-    <Box display="flex" flexDir="column" gap="12">
+    <Box display="flex" flexDir="column" gap="12" overflow="auto">
       {workflows.map((chainableId) => (
         <ChainableWorkflowCard key={chainableId} workflowId={chainableId} onChainWorkflow={onChainWorkflow} />
       ))}
