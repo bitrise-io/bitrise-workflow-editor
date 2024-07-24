5ff5dd3145b5f4cc1f02a7295f486207037396d0
Andras Eszes <andras.eszes@bitrise.io>
2024-07-24 17:31:29 +0200

    feat: add step drawer to the new workflows page layout

--- source/javascripts/pages/WorkflowsPage/WorkflowsPage.store.ts
+++ source/javascripts/pages/WorkflowsPage/WorkflowsPage.store.ts
@@ -1,11 +1,12 @@
 import { create } from 'zustand';
 
 type State = {
-  isDialogOpen?: 'create-workflow' | 'chain-workflow' | 'delete-workflow';
+  isDialogOpen?: 'create-workflow' | 'chain-workflow' | 'delete-workflow' | 'step-drawer';
 };
 
 type Action = {
   closeDialog: VoidFunction;
+  openStepDrawer: VoidFunction;
   openChainWorkflowDialog: VoidFunction;
   openCreateWorkflowDialog: VoidFunction;
   openDeleteWorkflowDialog: VoidFunction;
@@ -14,6 +15,7 @@ type Action = {
 export const useWorkflowsPageStore = create<State & Action>((set) => ({
   isDialogOpen: undefined,
   closeDialog: () => set(() => ({ isDialogOpen: undefined })),
+  openStepDrawer: () => set(() => ({ isDialogOpen: 'step-drawer' })),
   openChainWorkflowDialog: () => set(() => ({ isDialogOpen: 'chain-workflow' })),
   openCreateWorkflowDialog: () => set(() => ({ isDialogOpen: 'create-workflow' })),
   openDeleteWorkflowDialog: () => set(() => ({ isDialogOpen: 'delete-workflow' })),
