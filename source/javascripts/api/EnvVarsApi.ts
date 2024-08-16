0b3881ac54f99b7790bf20bdc704cc023933abde
Andras Eszes <andras.eszes@bitrise.io>
2024-08-16 16:11:15 +0200

    feat: load certificate env vars

--- source/javascripts/api/EnvVarsApi.ts
+++ source/javascripts/api/EnvVarsApi.ts
@@ -4,6 +4,12 @@ import Client from './client';
 const DEFAULT_OUTPUTS_PATH = '/api/app/:appSlug/default_step_outputs.json';
 const DEFAULT_OUTPUTS_PATH_LOCAL = '/api/default-outputs';
 
+enum Source {
+  BitriseIO = 'bitrise.io',
+  BitriseCLI = 'bitrise CLI',
+  CodeSigning = 'code signing files',
+}
+
 const defaultEnvVar: EnvVar = {
   key: '',
   value: '',
@@ -40,11 +46,11 @@ async function getDefaultOutputs({ appSlug, signal }: GetEnvVarsProps): Promise<
   const envVars: EnvVar[] = [];
 
   response.from_bitrise_cli.forEach((envVarObj) => {
-    envVars.push({ ...defaultEnvVar, key: Object.keys(envVarObj)[0], source: 'bitrise CLI' });
+    envVars.push({ ...defaultEnvVar, key: Object.keys(envVarObj)[0], source: Source.BitriseCLI });
   });
 
   response.from_bitriseio?.forEach((envVarObj) => {
-    envVars.push({ ...defaultEnvVar, key: Object.keys(envVarObj)[0], source: 'bitrise.io' });
+    envVars.push({ ...defaultEnvVar, key: Object.keys(envVarObj)[0], source: Source.BitriseIO });
   });
 
   return envVars;
@@ -74,11 +80,48 @@ async function getProvProfiles({ appSlug, signal, projectType }: GetEnvVarsStric
   const envVars: EnvVar[] = [];
 
   if (response.prov_profile_documents.length > 0) {
-    envVars.push({ ...defaultEnvVar, key: 'BITRISE_PROVISION_URL', source: 'code signing files' });
+    envVars.push({ ...defaultEnvVar, key: 'BITRISE_PROVISION_URL', source: Source.CodeSigning });
   }
 
   if (projectType !== 'xamarin') {
-    envVars.push({ ...defaultEnvVar, key: 'BITRISE_DEFAULT_PROVISION_URL', source: 'code signing files' });
+    envVars.push({ ...defaultEnvVar, key: 'BITRISE_DEFAULT_PROVISION_URL', source: Source.CodeSigning });
+  }
+
+  return envVars;
+}
+
+const CERTIFICATES_PATH = '/api/app/:appSlug/build_certificate/show.json';
+
+type CertificatesResponse = {
+  build_certificates: Array<{
+    id: string;
+    processed: boolean;
+    is_expose: boolean;
+    is_protected: boolean;
+    upload_file_name: string;
+    certificate_password: string;
+  }>;
+};
+
+function getCertificatesPath(appSlug: string) {
+  return CERTIFICATES_PATH.replace(':appSlug', appSlug);
+}
+
+async function getCertificates({ appSlug, signal, projectType }: GetEnvVarsStrictProps): Promise<EnvVar[]> {
+  const response = await Client.get<CertificatesResponse>(getCertificatesPath(appSlug), {
+    signal,
+  });
+
+  const envVars: EnvVar[] = [];
+
+  if (response.build_certificates.length > 0) {
+    envVars.push({ ...defaultEnvVar, key: 'BITRISE_CERTIFICATE_URL', source: Source.CodeSigning });
+    envVars.push({ ...defaultEnvVar, key: 'BITRISE_CERTIFICATE_PASSPHRASE', source: Source.CodeSigning });
+  }
+
+  if (projectType !== 'xamarin') {
+    envVars.push({ ...defaultEnvVar, key: 'BITRISE_DEFAULT_CERTIFICATE_URL', source: Source.CodeSigning });
+    envVars.push({ ...defaultEnvVar, key: 'BITRISE_DEFAULT_CERTIFICATE_PASSPHRASE', source: Source.CodeSigning });
   }
 
   return envVars;
@@ -91,6 +134,7 @@ async function getEnvVars({ appSlug, signal, projectType }: GetEnvVarsProps): Pr
   if (appSlug) {
     const strictProps = { appSlug, signal, projectType };
     promises.push(getProvProfiles(strictProps));
+    promises.push(getCertificates(strictProps));
   }
 
   return Promise.all(promises).then((results) => results.flatMap((v) => v));
