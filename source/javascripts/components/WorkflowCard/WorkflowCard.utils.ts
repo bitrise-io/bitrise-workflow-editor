a41c06bfc70b60ee0fbc6b126dd2a90a84c222f9
Andras Eszes <andras.eszes@bitrise.io>
2024-08-22 14:24:14 +0200

    chore: remove some unused functions

--- source/javascripts/components/WorkflowCard/WorkflowCard.utils.ts
+++ source/javascripts/components/WorkflowCard/WorkflowCard.utils.ts
@@ -1,5 +1,3 @@
-import { UniqueIdentifier } from '@dnd-kit/core';
-
 export function getUsedByText(usedBy: string[]) {
   switch (usedBy.length) {
     case 0:
@@ -9,21 +7,4 @@ export function getUsedByText(usedBy: string[]) {
     default:
       return `used by ${usedBy.length} Workflows`;
   }
-}
-
-export function getSortableStepId(workflowId: string, stepIndex: number) {
-  return `${workflowId}->${stepIndex}`;
-}
-
-export function parseSortableStepId(sortableStepId: string) {
-  const [workflowId, stepIndexAsString] = sortableStepId.split('->');
-
-  return {
-    workflowId,
-    stepIndex: Number(stepIndexAsString),
-  };
-}
-
-export function isSortableStepId(id: UniqueIdentifier): boolean {
-  return /^([A-Za-z0-9_]+)->([0-9]+)$/g.test(id.toString());
 }
