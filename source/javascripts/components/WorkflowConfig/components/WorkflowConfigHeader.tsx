3e3154e6896dbb3ed0ba7c45d60648da5c9f0b64
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-08-27 20:37:04 +0200

    fix: use WorkflowService.getUsedByText

--- source/javascripts/components/WorkflowConfig/components/WorkflowConfigHeader.tsx
+++ source/javascripts/components/WorkflowConfig/components/WorkflowConfigHeader.tsx
@@ -1,18 +1,8 @@
 import { Box, Tab, TabList, Text } from '@bitrise/bitkit';
+import WorkflowService from '@/core/models/WorkflowService';
 import { useWorkflowConfigContext } from '../WorkflowConfig.context';
 import useWorkflowUsedBy from '../hooks/useWorkflowUsedBy';
 import { WorkflowConfigTab } from '../WorkflowConfig.types';
-
-const getUsedByText = (usedBy: string[]) => {
-  switch (usedBy.length) {
-    case 0:
-      return 'Not used by other Workflow';
-    case 1:
-      return 'Used by 1 Workflow';
-    default:
-      return `Used by ${usedBy.length.toString()} Workflows`;
-  }
-};
 
 const WorkflowConfigHeader = () => {
   const result = useWorkflowConfigContext();
@@ -25,7 +15,7 @@ const WorkflowConfigHeader = () => {
           {result?.userValues.title || result?.id || 'Workflow'}
         </Text>
         <Text textStyle="body/sm/regular" color="text/secondary">
-          {getUsedByText(dependants)}
+          {WorkflowService.getUsedByText(dependants)}
         </Text>
       </Box>
       <Box position="relative" mt="8">
