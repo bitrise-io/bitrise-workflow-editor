6a88bfba438ca493cf079801fd97254ea9b751a1
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-09-04 18:53:55 +0200

    chore: fix jest svg import problem

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,12 @@ module.exports = {
   rootDir: '.',
   roots: ['./source'],
   testEnvironment: 'node',
-  transform: { '^.+.tsx?$': 'ts-jest' },
-  moduleNameMapper: { '@/(.*)': '<rootDir>/source/javascripts/$1' },
+  transform: {
+    '^.+.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|svg)$': 'identity-obj-proxy',
+    '@/(.*)': '<rootDir>/source/javascripts/$1',
+  },
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
 };
