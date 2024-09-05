20bc2efabe605c61a5ea33a818570edf57cacc5e
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-09-05 13:06:31 +0200

    refactor: SecretsPage

--- source/javascripts/models.ts
+++ source/javascripts/models.ts
@@ -82,20 +82,3 @@ export interface InputCategory {
   name: string;
   inputs: Variable[];
 }
-
-export type Secret = {
-  key: string;
-  value: string;
-  isProtected: boolean;
-  isExpand: boolean;
-  isExpose: boolean;
-  isKeyChangeable: boolean;
-  isSaved?: boolean;
-  isEditing?: boolean;
-  isShared?: boolean;
-};
-
-export type SecretWithState = Secret & {
-  isEditing: boolean;
-  isSaved?: boolean;
-};
