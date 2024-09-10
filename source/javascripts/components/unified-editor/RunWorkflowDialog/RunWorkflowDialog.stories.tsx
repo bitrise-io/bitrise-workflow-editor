d05bf699c3004c1cc7dd7042fc2d8e575ba13023
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-09-10 16:04:11 +0200

    feat: change RunWorkflowDialog implementation

--- source/javascripts/components/unified-editor/RunWorkflowDialog/RunWorkflowDialog.stories.tsx
+++ source/javascripts/components/unified-editor/RunWorkflowDialog/RunWorkflowDialog.stories.tsx
@@ -1,20 +1,37 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta } from '@storybook/react';
+import BuildApiMocks from '@/core/api/BuildApi.mswMocks';
 import RunWorkflowDialog from './RunWorkflowDialog';
 
 export default {
   component: RunWorkflowDialog,
   args: {
     isOpen: true,
-    defaultBranch: 'master',
     workflowId: 'primary-workflow',
   },
   argTypes: {
-    isOpen: { type: 'boolean', control: { type: 'boolean' } },
-    defaultBranch: { type: 'string' },
     workflowId: { type: 'string' },
+    isOpen: { type: 'boolean', control: { type: 'boolean' } },
     onClose: { type: 'function' },
-    onAction: { type: 'function' },
+  },
+  before: () => {
+    window.parent.pageProps = {
+      project: {
+        slug: 'asd-123',
+        name: 'Mock Project',
+        defaultBranch: 'main',
+      },
+    };
   },
 } as Meta<typeof RunWorkflowDialog>;
 
-export const Default: StoryObj<typeof RunWorkflowDialog> = {};
+export const Default = {
+  parameters: {
+    msw: { handlers: [BuildApiMocks.startBuild('success')] },
+  },
+};
+
+export const Error = {
+  parameters: {
+    msw: [BuildApiMocks.startBuild('error')],
+  },
+};
