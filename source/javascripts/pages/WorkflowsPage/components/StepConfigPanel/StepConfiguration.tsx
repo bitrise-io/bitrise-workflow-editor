1bb9dd2abc7dd1e350766e65638f4167ef495550
Zoltan Szabo <63643463+zoltanszabo-bitrise@users.noreply.github.com>
2024-09-13 10:26:19 +0200

    Run if improvements (#1156)
    * fix: texts improvement
    
    * feat: add is_skippable to UI

--- source/javascripts/pages/WorkflowsPage/components/StepConfigPanel/StepConfiguration.tsx
+++ source/javascripts/pages/WorkflowsPage/components/StepConfigPanel/StepConfiguration.tsx
@@ -50,8 +50,13 @@ const StepConfiguration = ({ step, inputCategories, onChange }: StepConfiguratio
           buttonContent={<Text fontWeight="demiBold">When to run</Text>}
         >
           <Box display="flex">
-            <Text flex="1">Run if previous Step(s) failed</Text>
+            <Text flex="1">Run even if previous Steps failed</Text>
             <Toggle {...form.register('is_always_run')} defaultChecked={step.isAlwaysRun()} />
+          </Box>
+          <Divider my="24" />
+          <Box display="flex">
+            <Text flex="1">Continue build even if this Step fails</Text>
+            <Toggle {...form.register('is_skippable')} defaultChecked={step.isSkippable()} />
           </Box>
           <Divider my="24" />
           <StepInput
