36418b8a428807b3305ba17ea0bb612b8a651a14
Zoltan Szabo <63643463+zoltanszabo-bitrise@users.noreply.github.com>
2024-09-18 11:37:15 +0200

    fix: normalized version should always be based upon CVS version (#1177)

--- source/javascripts/core/api/StepApi.ts
+++ source/javascripts/core/api/StepApi.ts
@@ -54,7 +54,7 @@ function toStep(
 
   const title = StepService.resolveTitle(response.cvs || cvs, response.step);
   const icon = StepService.resolveIcon(response.step, response.info);
-  const normalizedVersion = VersionUtils.normalizeVersion(version) || VersionUtils.normalizeVersion(response.version);
+  const normalizedVersion = VersionUtils.normalizeVersion(version);
   const resolvedVersion = response.version || VersionUtils.resolveVersion(version, versions);
   const latestVersion = response.latest_version_number || VersionUtils.resolveVersion('', versions);
   const isLatest = Boolean(response.is_latest);
