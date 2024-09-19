6d330a349e656f4cf2c87221e690a67020b1881a
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-09-19 15:56:03 +0200

    fix: move the delete workflow button to the canvas

--- source/javascripts/components/unified-editor/WorkflowConfig/components/WorkflowConfigHeader.tsx
+++ source/javascripts/components/unified-editor/WorkflowConfig/components/WorkflowConfigHeader.tsx
@@ -1,22 +1,35 @@
-import { Box, Tab, TabList, Text } from '@bitrise/bitkit';
+import { Box, IconButton, Tab, TabList, Text } from '@bitrise/bitkit';
 import WorkflowService from '@/core/models/WorkflowService';
 import useDependantWorkflows from '@/hooks/useDependantWorkflows';
+import { useWorkflowsPageStore } from '@/pages/WorkflowsPage/WorkflowsPage.store';
 import { useWorkflowConfigContext } from '../WorkflowConfig.context';
 import { WorkflowConfigTab } from '../WorkflowConfig.types';
 
 const WorkflowConfigHeader = () => {
   const { id, userValues } = useWorkflowConfigContext() ?? { id: '' };
   const dependants = useDependantWorkflows(id);
+  const { openDeleteWorkflowDialog } = useWorkflowsPageStore();
 
   return (
     <>
-      <Box px="24" py="16">
-        <Text as="h3" textStyle="heading/h3">
-          {userValues?.title || id || 'Workflow'}
-        </Text>
-        <Text textStyle="body/sm/regular" color="text/secondary">
-          {WorkflowService.getUsedByText(dependants)}
-        </Text>
+      <Box px="24" py="16" display="flex" justifyContent="space-between" alignItems="center">
+        <Box>
+          <Text as="h3" textStyle="heading/h3">
+            {userValues?.title || id || 'Workflow'}
+          </Text>
+          <Text textStyle="body/sm/regular" color="text/secondary">
+            {WorkflowService.getUsedByText(dependants)}
+          </Text>
+        </Box>
+        <IconButton
+          isDanger
+          size="md"
+          variant="secondary"
+          iconName="Trash"
+          aria-label={`Delete '${id}'`}
+          tooltipProps={{ 'aria-label': `Delete '${id}'` }}
+          onClick={openDeleteWorkflowDialog}
+        />
       </Box>
       <Box position="relative" mt="8">
         <TabList paddingX="8">
