09ece3990a9a4693a0d3a6f5653229488a3ab22e
Laszlo Moczo <laszlo.moczo@bitrise.io>
2024-10-24 14:58:39 +0200

    FRONTEND-487

--- source/javascripts/components/unified-editor/WorkflowConfig/WorkflowConfigPanel.tsx
+++ source/javascripts/components/unified-editor/WorkflowConfig/WorkflowConfigPanel.tsx
@@ -1,27 +1,58 @@
-import { TabPanel, TabPanels, Tabs } from '@bitrise/bitkit';
+import { useEffect } from 'react';
+import { TabPanel, TabPanels, Tabs, useTabs } from '@bitrise/bitkit';
 import TriggersTabPanel from '@/pages/WorkflowsPage/components/WorkflowConfigPanel/components/TriggersTabPanel';
 import useFeatureFlag from '@/hooks/useFeatureFlag';
+import useSearchParams from '@/hooks/useSearchParams';
 import WorkflowConfigHeader from './components/WorkflowConfigHeader';
 import ConfigurationTab from './tabs/ConfigurationTab';
 import PropertiesTab from './tabs/PropertiesTab';
 import WorkflowConfigProvider from './WorkflowConfig.context';
 import { WorkflowConfigTab } from './WorkflowConfig.types';
 
+const TAB_IDS = [WorkflowConfigTab.CONFIGURATION, WorkflowConfigTab.PROPERTIES, WorkflowConfigTab.TRIGGERS];
+
 const WorkflowConfigPanelContent = () => {
   const isTargetBasedTriggersEnabled = useFeatureFlag('enable-target-based-triggers');
 
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const { setTabIndex, tabIndex } = useTabs<WorkflowConfigTab>({
+    defaultId: (searchParams.get as WorkflowConfigTab) || WorkflowConfigTab.CONFIGURATION,
+    tabIds: TAB_IDS,
+  });
+
+  const onTabChange = (index: number) => {
+    setSearchParams({
+      ...searchParams,
+      tab: TAB_IDS[index],
+    });
+  };
+
+  useEffect(() => {
+    if (searchParams.tab) {
+      setTabIndex(TAB_IDS.indexOf(searchParams.tab as WorkflowConfigTab));
+    }
+  }, [searchParams, setTabIndex]);
+
   return (
-    <Tabs display="flex" flexDir="column" borderLeft="1px solid" borderColor="border/regular">
+    <Tabs
+      display="flex"
+      flexDir="column"
+      borderLeft="1px solid"
+      borderColor="border/regular"
+      index={tabIndex}
+      onChange={onTabChange}
+    >
       <WorkflowConfigHeader variant="panel" />
       <TabPanels flex="1" minH="0">
-        <TabPanel id={WorkflowConfigTab.CONFIGURATION} p="24" overflowY="auto" h="100%">
+        <TabPanel p="24" overflowY="auto" h="100%">
           <ConfigurationTab />
         </TabPanel>
-        <TabPanel id={WorkflowConfigTab.PROPERTIES} p="24" overflowY="auto" h="100%">
+        <TabPanel p="24" overflowY="auto" h="100%">
           <PropertiesTab variant="panel" />
         </TabPanel>
         {isTargetBasedTriggersEnabled && (
-          <TabPanel id={WorkflowConfigTab.TRIGGERS} overflowY="auto" h="100%">
+          <TabPanel overflowY="auto" h="100%">
             <TriggersTabPanel />
           </TabPanel>
         )}
