fb8c14f198e36962d1f6a68bf9aaeaeac137f53e
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-10-25 13:00:33 +0200

    feat: implement UpgradePlanEmptyState

---
+++ source/javascripts/pages/PipelinesPage/components/UpgradePlanEmptyState.stories.tsx
@@ -0,0 +1,8 @@
+import { Meta, StoryObj } from '@storybook/react';
+import UpgradePlanEmptyState from './UpgradePlanEmptyState';
+
+export default {
+  component: UpgradePlanEmptyState,
+} as Meta<typeof UpgradePlanEmptyState>;
+
+export const UpgradePlan: StoryObj<typeof UpgradePlanEmptyState> = {};
