a54b768f3448c64f23025e526faa83bb54f218bb
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-10-25 12:57:46 +0200

    feat: implement CreateFirstGraphPipelineEmptyState

---
+++ source/javascripts/pages/PipelinesPage/components/CreateFirstGraphPipelineEmptyState.stories.tsx
@@ -0,0 +1,8 @@
+import { Meta, StoryObj } from '@storybook/react';
+import CreateFirstGraphPipelineEmptyState from './CreateFirstGraphPipelineEmptyState';
+
+export default {
+  component: CreateFirstGraphPipelineEmptyState,
+} as Meta<typeof CreateFirstGraphPipelineEmptyState>;
+
+export const CreateFirstGraphPipeline: StoryObj<typeof CreateFirstGraphPipelineEmptyState> = {};
