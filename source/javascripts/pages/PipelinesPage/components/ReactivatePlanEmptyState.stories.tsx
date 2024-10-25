8072c0d722b2f300d5a7d7d8fe3202ef7f62dfd2
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-10-25 13:07:12 +0200

    feat: implement ReactivatePlanEmptyState

---
+++ source/javascripts/pages/PipelinesPage/components/ReactivatePlanEmptyState.stories.tsx
@@ -0,0 +1,8 @@
+import { Meta, StoryObj } from '@storybook/react';
+import ReactivatePlanEmptyState from '@/pages/PipelinesPage/components/ReactivatePlanEmptyState';
+
+export default {
+  component: ReactivatePlanEmptyState,
+} as Meta<typeof ReactivatePlanEmptyState>;
+
+export const ReactivatePlan: StoryObj<typeof ReactivatePlanEmptyState> = {};
