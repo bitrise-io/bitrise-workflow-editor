675bdc090113b2c9ae6a57fef968f708a6db880d
Andras Eszes <andras.eszes@bitrise.io>
2024-10-31 16:58:34 +0100

    fix: workflow card empty state & utility workflow stack and machine

--- source/javascripts/components/unified-editor/WorkflowConfig/tabs/ConfigurationTab.tsx
+++ source/javascripts/components/unified-editor/WorkflowConfig/tabs/ConfigurationTab.tsx
@@ -1,12 +1,17 @@
 import { Box } from '@bitrise/bitkit';
 import RuntimeUtils from '@/core/utils/RuntimeUtils';
+import WorkflowService from '@/core/models/WorkflowService';
 import EnvVarsCard from '../components/EnvVarsCard';
 import StackAndMachineCard from '../components/StackAndMachineCard';
+import { useWorkflowConfigContext } from '../WorkflowConfig.context';
 
 const ConfigurationTab = () => {
+  const workflow = useWorkflowConfigContext();
+  const isUtilityWorkflow = WorkflowService.isUtilityWorkflow(workflow?.id || '');
+
   return (
     <Box display="flex" flexDir="column" gap="24">
-      {RuntimeUtils.isWebsiteMode() && <StackAndMachineCard />}
+      {RuntimeUtils.isWebsiteMode() && !isUtilityWorkflow && <StackAndMachineCard />}
       <EnvVarsCard />
     </Box>
   );
