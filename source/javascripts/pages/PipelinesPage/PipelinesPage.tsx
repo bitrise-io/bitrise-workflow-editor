dacbbe6331262bd882e3791680e3a64573f2aa30
Gábor Szakács <gaborszakacs@users.noreply.github.com>
2024-10-31 15:34:11 +0100

    Use graph specific empty pipelines page if applicable (#1241)

--- source/javascripts/pages/PipelinesPage/PipelinesPage.tsx
+++ source/javascripts/pages/PipelinesPage/PipelinesPage.tsx
@@ -2,9 +2,13 @@ import 'reactflow/dist/style.css';
 import { Box } from '@bitrise/bitkit';
 import { BitriseYml } from '@/core/models/BitriseYml';
 import BitriseYmlProvider from '@/contexts/BitriseYmlProvider';
+import useFeatureFlag from '@/hooks/useFeatureFlag';
+import usePipelineSelector from './hooks/usePipelineSelector';
 import StagePipelineEmptyState from './components/PipelineCanvas/StagedPipelineCanvas/components/StagePipelineEmptyState';
 import PipelineCanvas from './components/PipelineCanvas/PipelineCanvas';
 import Drawers from './components/Drawers/Drawers';
+import { usePipelinesPageStore, PipelineConfigDialogType } from './PipelinesPage.store';
+import CreateFirstGraphPipelineEmptyState from './components/PipelineCanvas/GraphPipelineCanvas/components/CreateFirstGraphPipelineEmptyState';
 
 type Props = {
   yml: BitriseYml;
@@ -16,17 +20,34 @@ const PipelinesPage = ({ yml, onChange }: Props) => {
     return null;
   }
 
-  const hasPipelines = Object.keys(yml.pipelines || {}).length > 0;
-
   return (
     <BitriseYmlProvider yml={yml} onChange={onChange}>
-      <Drawers>
-        <Box display="flex" flexDir="column" h="100%">
-          {hasPipelines ? <PipelineCanvas /> : <StagePipelineEmptyState />}
-        </Box>
-      </Drawers>
+      <PipelinesPageContent />
     </BitriseYmlProvider>
   );
+};
+
+const PipelinesPageContent = () => {
+  const { keys } = usePipelineSelector();
+  const hasPipelines = keys.length > 0;
+
+  return (
+    <Drawers>
+      <Box display="flex" flexDir="column" h="100%">
+        {hasPipelines ? <PipelineCanvas /> : <EmptyState />}
+      </Box>
+    </Drawers>
+  );
+};
+
+const EmptyState = () => {
+  const isGraphPipelinesEnabled = useFeatureFlag('enable-dag-pipelines');
+  const { openDialog } = usePipelinesPageStore();
+
+  if (isGraphPipelinesEnabled) {
+    return <CreateFirstGraphPipelineEmptyState onCreate={openDialog(PipelineConfigDialogType.CREATE_PIPELINE)} />;
+  }
+  return <StagePipelineEmptyState />;
 };
 
 export default PipelinesPage;
