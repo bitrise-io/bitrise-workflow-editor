079d0a670cb772bc4d8b3808efd05fed93802d5f
Andras Eszes <andras.eszes@bitrise.io>
2024-11-04 10:17:31 +0100

    chore: cleanup DeletePipelineDialog stories

--- source/javascripts/pages/PipelinesPage/components/PipelineConfigDrawer/components/DeletePipelineDialog/DeletePipelineDialog.stories.tsx
+++ source/javascripts/pages/PipelinesPage/components/PipelineConfigDrawer/components/DeletePipelineDialog/DeletePipelineDialog.stories.tsx
@@ -1,24 +1,16 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { withBitriseYml } from '@/contexts/BitriseYmlProvider';
-import { MockYml } from '@/core/models/BitriseYml.mocks';
 import DeletePipelineDialog from './DeletePipelineDialog';
 
 export default {
   component: DeletePipelineDialog,
   args: {
     isOpen: true,
+    pipelineId: 'graph-pipeline',
   },
   argTypes: {
-    isOpen: { type: 'boolean' },
     onClose: { type: 'function' },
     onDeletePipeline: { type: 'function' },
   },
-  parameters: {
-    query: {
-      pipeline_id: 'pl-1',
-    },
-  },
-  decorators: (Story) => withBitriseYml(MockYml, Story),
 } as Meta<typeof DeletePipelineDialog>;
 
 export const Default: StoryObj = {};
