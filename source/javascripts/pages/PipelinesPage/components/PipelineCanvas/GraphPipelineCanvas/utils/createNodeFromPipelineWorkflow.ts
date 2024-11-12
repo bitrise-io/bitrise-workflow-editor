1c7b9449093f53534df0800c6ba252171c795b1f
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-11-12 14:52:24 +0100

    fix: disable actions on graph pipeline if feature is not enabled

--- source/javascripts/pages/PipelinesPage/components/PipelineCanvas/GraphPipelineCanvas/utils/createNodeFromPipelineWorkflow.ts
+++ source/javascripts/pages/PipelinesPage/components/PipelineCanvas/GraphPipelineCanvas/utils/createNodeFromPipelineWorkflow.ts
@@ -1,14 +1,20 @@
+import { Node } from '@xyflow/react';
 import { PipelineWorkflow } from '@/core/models/Workflow';
 import { WORKFLOW_NODE_HEIGHT, WORKFLOW_NODE_TYPE, WORKFLOW_NODE_WIDTH } from '../GraphPipelineCanvas.const';
+import { WorkflowNodeDataType } from '../components/WorkflowNode/WorkflowNode';
 
-export default function createNodeFromPipelineWorkflow(workflow: PipelineWorkflow, pipelineId?: string) {
+export default function createNodeFromPipelineWorkflow(
+  workflow: PipelineWorkflow,
+  pipelineId?: string,
+  actionable: boolean = false,
+): Node<WorkflowNodeDataType> {
   return {
     id: workflow.id,
-    deletable: true,
+    deletable: actionable,
     draggable: false,
     focusable: false,
     selectable: true,
-    connectable: false,
+    connectable: actionable,
     type: WORKFLOW_NODE_TYPE,
     width: WORKFLOW_NODE_WIDTH,
     height: WORKFLOW_NODE_HEIGHT,
