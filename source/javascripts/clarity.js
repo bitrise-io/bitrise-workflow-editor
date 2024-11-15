ea46212fdedc5471d080fcf24b256dd72c6babb2
Andras Eszes <andras.eszes@bitrise.io>
2024-11-15 16:12:24 +0100

    feat: add the proper project ID

--- source/javascripts/clarity.js
+++ source/javascripts/clarity.js
@@ -2,4 +2,4 @@
 	c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
 	t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
 	y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-})(window, document, "clarity", "script", "oyw5ozczks"); // NOTE: This is my personal Clarity tag it should be replaced with the correct one
+})(window, document, "clarity", "script", "mxdzahxph2");
