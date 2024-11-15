f80ed9269d582bf0403de51353ffcad5c9148599
Laszlo Moczo <laszlo.moczo@bitrise.io>
2024-11-15 13:01:12 +0100

    Cleanup not used files (#1306)

--- source/javascripts/components/common/notifications/CreatingYmlOnWebsiteProgress.tsx
+++
@@ -1,7 +0,0 @@
-import { Notification } from '@bitrise/bitkit';
-
-const CreatingYmlOnWebsiteProgress = (): JSX.Element => (
-  <Notification status="progress">{window.strings.yml.store_on_website.loading}</Notification>
-);
-
-export default CreatingYmlOnWebsiteProgress;
