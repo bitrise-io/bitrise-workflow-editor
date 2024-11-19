b55ce9488d8f037d7b4e97217208e5adfdc58fd6
Zoltan Szabo <63643463+zoltanszabo-bitrise@users.noreply.github.com>
2024-11-19 14:03:00 +0100

    fix: expose underlying error to caller (#1319)

--- source/javascripts/components/unified-editor/StartBuildDialog/useStartBuild.ts
+++ source/javascripts/components/unified-editor/StartBuildDialog/useStartBuild.ts
@@ -1,10 +1,17 @@
 import { useMutation } from '@tanstack/react-query';
-import BuildApi from '@/core/api/BuildApi';
+import BuildApi, { StartBuildResponse } from '@/core/api/BuildApi';
 import WindowUtils from '@/core/utils/WindowUtils';
+import { ClientError } from '@/core/api/client';
+
+type RequestBody = {
+  pipelineId?: string;
+  workflowId?: string;
+  branch: string;
+};
 
 function useStartBuild() {
-  return useMutation({
-    mutationFn: ({ pipelineId, workflowId, branch }: { pipelineId?: string; workflowId?: string; branch: string }) =>
+  return useMutation<StartBuildResponse | undefined, ClientError, RequestBody>({
+    mutationFn: ({ pipelineId, workflowId, branch }) =>
       BuildApi.startBuild({
         appSlug: WindowUtils.appSlug() ?? '',
         branch,
