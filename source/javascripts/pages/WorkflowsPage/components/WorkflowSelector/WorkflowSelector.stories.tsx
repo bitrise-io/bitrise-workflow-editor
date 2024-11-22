b95850e50a7909b8251183c6d7dea0010ab2e99b
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-11-22 09:26:34 +0100

    chore: clean-up unused WorkflowsPage components

--- source/javascripts/pages/WorkflowsPage/components/WorkflowSelector/WorkflowSelector.stories.tsx
+++ source/javascripts/pages/WorkflowsPage/components/WorkflowSelector/WorkflowSelector.stories.tsx
@@ -1,31 +1,19 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { withBitriseYml } from '@/contexts/BitriseYmlProvider';
+import { MockYml } from '@/core/models/BitriseYml.mocks';
 import WorkflowSelector from './WorkflowSelector';
 
 type Story = StoryObj<typeof WorkflowSelector>;
 
-const createWorkflowIds = (count = 12): string[] => {
-  const items = Array(count).fill(null);
-
-  return items.map((_, index) => {
-    const shouldBeUtility = index % 3 === 0;
-    return shouldBeUtility ? `_utility_${index}` : `workflow_${index}`;
-  }, []);
-};
-
 const meta: Meta<typeof WorkflowSelector> = {
   component: WorkflowSelector,
-  args: {
-    workflowIds: createWorkflowIds(),
-    selectedWorkflowId: 'workflow_0',
-  },
+  args: {},
   argTypes: {
-    onSelectWorkflowId: {
-      type: 'function',
-    },
     onCreateWorkflow: {
       type: 'function',
     },
   },
+  decorators: (Story) => withBitriseYml(MockYml, Story),
 };
 
 export const Default: Story = {};
