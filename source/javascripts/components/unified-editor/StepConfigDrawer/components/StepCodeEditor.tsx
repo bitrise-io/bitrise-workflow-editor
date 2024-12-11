3c9befad2102088f03b0529e182fde2bf359554c
Zoltan Szabo <zoltan.szabo@bitrise.io>
2024-12-11 16:10:51 +0100

    fix: disable quickSuggestions

--- source/javascripts/components/unified-editor/StepConfigDrawer/components/StepCodeEditor.tsx
+++ source/javascripts/components/unified-editor/StepConfigDrawer/components/StepCodeEditor.tsx
@@ -13,6 +13,8 @@ const EDITOR_OPTIONS = {
   contextmenu: false,
   minimap: { enabled: false },
   padding: { top: 16, bottom: 16 },
+  suggestOnTriggerCharacters: true,
+  quickSuggestions: false,
 };
 
 type Props = {
