401bc63e4d636062a4afd39d8e79c47144aa2eed
Zoltan Szabo <63643463+zoltanszabo-bitrise@users.noreply.github.com>
2024-12-12 08:34:06 +0100

    feat: improve completion provider (#1364)

--- source/javascripts/hooks/useMonacoCompletionProvider.ts
+++ source/javascripts/hooks/useMonacoCompletionProvider.ts
@@ -15,12 +15,26 @@ const useEnvVarsAndSecretsCompletionProvider = ({ monaco, language }: Props) => 
     () => ({
       triggerCharacters: ['$'],
       provideCompletionItems: (model, position) => {
-        const word = model.getWordUntilPosition(position);
+        const wordUntilPosition = model.getWordUntilPosition(position);
+        const { startColumn, endColumn } = wordUntilPosition;
+
+        const { lineNumber } = position;
+        const lineContent = model.getLineContent(lineNumber);
+        // NOTE Needs to be -2 because column is 1-based, and chars are 0-based
+        // -1 would get us the char at the cursor
+        const prefixPos = startColumn - 2;
+        const prefixChar = lineContent.charAt(prefixPos);
+
+        // Word doesn't have $ prefix, so it's not an env var or secret
+        if (prefixChar !== '$') {
+          return { suggestions: [] };
+        }
+
         const range = {
-          startLineNumber: position.lineNumber,
-          startColumn: word.startColumn,
-          endLineNumber: position.lineNumber,
-          endColumn: word.endColumn,
+          startLineNumber: lineNumber,
+          startColumn,
+          endLineNumber: lineNumber,
+          endColumn,
         };
 
         const suggestions: languages.CompletionItem[] = items.map((item) => ({
