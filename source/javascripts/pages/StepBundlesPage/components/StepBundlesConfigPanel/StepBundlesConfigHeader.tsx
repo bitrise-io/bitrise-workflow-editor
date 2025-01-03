021405923d54d342ac3e888122e92351243842c6
Luca Kollar <luca.gelencser-kollar@bitrise.io>
2025-01-03 13:23:34 +0100

    Removed tabs from StepBundlesConfigPanel

--- source/javascripts/pages/StepBundlesPage/components/StepBundlesConfigPanel/StepBundlesConfigHeader.tsx
+++ source/javascripts/pages/StepBundlesPage/components/StepBundlesConfigPanel/StepBundlesConfigHeader.tsx
@@ -1,4 +1,4 @@
-import { Box, Tab, TabList, Text } from '@bitrise/bitkit';
+import { Box, Text } from '@bitrise/bitkit';
 import useDependantWorkflows from '@/hooks/useDependantWorkflows';
 import { useWorkflowConfigContext } from '@/components/unified-editor/WorkflowConfig/WorkflowConfig.context';
 import StepBundleService from '@/core/models/StepBundleService';
@@ -12,24 +12,16 @@ const StepBundlesConfigHeader = ({ parentStepBundleId }: Props) => {
   const dependants = useDependantWorkflows({ workflowId: id });
 
   return (
-    <>
-      <Box display="flex" justifyContent="space-between" alignItems="center" px={24} py={16}>
-        <Box>
-          <Text as="h3" textStyle="heading/h3">
-            {parentStepBundleId}
-          </Text>
-          <Text textStyle="body/sm/regular" color="text/secondary">
-            {StepBundleService.getUsedByText(dependants.length)}
-          </Text>
-        </Box>
+    <Box display="flex" justifyContent="space-between" padding="24px 24px 16px 24px">
+      <Box>
+        <Text as="h3" textStyle="heading/h3">
+          {parentStepBundleId}
+        </Text>
+        <Text textStyle="body/sm/regular" color="text/secondary">
+          {StepBundleService.getUsedByText(dependants.length)}
+        </Text>
       </Box>
-      <Box mt="16">
-        <TabList position="relative" paddingX="8">
-          <Tab>Configuration</Tab>
-          <Tab>Properties</Tab>
-        </TabList>
-      </Box>
-    </>
+    </Box>
   );
 };
 
--- source/javascripts/pages/StepBundlesPage/components/StepBundlesConfigPanel/StepBundlesConfigPanel.tsx
+++ source/javascripts/pages/StepBundlesPage/components/StepBundlesConfigPanel/StepBundlesConfigPanel.tsx
@@ -1,16 +1,10 @@
-import { useEffect } from 'react';
-import { TabPanel, TabPanels, Tabs, useTabs } from '@bitrise/bitkit';
-import useSearchParams from '@/hooks/useSearchParams';
-import { StepBundlesConfigTab } from '@/pages/StepBundlesPage/components/StepBundlesConfigPanel/StepBundlesConfig.types';
+import { Box } from '@bitrise/bitkit';
 import useSelectedStepBundle from '@/pages/StepBundlesPage/hooks/useSelectedStepBundle';
 import { useStepBundles } from '@/hooks/useStepBundles';
 import { useStepBundlesPageStore } from '@/pages/StepBundlesPage/StepBundlesPage.store';
 import StepBundlesConfigProvider from '@/pages/StepBundlesPage/components/StepBundlesConfigPanel/StepBundlesConfig.context';
-import StepBundlesConfigurationTab from '@/pages/StepBundlesPage/components/StepBundlesConfigPanel/StepBundlesConfigurationTab';
-import StepBundlesConfigHeader from '@/pages/StepBundlesPage/components/StepBundlesConfigPanel/StepBundlesConfigHeader';
 import StepBundlesPropertiesTab from '@/components/unified-editor/StepBundleDrawer/StepBundlePropertiesTab';
-
-const TAB_IDS = [StepBundlesConfigTab.CONFIGURATION, StepBundlesConfigTab.PROPERTIES];
+import StepBundlesConfigHeader from '@/pages/StepBundlesPage/components/StepBundlesConfigPanel/StepBundlesConfigHeader';
 
 type ConfigPanelContentProps = {
   stepBundleId: string;
@@ -19,25 +13,6 @@ type ConfigPanelContentProps = {
 const StepBundlesConfigPanelContent = ({ stepBundleId }: ConfigPanelContentProps) => {
   const [, setSelectedStepBundle] = useSelectedStepBundle();
   const stepBundles = useStepBundles();
-  const { setTabIndex, tabIndex } = useTabs<StepBundlesConfigTab>({
-    tabIds: TAB_IDS,
-  });
-
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  useEffect(() => {
-    if (searchParams.tab) {
-      setTabIndex(TAB_IDS.indexOf(searchParams.tab as StepBundlesConfigTab));
-    }
-  }, [searchParams, setTabIndex]);
-
-  const onTabChange = (index: number) => {
-    setSearchParams({
-      ...searchParams,
-      tab: TAB_IDS[index],
-    });
-  };
-
   const closeDialog = useStepBundlesPageStore((s) => s.closeDialog);
   const onDelete = (deletedId: string) => {
     setSelectedStepBundle(Object.keys(stepBundles).find((bundleId) => bundleId !== deletedId));
@@ -45,24 +20,12 @@ const StepBundlesConfigPanelContent = ({ stepBundleId }: ConfigPanelContentProps
   };
 
   return (
-    <Tabs
-      display="flex"
-      flexDir="column"
-      borderLeft="1px solid"
-      borderColor="border/regular"
-      index={tabIndex}
-      onChange={onTabChange}
-    >
+    <Box borderLeft="1px solid" borderColor="border/regular">
       <StepBundlesConfigHeader parentStepBundleId={stepBundleId} />
-      <TabPanels flex="1" minH="0">
-        <TabPanel p="24" overflowY="auto" h="100%">
-          <StepBundlesConfigurationTab parentStepBundleId={stepBundleId} />
-        </TabPanel>
-        <TabPanel p="24" overflowY="auto" h="100%">
-          <StepBundlesPropertiesTab stepBundleId={stepBundleId} onDelete={onDelete} />
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+      <Box padding="16px 24px">
+        <StepBundlesPropertiesTab stepBundleId={stepBundleId} onDelete={onDelete} />
+      </Box>
+    </Box>
   );
 };
 
