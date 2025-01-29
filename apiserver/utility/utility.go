74a034099b9995e4326d11ffd2ab174fc0d02458
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-01-29 11:25:31 +0100

    Pass in validation type to cli.CreateBitriseConfigFromCLIParams

--- apiserver/utility/utility.go
+++ apiserver/utility/utility.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/bitrise-io/bitrise/bitrise"
 	"github.com/bitrise-io/bitrise/cli"
 )
 
@@ -26,7 +27,7 @@ func ValidateBitriseConfigAndSecret(bitriseConfig, secretsConfig string) (*Warni
 	bitriseConfigBase64 := base64.StdEncoding.EncodeToString([]byte(bitriseConfig))
 	secretsConfigBase64 := base64.StdEncoding.EncodeToString([]byte(secretsConfig))
 
-	_, bitriseWarns, bitriseErr := cli.CreateBitriseConfigFromCLIParams(bitriseConfigBase64, "")
+	_, bitriseWarns, bitriseErr := cli.CreateBitriseConfigFromCLIParams(bitriseConfigBase64, "", bitrise.ValidationTypeFull)
 	var secretsErr error
 	if secretsConfigBase64 != "" {
 		_, secretsErr = cli.CreateInventoryFromCLIParams(secretsConfigBase64, "")
