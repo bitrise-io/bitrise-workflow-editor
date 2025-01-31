56735dcfc2bf381677f449ad3409f94fd0640103
Andras Eszes <andras.eszes@bitrise.io>
2025-01-31 13:40:34 +0100

    feat: display pipeline conversion notification if needed (#1417)
    * feat: add PipelineService.hasStepInside method
    
    * feat: use pipelineId instead pipeline object
    
    * feat: set locastorage when need to show the notification
    
    * feat: display Ribbon on PipelineCanvas

--- source/javascripts/pages/PipelinesPage/components/CreatePipelineDialog/CreatePipelineDialog.tsx
+++ source/javascripts/pages/PipelinesPage/components/CreatePipelineDialog/CreatePipelineDialog.tsx
@@ -1,15 +1,18 @@
 import { DialogProps } from '@bitrise/bitkit';
 import PipelineService from '@/core/models/PipelineService';
 import CreateEntityDialog from '@/components/unified-editor/CreateEntityDialog/CreateEntityDialog';
-import useBitriseYmlStore from '@/hooks/useBitriseYmlStore';
+import useBitriseYmlStore, { useBitriseYmlStoreApi } from '@/hooks/useBitriseYmlStore';
 import useFeatureFlag from '@/hooks/useFeatureFlag';
 import usePipelineSelector from '../../hooks/usePipelineSelector';
+import usePipelineConversionNotification from '../../hooks/usePipelineConversionNotification';
 
 type Props = Omit<DialogProps, 'title'> & {
   onCreatePipeline: (pipelineId: string, basePipelineId?: string) => void;
 };
 
 const CreatePipelineDialog = ({ onCreatePipeline, onClose, onCloseComplete, ...props }: Props) => {
+  const bitriseYmlStoreApi = useBitriseYmlStoreApi();
+  const { displayPipelineConversionNotificationFor } = usePipelineConversionNotification();
   const isPipelineConversionEnabled = useFeatureFlag('enable-create-graph-pipeline-based-on-staged-pipeline');
 
   const baseEntityIds = useBitriseYmlStore(({ yml }) => {
@@ -32,13 +35,32 @@ const CreatePipelineDialog = ({ onCreatePipeline, onClose, onCloseComplete, ...p
     onCloseComplete?.();
   };
 
+  const handleCreatePipeline = (pipelineId: string, basePipelineId?: string) => {
+    onCreatePipeline(pipelineId, basePipelineId);
+
+    if (!isPipelineConversionEnabled) {
+      return;
+    }
+
+    const { yml } = bitriseYmlStoreApi.getState();
+    const basePipeline = PipelineService.getPipeline(basePipelineId ?? '', yml);
+
+    if (!basePipeline || PipelineService.isGraph(basePipeline)) {
+      return;
+    }
+
+    if (PipelineService.hasStepInside(pipelineId, 'pull-intermediate-files', yml)) {
+      displayPipelineConversionNotificationFor(pipelineId);
+    }
+  };
+
   return (
     <CreateEntityDialog
       baseEntityIds={baseEntityIds}
       entityName="Pipeline"
       onClose={onClose}
       onCloseComplete={handleCloseComplete}
-      onCreateEntity={onCreatePipeline}
+      onCreateEntity={handleCreatePipeline}
       sanitizer={PipelineService.sanitizeName}
       validator={(v) => PipelineService.validateName(v, pipelineIds)}
       {...props}
