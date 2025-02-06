d6648ab4e947dea81d7a92a32edcd0aa26d7b566
Luca Kollar <luca.gelencser-kollar@bitrise.io>
2025-02-06 14:56:00 +0100

    Added description and summary to Step bundles

--- source/javascripts/components/unified-editor/StepBundlesConfig/StepBundlePropertiesTab.tsx
+++ source/javascripts/components/unified-editor/StepBundlesConfig/StepBundlePropertiesTab.tsx
@@ -1,7 +1,10 @@
-import { Button, useDisclosure } from '@bitrise/bitkit';
+import { ChangeEventHandler, useState } from 'react';
+import { Button, Textarea, useDisclosure } from '@bitrise/bitkit';
+import { useDebounceCallback } from 'usehooks-ts';
 import EditableInput from '@/components/EditableInput/EditableInput';
 import StepBundleService from '@/core/models/StepBundleService';
 import { useStepBundles } from '@/hooks/useStepBundles';
+import useBitriseYmlStore from '@/hooks/useBitriseYmlStore';
 import DeleteStepBundleDialog from '../DeleteStepBundleDialog/DeleteStepBundleDialog';
 import { useStepBundleConfigContext } from './StepBundlesConfig.context';
 import useRenameStepBundle from './hooks/useRenameStepBundle';
@@ -17,13 +20,31 @@ const StepBundlePropertiesTab = (props: StepBundlePropertiesTabProps) => {
   const { isOpen: isDeleteDialogOpen, onOpen: openDeleteDialog, onClose: closeDeleteDialog } = useDisclosure();
   const stepBundles = useStepBundles();
   const stepBundleIds = Object.keys(stepBundles);
+  const updateStepBundle = useBitriseYmlStore((s) => s.updateStepBundle);
+  const debouncedUpdateStepBundle = useDebounceCallback(updateStepBundle, 100);
 
+  const [{ summary, description }, setValues] = useState({
+    summary: stepBundle?.userValues.summary || '',
+    description: stepBundle?.userValues.description || '',
+  });
   const rename = useRenameStepBundle(onRename);
 
   const handleNameChange = (newValue: string) => {
     if (newValue !== stepBundle?.id) {
       rename(newValue);
     }
+  };
+
+  const handleSummaryChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setValues((prev) => ({ ...prev, summary: e.target.value }));
+    debouncedUpdateStepBundle(stepBundle?.id || '', { summary: e.target.value });
+  };
+
+  const handleDescriptionChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setValues((prev) => ({ ...prev, description: e.target.value }));
+    debouncedUpdateStepBundle(stepBundle?.id || '', {
+      description: e.target.value,
+    });
   };
 
   return (
@@ -36,7 +57,10 @@ const StepBundlePropertiesTab = (props: StepBundlePropertiesTabProps) => {
         sanitize={StepBundleService.sanitizeName}
         validate={(v) => StepBundleService.validateName(v, stepBundle?.id || '', stepBundleIds)}
         onCommit={handleNameChange}
+        marginBlockEnd="24"
       />
+      <Textarea label="Summary" value={summary} onChange={handleSummaryChange} marginBlockEnd="24" />
+      <Textarea label="Description" value={description} onChange={handleDescriptionChange} />
       {!!onDelete && (
         <>
           <Button leftIconName="Trash" variant="secondary" isDanger marginBlockStart="24" onClick={openDeleteDialog}>
