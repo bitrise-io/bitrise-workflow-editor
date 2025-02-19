62b4f3e89a74bbd5f544ba067b3e78b063424867
Andras Eszes <andras.eszes@bitrise.io>
2025-02-19 09:06:12 +0100

    refactor: handle parallel field type better (#1458)
    * refactor: handle parallel field type better
    
    * feat: improve input validation
    
    * fix: minor issues
    
    * refactor: update parallel validation to enforce positive integers

--- source/javascripts/core/services/GraphPipelineWorkflowService.ts
+++ source/javascripts/core/services/GraphPipelineWorkflowService.ts
@@ -1,11 +1,25 @@
 function validateParallel(parallel?: string | number) {
-  if (!parallel || (typeof parallel === 'string' && parallel.startsWith('$')) || !isNaN(Number(parallel))) {
+  const isEmpty = !parallel && parallel !== 0 && parallel !== '0';
+  const isEnvVar = typeof parallel === 'string' && parallel.startsWith('$');
+  const isPositiveInteger = isIntegerValue(parallel) && Number(parallel) > 0;
+
+  if (isEmpty || isEnvVar || isPositiveInteger) {
     return true;
   }
 
-  return 'Parallel copies should be a number or a valid environment variable.';
+  return 'Parallel copies should be a positive integer or a valid environment variable.';
+}
+
+function isIntegerValue(value?: string | number) {
+  return Number.isInteger(Number(value));
+}
+
+function asIntegerIfPossible(value?: string | number) {
+  return isIntegerValue(value) ? Number(value) : value;
 }
 
 export default {
+  isIntegerValue,
   validateParallel,
+  asIntegerIfPossible,
 };
