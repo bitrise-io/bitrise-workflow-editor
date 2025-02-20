1400a1d5d017a31fc6eb890560fd59ae229808c7
Andras Eszes <andras.eszes@bitrise.io>
2025-02-20 14:57:04 +0100

    feat: re-introduce Launchdarkly SDK

--- source/javascripts/hooks/useFeatureFlag.ts
+++ source/javascripts/hooks/useFeatureFlag.ts
@@ -1,3 +1,6 @@
+import { initialize, LDClient } from 'launchdarkly-js-client-sdk';
+import WindowUtils from '@/core/utils/WindowUtils';
+
 const defaultValues = {
   'enable-wfe-diff-editor': false,
   'enable-wfe-step-bundles-ui': false,
@@ -6,12 +9,23 @@ const defaultValues = {
 
 type FeatureFlags = typeof defaultValues;
 
+let client: LDClient | undefined;
+
+if (WindowUtils.workspaceSlug() && !client) {
+  client = initialize(
+    window.parent.location.host === 'app.bitrise.io' ? '5e70774c8a726707851d2fff' : '5e70774c8a726707851d2ffe',
+    {
+      kind: 'user',
+      key: `org-${WindowUtils.workspaceSlug()}`,
+    },
+  );
+}
+
 const useFeatureFlag = <K extends keyof FeatureFlags>(key: K): FeatureFlags[K] => {
   const localValue = window.localFeatureFlags?.[key];
-  const accountValue = window.parent?.globalProps?.featureFlags?.account?.[key];
-  const defaultValue = defaultValues[key];
+  const defaultValue = WindowUtils.globalProps()?.featureFlags?.account?.[key] ?? defaultValues[key];
 
-  return (localValue ?? accountValue ?? defaultValue) as FeatureFlags[K];
+  return (localValue ?? client?.variation(key, defaultValue) ?? defaultValue) as FeatureFlags[K];
 };
 
 export default useFeatureFlag;
