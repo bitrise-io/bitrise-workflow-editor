b8825e97ddfba5bd9aa3179fe2aa0d24d04de3c6
Luca Kollar <luca.gelencser-kollar@bitrise.io>
2025-02-25 13:30:16 +0100

    Added tabs for Step bundles

--- source/javascripts/components/unified-editor/StepBundlesConfig/StepBundlesConfigHeader.tsx
+++ source/javascripts/components/unified-editor/StepBundlesConfig/StepBundlesConfigHeader.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@bitrise/bitkit';
+import { Box, Tab, TabList, Text } from '@bitrise/bitkit';
 import useDependantWorkflows from '@/hooks/useDependantWorkflows';
 import StepBundleService from '@/core/services/StepBundleService';
 import { useStepBundleConfigContext } from './StepBundlesConfig.context';
@@ -10,12 +10,18 @@ const StepBundlesConfigHeader = () => {
 
   return (
     <>
-      <Text as="h3" textStyle="heading/h3">
-        {userValues?.title || id || 'Step bundle'}
-      </Text>
-      <Text textStyle="body/sm/regular" color="text/secondary">
-        {StepBundleService.getUsedByText(dependants.length)}
-      </Text>
+      <Box padding="24">
+        <Text as="h3" textStyle="heading/h3">
+          {userValues?.title || id || 'Step bundle'}
+        </Text>
+        <Text textStyle="body/sm/regular" color="text/secondary">
+          {StepBundleService.getUsedByText(dependants.length)}
+        </Text>
+      </Box>
+      <TabList paddingX="8">
+        <Tab>Configuration</Tab>
+        <Tab>Properties</Tab>
+      </TabList>
     </>
   );
 };
--- source/javascripts/components/unified-editor/StepBundlesConfig/StepBundlesConfigPanel.tsx
+++ source/javascripts/components/unified-editor/StepBundlesConfig/StepBundlesConfigPanel.tsx
@@ -1,6 +1,10 @@
-import { Box } from '@bitrise/bitkit';
+import { useEffect } from 'react';
+import { TabPanel, TabPanels, Tabs, useTabs } from '@bitrise/bitkit';
 import useSelectedStepBundle from '@/hooks/useSelectedStepBundle';
 import { useStepBundlesPageStore } from '@/pages/StepBundlesPage/StepBundlesPage.store';
+import { StepBundleConfigTab } from '@/components/unified-editor/StepBundlesConfig/StepBundleConfigTab';
+import StepBundleConfigurationTab from '@/components/unified-editor/StepBundlesConfig/StepBundleConfigurationTab';
+import useSearchParams from '@/hooks/useSearchParams';
 import StepBundlePropertiesTab from './StepBundlePropertiesTab';
 import StepBundlesConfigHeader from './StepBundlesConfigHeader';
 import StepBundlesConfigProvider from './StepBundlesConfig.context';
@@ -9,17 +13,46 @@ type Props = {
   id: string;
 };
 
+const TAB_IDS = [StepBundleConfigTab.CONFIGURATION, StepBundleConfigTab.PROPERTIES];
+
 const StepBundlesConfigPanelContent = () => {
   const { closeDialog } = useStepBundlesPageStore();
   const [, setSelectedStepBundle] = useSelectedStepBundle();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { setTabIndex, tabIndex } = useTabs<StepBundleConfigTab>({ tabIds: TAB_IDS });
+
+  const onTabChange = (index: number) => {
+    setSearchParams({
+      ...searchParams,
+      tab: TAB_IDS[index],
+    });
+  };
+
+  useEffect(() => {
+    if (searchParams.tab) {
+      setTabIndex(TAB_IDS.indexOf(searchParams.tab as StepBundleConfigTab));
+    }
+  }, [searchParams, setTabIndex]);
 
   return (
-    <Box padding="24" borderLeft="1px solid" borderColor="border/regular">
-      <Box as="header" marginBlockEnd="24">
-        <StepBundlesConfigHeader />
-      </Box>
-      <StepBundlePropertiesTab onDelete={() => closeDialog()} onRename={setSelectedStepBundle} />
-    </Box>
+    <Tabs
+      display="flex"
+      flexDir="column"
+      borderLeft="1px solid"
+      borderColor="border/regular"
+      index={tabIndex}
+      onChange={onTabChange}
+    >
+      <StepBundlesConfigHeader />
+      <TabPanels flex="1" minH="0">
+        <TabPanel p="24" overflowY="auto" h="100%">
+          <StepBundleConfigurationTab />
+        </TabPanel>
+        <TabPanel p="24" overflowY="auto" h="100%">
+          <StepBundlePropertiesTab onDelete={() => closeDialog()} onRename={setSelectedStepBundle} />
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 };
 
