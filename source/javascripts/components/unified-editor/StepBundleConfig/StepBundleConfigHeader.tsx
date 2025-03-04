d242e0c1f820c58255301aa9eaa89efa29da800b
Laszlo Moczo <laszlo.moczo@bitrise.io>
2025-03-04 20:30:48 +0100

    BIVS-3025

--- source/javascripts/components/unified-editor/StepBundleConfig/StepBundleConfigHeader.tsx
+++ source/javascripts/components/unified-editor/StepBundleConfig/StepBundleConfigHeader.tsx
@@ -10,7 +10,8 @@ type HeaderProps = {
 
 const StepBundleConfigHeader = (props: HeaderProps) => {
   const { variant } = props;
-  const { cvs, id, userValues } = useStepBundleConfigContext() ?? {};
+  const { stepBundle } = useStepBundleConfigContext() ?? {};
+  const { cvs, id, userValues } = stepBundle || {};
   const enableStepBundles = useFeatureFlag('enable-wfe-step-bundles-ui');
   const dependants = useDependantWorkflows({ stepBundleCvs: cvs });
 
--- source/javascripts/components/unified-editor/StepBundleConfig/StepBundleInputs/StepBundleInputsForm.tsx
+++ source/javascripts/components/unified-editor/StepBundleConfig/StepBundleInputs/StepBundleInputsForm.tsx
@@ -17,17 +17,7 @@ import StepBundleService from '@/core/services/StepBundleService';
 import StepInput from '../../StepConfigDrawer/components/StepInput';
 import { FormItems, FormMode } from '../types/StepBundle.types';
 import StepSelectInput from '../../StepConfigDrawer/components/StepSelectInput';
-
-function expandInput(input: EnvironmentItemModel = {}) {
-  const { opts, ...keyValuePair } = input;
-  const key = Object.keys(keyValuePair)[0] || '';
-  const value = (Object.values(keyValuePair)[0] as string | null) || '';
-  return {
-    key,
-    value,
-    opts: opts || {},
-  };
-}
+import { expandInput } from '../utils/StepBundle.utils';
 
 type StepBundleInputsFormProps = {
   ids: string[];
