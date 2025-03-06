33a1577491e5b946350b1e7aff38537e8648e60b
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-03-06 10:02:42 +0100

    Add failing step related envs to GetDefaultOutputsHandler (#1507)
    * Add failing step related envs to GetDefaultOutputsHandler
    
    * Fix default envs test

--- apiserver/service/default_options_test.go
+++ apiserver/service/default_options_test.go
@@ -18,5 +18,5 @@ func TestGetDefaultOutputsHandler(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	require.Equal(t, http.StatusOK, rr.Code, rr.Body.String())
-	require.Equal(t, "{\"from_bitrise_cli\":[{\"BITRISE_SOURCE_DIR\":\"\"},{\"BITRISE_DEPLOY_DIR\":\"\"},{\"BITRISE_BUILD_STATUS\":\"\"},{\"BITRISE_TRIGGERED_WORKFLOW_ID\":\"\"},{\"BITRISE_TRIGGERED_WORKFLOW_TITLE\":\"\"},{\"CI\":\"\"},{\"PR\":\"\"}]}\n", rr.Body.String())
+	require.Equal(t, "{\"from_bitrise_cli\":[{\"BITRISE_SOURCE_DIR\":\"\"},{\"BITRISE_DEPLOY_DIR\":\"\"},{\"BITRISE_TEST_RESULT_DIR\":\"\"},{\"BITRISE_BUILD_STATUS\":\"\"},{\"BITRISE_TRIGGERED_WORKFLOW_ID\":\"\"},{\"BITRISE_TRIGGERED_WORKFLOW_TITLE\":\"\"},{\"CI\":\"\"},{\"PR\":\"\"},{\"BITRISE_FAILED_STEP_TITLE\":\"\"},{\"BITRISE_FAILED_STEP_ERROR_MESSAGE\":\"\"}]}\n", rr.Body.String())
 }
