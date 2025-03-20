cfce62798e5323f9816a037b8321828bbabaac40
zsolt-vicze <zsolt.vicze@bitrise.io>
2025-03-20 15:00:19 +0000

    Do not commit vendor directory

--- vendor/github.com/bitrise-io/stepman/activator/activator.go
+++
@@ -1,8 +0,0 @@
-package activator
-
-type ActivatedStep struct {
-	StepYMLPath string
-
-	// DidStepLibUpdate indicates that the local steplib cache was updated while resolving the exact step version.
-	DidStepLibUpdate bool
-}
