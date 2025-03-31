499c256662ef76dc2f0c4121506b0802cc50d17b
Laszlo Moczo <laszlo.moczo@bitrise.io>
2025-03-31 16:03:48 +0200

    Refact notifications on Configuration YAML page [BIVS-3056]

--- source/javascripts/pages/YmlPage/YmlPage.tsx
+++ source/javascripts/pages/YmlPage/YmlPage.tsx
@@ -1,7 +1,13 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Link, Notification, Text } from '@bitrise/bitkit';
 import { BitriseYmlSettings } from '@/core/models/BitriseYmlSettings';
+import useUserMetaData from '@/hooks/useUserMetaData';
+import RuntimeUtils from '@/core/utils/RuntimeUtils';
 import YmlEditor from './components/YmlEditor';
 import YmlEditorHeader from './components/YmlEditorHeader';
+
+const SPLITTED_METADATA_KEY = 'wfe_modular_yaml_git_notification_closed';
+const SPLIT_METADATA_ENTERPRISE_KEY = 'wfe_modular_yaml_enterprise_notification_closed';
+const SPLIT_METADATA_KEY = 'wfe_modular_yaml_split_notification_closed';
 
 type YmlPageProps = {
   ciConfigYml: string;
@@ -14,6 +20,17 @@ type YmlPageProps = {
 const YmlPage = (props: YmlPageProps) => {
   const { ciConfigYml, isEditorLoading, onConfigSourceChangeSaved, onEditorChange, ymlSettings } = props;
 
+  const isWebsiteMode = RuntimeUtils.isWebsiteMode();
+
+  const { value: splittedMetaDataValue, update: updateSplittedMetaData } = useUserMetaData(
+    SPLITTED_METADATA_KEY,
+    isWebsiteMode && !!ymlSettings.isYmlSplit && !!ymlSettings.usesRepositoryYml,
+  );
+  const { value: splitMetaDataValue, update: updateSplitMetaData } = useUserMetaData(
+    ymlSettings.isModularYamlSupported ? SPLIT_METADATA_ENTERPRISE_KEY : SPLIT_METADATA_KEY,
+    isWebsiteMode && !ymlSettings.isYmlSplit && ymlSettings.lines > 500,
+  );
+
   return (
     <Box height="100%" display="flex" flexDirection="column">
       <YmlEditorHeader
@@ -21,7 +38,49 @@ const YmlPage = (props: YmlPageProps) => {
         onConfigSourceChangeSaved={onConfigSourceChangeSaved}
         ymlSettings={ymlSettings || {}}
       />
-      <Box flexGrow="1" flexShrink="1" borderRadius="8" paddingBlock="12" backgroundColor="#1e1e1e">
+      <Box flexGrow="1" flexShrink="1" paddingBlock="12" backgroundColor="#1e1e1e" position="relative">
+        {(splitMetaDataValue === null || splittedMetaDataValue === null) && (
+          <Notification
+            pos="absolute"
+            zIndex="1000"
+            left="50%"
+            transform="translateX(-50%)"
+            status="info"
+            onClose={() => {
+              if (splitMetaDataValue === null) {
+                updateSplitMetaData('true');
+              }
+              if (splittedMetaDataValue === null) {
+                updateSplittedMetaData('true');
+              }
+            }}
+            whiteSpace="nowrap"
+            width="auto"
+          >
+            {splitMetaDataValue === null && (
+              <>
+                Optimize your config by splitting your{' '}
+                <Text as="span" fontWeight="600">
+                  {ymlSettings.lines}-lines
+                </Text>{' '}
+                YAML file into smaller chunks.{' '}
+                <Link
+                  href="https://devcenter.bitrise.io/en/builds/configuration-yaml/modular-yaml-configuration.html"
+                  isExternal
+                  isUnderlined
+                >
+                  Learn more
+                </Link>
+              </>
+            )}
+            {splittedMetaDataValue === null && (
+              <>
+                Your configuration in the Git repository is split across multiple files, but on this page you can see it
+                as one merged YAML.
+              </>
+            )}
+          </Notification>
+        )}
         <YmlEditor
           ciConfigYml={ciConfigYml}
           isLoading={isEditorLoading}
