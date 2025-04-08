3c8522b61cb7e8c3b42ea8a0f28d5b4ed5ad5f4d
Andras Eszes <andras.eszes@bitrise.io>
2025-04-08 16:44:46 +0200

    feat: refactor InitialDataLoader to use separate hooks for CI config and update state management

--- source/javascripts/main.tsx
+++ source/javascripts/main.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, StrictMode } from 'react';
+import { PropsWithChildren, StrictMode, useEffect } from 'react';
 import { Box, Provider as BitkitProvider } from '@bitrise/bitkit';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
@@ -10,62 +10,78 @@ import Navigation from './components/Navigation.react';
 import Header from './components/Header';
 import { paths, routes } from './routes';
 import LazyRoute from './components/LazyRoute';
-import BitriseYmlProvider from './contexts/BitriseYmlProvider';
-import { useGetCiConfigQuery } from './hooks/useCiConfig';
+import { useGetCiConfigJson, useGetCiConfigYml } from './hooks/useCiConfig';
 import PageProps from './core/utils/PageProps';
 import useHashLocation from './hooks/useHashLocation';
 import useHashSearch from './hooks/useHashSearch';
-
-const Providers = ({ children }: PropsWithChildren) => {
-  const { data: yml } = useGetCiConfigQuery({ projectSlug: PageProps.appSlug() });
-
-  if (!yml) {
-    return null;
-  }
-
-  return (
-    <ReactFlowProvider>
-      <BitriseYmlProvider yml={yml} onChange={noop}>
-        <BitkitProvider>{children}</BitkitProvider>
-      </BitriseYmlProvider>
-    </ReactFlowProvider>
-  );
-};
+import { bitriseYmlStore } from './core/stores/BitriseYmlStore';
 
 const App = () => {
   return (
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <Providers>
-          <Box h="100dvh" display="flex" flexDirection="column">
-            <Header
-              appName=""
-              workspacePath=""
-              isDiffDisabled
-              isSaveDisabled
-              isDiscardDisabled
-              isSaveInProgress={false}
-              onDiffClick={noop}
-              onSaveClick={noop}
-              onDiscardClick={noop}
-            />
-            <Box display="flex" flex="1" alignItems="stretch">
-              <Navigation borderRight="1px solid" borderColor="border/regular" />
-              <Box flex="1" overflowX="hidden" overflowY="auto">
-                <Router hook={useHashLocation} searchHook={useHashSearch}>
-                  <Switch>
-                    {routes.map(({ path, component }) => (
-                      <LazyRoute key={path} path={new RegExp(`^\\${path}`)} component={component} />
-                    ))}
-                    <Redirect to={paths.workflows} replace />
-                  </Switch>
-                </Router>
-              </Box>
-            </Box>
-          </Box>
-        </Providers>
+        <ReactFlowProvider>
+          <BitkitProvider>
+            <InitialDataLoader>
+              <Layout />
+            </InitialDataLoader>
+          </BitkitProvider>
+        </ReactFlowProvider>
       </QueryClientProvider>
     </StrictMode>
+  );
+};
+
+const InitialDataLoader = ({ children }: PropsWithChildren) => {
+  // TODO: Handle errors
+  const { data: initialCiConfigYml } = useGetCiConfigYml({ projectSlug: PageProps.appSlug() });
+  const { data: initialCiConfigJson } = useGetCiConfigJson({ projectSlug: PageProps.appSlug() });
+
+  useEffect(() => {
+    bitriseYmlStore.setState({
+      yml: initialCiConfigJson,
+      savedYml: initialCiConfigJson,
+      ymlString: initialCiConfigYml,
+      savedYmlString: initialCiConfigYml,
+    });
+  }, [initialCiConfigYml, initialCiConfigJson]);
+
+  if (!initialCiConfigYml || !initialCiConfigJson) {
+    // TODO: Loading state
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+const Layout = () => {
+  return (
+    <Box h="100dvh" display="flex" flexDirection="column">
+      <Header
+        appName=""
+        workspacePath=""
+        isDiffDisabled
+        isSaveDisabled
+        isDiscardDisabled
+        isSaveInProgress={false}
+        onDiffClick={noop}
+        onSaveClick={noop}
+        onDiscardClick={noop}
+      />
+      <Box display="flex" flex="1" alignItems="stretch">
+        <Navigation borderRight="1px solid" borderColor="border/regular" />
+        <Box flex="1" overflowX="hidden" overflowY="auto">
+          <Router hook={useHashLocation} searchHook={useHashSearch}>
+            <Switch>
+              {routes.map(({ path, component }) => (
+                <LazyRoute key={path} path={new RegExp(`^\\${path}`)} component={component} />
+              ))}
+              <Redirect to={paths.workflows} replace />
+            </Switch>
+          </Router>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
