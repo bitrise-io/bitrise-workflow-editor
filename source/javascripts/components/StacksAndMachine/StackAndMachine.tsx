705ecac3072f0856ec0ddf6a678118444672237e
Laszlo Moczo <laszlo.moczo@bitrise.io>
2025-04-16 20:45:24 +0200

    [BIVS-3124] Use new Stack and Machine component in Workflow drawer/panel

--- source/javascripts/components/StacksAndMachine/StackAndMachine.tsx
+++ source/javascripts/components/StacksAndMachine/StackAndMachine.tsx
@@ -1,5 +1,5 @@
 import { RefObject, useCallback, useRef } from 'react';
-import { Box, Card, Link, Notification } from '@bitrise/bitkit';
+import { Box, Link, Notification } from '@bitrise/bitkit';
 
 import { useResizeObserver } from 'usehooks-ts';
 
@@ -12,13 +12,15 @@ import useProjectStackAndMachine from '@/hooks/useProjectStackAndMachine';
 import MachineTypeSelector from './MachineTypeSelector';
 import StackSelector from './StackSelector';
 import DeprecatedMachineNotification from './DeprecatedMachineNotification';
+import StackAndMachineWrapper from './StackAndMachineWrapper';
 
 const useOrientation = (ref: RefObject<HTMLDivElement>) => {
   const { width } = useResizeObserver({ ref, box: 'border-box' });
-  return width && width < 768 ? 'vertical' : 'horizontal';
+  return width && width < 960 ? 'vertical' : 'horizontal';
 };
 
 type Props = {
+  isExpandable?: boolean;
   stackId: string;
   machineTypeId: string;
   onChange: (stackId: string, machineTypeId: string, rollbackVersion: string) => void;
@@ -28,6 +30,7 @@ type Props = {
 };
 
 const StackAndMachine = ({
+  isExpandable,
   stackId,
   machineTypeId,
   onChange,
@@ -97,7 +100,12 @@ const StackAndMachine = ({
     !!stackRollbackVersion && !!availableRollbackVersion && stackRollbackVersion === availableRollbackVersion;
 
   return (
-    <Card padding="16">
+    <StackAndMachineWrapper
+      isDefault={!stackId && !machineTypeId}
+      isExpandable={isExpandable}
+      machineTypeName={selectedMachineType.name}
+      stackName={selectedStack.name}
+    >
       <Box ref={ref} display="flex" flexDir={orientation === 'horizontal' ? 'row' : 'column'} gap="24">
         <StackSelector
           stack={selectedStack}
@@ -134,7 +142,7 @@ const StackAndMachine = ({
         </Notification>
       )}
       <DeprecatedMachineNotification machineTypeId={selectedMachineType.id} />
-    </Card>
+    </StackAndMachineWrapper>
   );
 };
 
