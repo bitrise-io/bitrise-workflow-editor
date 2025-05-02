fd898013d36b14d6679c503cc0cf46142b0c44ff
Zoltan Szabo <zoltan.szabo@bitrise.io>
2025-05-02 12:10:41 +0200

    feat: enhance EnvVarService with workflow validation and improve error handling

--- source/javascripts/core/stores/BitriseYmlStore.ts
+++ source/javascripts/core/stores/BitriseYmlStore.ts
@@ -521,3 +521,12 @@ export function updateBitriseYmlDocument(mutator: YamlMutator /* , event = Bitri
 
   // dispatchBitriseYmlEvent(event);
 }
+
+export function getBitriseYmlDocument() {
+  return bitriseYmlStore.getState().ymlDocument.clone();
+}
+
+export function isWorkflowExists(id: string): boolean {
+  const doc = getBitriseYmlDocument();
+  return doc.hasIn(['workflows', id]);
+}
