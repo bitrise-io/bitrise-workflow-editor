00a801c552217d00696e583e87dd432827a59ec2
Laszlo Moczo <laszlo.moczo@bitrise.io>
2025-05-08 12:11:32 +0200

    Hack

--- source/javascripts/components/unified-editor/StepConfigDrawer/components/ExpandableMessage.tsx
+++ source/javascripts/components/unified-editor/StepConfigDrawer/components/ExpandableMessage.tsx
@@ -4,11 +4,12 @@ type ExpandableMessageProps = {
   buttonLabel: string;
   children: string;
   isExpanded?: boolean;
+  onButtonClick?: VoidFunction;
   title: string;
 };
 
 const ExpandableMessage = forwardRef<ExpandableMessageProps, 'div'>((props, ref) => {
-  const { buttonLabel, children, isExpanded, title } = props;
+  const { buttonLabel, children, isExpanded, onButtonClick, title } = props;
 
   const { isOpen, onToggle } = useDisclosure({ defaultIsOpen: isExpanded });
 
@@ -17,7 +18,7 @@ const ExpandableMessage = forwardRef<ExpandableMessageProps, 'div'>((props, ref)
       <Box onClick={() => onToggle()} padding="16" data-group role="button" display="flex" alignItems="center" gap="8">
         <Icon name="ChevronDown" transform={isOpen ? 'rotate(180deg)' : undefined} size="24" transition=".3s" />
         {title}
-        <Button size="sm" variant="secondary" marginInlineStart="auto">
+        <Button size="sm" variant="secondary" marginInlineStart="auto" onClick={onButtonClick}>
           {buttonLabel}
         </Button>
       </Box>
--- source/javascripts/components/unified-editor/StepConfigDrawer/components/StepMaker.tsx
+++ source/javascripts/components/unified-editor/StepConfigDrawer/components/StepMaker.tsx
@@ -11,20 +11,23 @@ import aiAvatar from './purr.png';
 
 interface MessageItemProps extends BoxProps {
   message: Message;
+  onPlanButtonClick?: VoidFunction;
 }
 
 const MessageItem = (props: MessageItemProps) => {
-  const { message } = props;
+  const { message, onPlanButtonClick } = props;
   return (
     <Box display="flex" flexDir={message.sender === 'user' ? 'row-reverse' : 'row'} marginBlockEnd="8" gap="8">
-      <Avatar
-        name={message.sender === 'user' ? 'You' : 'Bitbot'}
-        src={message.sender === 'user' ? undefined : aiAvatar}
-        variant="user"
-      />
+      {message.sender === 'user' && (
+        /* <Box padding="8" borderRadius="50%" backgroundColor="background/selected">
+          <Icon name="Person" size="16" color="icon/interactive" />
+        </Box> */
+        <Avatar name="U" variant="user" />
+      )}
+      {message.sender === 'ai' && <Avatar name="AI" src={aiAvatar} variant="user" />}
       <Box padding="12" backgroundColor="background/secondary" borderRadius="8" flex="1">
         {message.type === 'plan' && (
-          <ExpandableMessage buttonLabel="Proceed with plan" title="Here is the plan">
+          <ExpandableMessage buttonLabel="Proceed with plan" onButtonClick={onPlanButtonClick} title="Here is the plan">
             {message.content}
           </ExpandableMessage>
         )}
@@ -64,7 +67,11 @@ const StepMaker = () => {
         marginBlockStart="16"
       >
         {messages.map((message, index) => (
-          <MessageItem key={index} message={message} />
+          <MessageItem
+            key={index}
+            message={message}
+            onPlanButtonClick={() => sendMessage('process_with_plan', 'Proceed with code generation')}
+          />
         ))}
       </Box>
       <Box as="form" display="flex" gap="8" onSubmit={handleSubmit}>
