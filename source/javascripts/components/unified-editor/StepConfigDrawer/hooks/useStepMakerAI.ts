d69b655cc5cd0f7bd37e63140b7362aff1515bc1
Tamas Papik <trapacska@gmail.com>
2025-05-08 12:35:11 +0200

    added code gen

--- source/javascripts/components/unified-editor/StepConfigDrawer/hooks/useStepMakerAI.ts
+++ source/javascripts/components/unified-editor/StepConfigDrawer/hooks/useStepMakerAI.ts
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 import { ResponseInput } from 'openai/resources/responses/responses';
 import { useState } from 'react';
 
-import { plannerPrompt } from './prompts';
+import { coderSystemPrompt, plannerPrompt } from './prompts';
 
 export type Message = {
   content: string;
@@ -17,6 +17,7 @@ type Props = {
 };
 
 const FUNCTION_CALL_PLAN = 'store_plan';
+const FUNCTION_CALL_STORE_BASH_SCRIPT = 'store_bash_script';
 
 // type StepMakerState = InitialState | Planning | CodeGeneration | WaitingForBuild | BuildLogEvaluation;
 
@@ -63,26 +64,9 @@ const useStepMakerAI = (props: Props) => {
 
   const [messages, setMessages] = useState<Message[]>([]);
 
-  const coderSystemPrompt = `
-You are a DevOps engineer implementing Bitrise CI/CD workflows. You are given a high-level plan to implement a new feature in an existing workflow. Your task is to output the bitrise.yml file that implements the requested changes. Only output raw YML, no explanations or comments, no Markdown code blocks.
-The selected workflow to improve: ${selectedWorkflow}
-
-This is the high-level plan you need to implement. It might contain unanswered questions. In this case, use your best judgment to fill in the gaps.
-`;
-
-  // const [state, setState] = useState<StepMakerState>({
-  //   kind: 'initial',
-  //   examplePrompts: [
-  //     'Add a step to send a Slack message when the build fails.',
-  //     'Add a step to run unit tests before deploying to production.',
-  //     'Add a step to send an email notification when the build succeeds.',
-  //   ],
-  //   messages: [],
-  // });
-
   const [toolOutputId, setToolOutputId] = useState('');
 
-  const sendMessage = async (action: 'chat' | 'process_with_plan', input: string) => {
+  const sendMessage = async (action: 'chat' | 'process_with_plan' | 'generate_code', input: string) => {
     setIsLoading(true);
     console.log(coderSystemPrompt, action);
     setMessages((prev) => [...prev, { content: input, sender: 'user', type: 'message' }]);
@@ -96,9 +80,9 @@ This is the high-level plan you need to implement. It might contain unanswered q
     ];
 
     if (toolOutputId.length > 0) {
-      let output = 'plan: ok!';
+      let output = 'ok';
       if (action !== 'process_with_plan') {
-        output = 'plan: requires refinement';
+        output = 'requires refinement';
       }
 
       inputs.push({
@@ -110,9 +94,14 @@ This is the high-level plan you need to implement. It might contain unanswered q
       setToolOutputId('');
     }
 
+    let instructions = plannerPrompt(selectedWorkflow, bitriseYml);
+    if (action === 'generate_code') {
+      instructions = coderSystemPrompt(selectedWorkflow, bitriseYml);
+    }
+
     const response = await client.responses.create({
       model: 'gpt-4o-mini',
-      instructions: plannerPrompt(selectedWorkflow, bitriseYml),
+      instructions,
       input: inputs,
       previous_response_id: responseId,
       tools: [
@@ -143,7 +132,7 @@ This is the high-level plan you need to implement. It might contain unanswered q
           description: 'Store the current high-level plan for the next phase (code generation).',
         },
         {
-          name: 'store_bash_script',
+          name: FUNCTION_CALL_STORE_BASH_SCRIPT,
           strict: false,
           parameters: {
             type: 'object',
@@ -171,23 +160,6 @@ This is the high-level plan you need to implement. It might contain unanswered q
 
     console.log('Response:', response);
 
-    // let type = 'message';
-    // if(response.output[0].type === 'function_call' &&
-    //   response.output[0].name === 'process_plan' )
-    //   {
-    //     type = 'plan';
-    //   }
-    //   [
-    //     {
-    //         "id": "fc_681c74a030b08191a5101aedcfebcef3050d103f60d8a0dc",
-    //         "type": "function_call",
-    //         "status": "completed",
-    //         "arguments": "{\"plan\":\"1. Research Method to Fetch External IP:\\n   - Use a reliable external service or command that returns the external IP address, such as `curl` with a service like `http://ipinfo.io/ip` or `http://ifconfig.me`.\\n\\n2. Error Handling:\\n   - Ensure the command can handle potential errors gracefully. For example, handle situations where the external service is unreachable or returns an unexpected result.\\n\\n3. Security Considerations:\\n   - Ensure that using an external service does not expose sensitive information about your environment.\\n\\n4. Integration with the Bitrise Workflow:\\n   - Add the command to the existing bash script step within your Bitrise workflow.\\n   - Use `echo` to print the result to the console.\\n\\n5. Testing:\\n   - Test the script in a controlled environment to ensure it works as expected without throwing errors.\\n\\n6. Documentation:\\n   - Optionally update any relevant documentation to ensure the process is clear for future reference.\"}",
-    //         "call_id": "call_YRQHlxiphLgI5wY41QDNvGTl",
-    //         "name": "process_plan"
-    //     }
-    // ]
-
     if (response.output[0].type === 'message') {
       setMessages((prev) => [...prev, { content: response.output_text, sender: 'ai', type: 'message' }]);
     } else if (response.output[0].type === 'function_call' && response.output[0].name === FUNCTION_CALL_PLAN) {
@@ -196,31 +168,16 @@ This is the high-level plan you need to implement. It might contain unanswered q
         ...prev,
         { content: JSON.parse((response.output[0] as any).arguments).plan, sender: 'ai', type: 'plan' },
       ]);
+    } else if (
+      response.output[0].type === 'function_call' &&
+      response.output[0].name === FUNCTION_CALL_STORE_BASH_SCRIPT
+    ) {
+      setToolOutputId(response.output[0].call_id);
+      setMessages((prev) => [
+        ...prev,
+        { content: JSON.parse((response.output[0] as any).arguments).script, sender: 'ai', type: 'content' },
+      ]);
     }
-    // let nextState: StepMakerState;
-    // switch (state.kind) {
-    //   case 'initial':
-    //     nextState = {
-    //       kind: 'planning',
-    //       messages: [...state.messages, { content: response.output_text, sender: 'ai', type: 'message' }],
-    //       userQA: new Map(),
-    //     };
-    //     setState(nextState);
-    //     break;
-    //   case 'planning':
-    //     nextState = {
-    //       kind: 'planning',
-    //       userQA: new Map(),
-    //       messages: [...state.messages, { content: response.output_text, sender: 'ai', type: 'message' }],
-    //     };
-    //     break;
-    //   case 'codeGeneration':
-    //     break;
-    //   case 'waitingForBuild':
-    //     break;
-    //   case 'buildLogEvaluation':
-    //     break;
-    // }
   };
 
   return { isLoading, messages, sendMessage };
