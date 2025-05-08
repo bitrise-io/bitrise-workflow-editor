ccfa557ba9a74431236c121fe78d7330883f4fa8
Laszlo Moczo <laszlo.moczo@bitrise.io>
2025-05-08 12:29:28 +0200

    Hack

--- source/javascripts/components/unified-editor/StepConfigDrawer/components/ExpandableMessage.tsx
+++ source/javascripts/components/unified-editor/StepConfigDrawer/components/ExpandableMessage.tsx
@@ -13,18 +13,26 @@ const ExpandableMessage = forwardRef<ExpandableMessageProps, 'div'>((props, ref)
 
   const { isOpen, onToggle } = useDisclosure({ defaultIsOpen: isExpanded });
 
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    onButtonClick?.();
+  };
+
   return (
     <Card variant="outline" ref={ref} width="100%">
       <Box onClick={() => onToggle()} padding="16" data-group role="button" display="flex" alignItems="center" gap="8">
         <Icon name="ChevronDown" transform={isOpen ? 'rotate(180deg)' : undefined} size="24" transition=".3s" />
         {title}
-        <Button size="sm" variant="secondary" marginInlineStart="auto" onClick={onButtonClick}>
+        <Button size="sm" variant="secondary" marginInlineStart="auto" onClick={handleClick}>
           {buttonLabel}
         </Button>
       </Box>
       <Collapse in={isOpen} style={{ overflow: 'unset' }}>
         <Box padding="16">
           <MarkdownContent md={children} />
+          <Button size="sm" variant="secondary" marginBlockStart="16" marginInlineStart="auto" onClick={handleClick}>
+            {buttonLabel}
+          </Button>
         </Box>
       </Collapse>
     </Card>
