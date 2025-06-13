54596af279ae2c8dd585cbd67381599ac312af4a
Andras Eszes <andras.eszes@bitrise.io>
2025-06-13 07:22:48 +0200

    perf: cache ymlJson in useBitriseYmlStore hook

--- source/javascripts/hooks/useBitriseYmlStore.ts
+++ source/javascripts/hooks/useBitriseYmlStore.ts
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { Document } from 'yaml';
 import { useStore } from 'zustand';
 
 import { BitriseYml } from '@/core/models/BitriseYml';
@@ -9,11 +10,22 @@ type ExtendedBitriseYmlStoreState = Omit<BitriseYmlStoreState, 'yml'> & {
   yml: BitriseYml;
 };
 
+let cachedYmlJson: BitriseYml | null = null;
+let cachedYmlDocument: Document | null = null;
+
 function useBitriseYmlStore<U = ExtendedBitriseYmlStoreState>(selector?: (state: ExtendedBitriseYmlStoreState) => U) {
   const ymlDocument = useStore(bitriseYmlStore, (state) => state.ymlDocument);
-  const ymlDocumentAsJson = useMemo(() => ymlDocument.toJSON() as BitriseYml, [ymlDocument]);
-  const withBitriseYml = useShallow((state: BitriseYmlStoreState) => ({ ...state, yml: ymlDocumentAsJson }));
 
+  const ymlJson = useMemo(() => {
+    if (ymlDocument !== cachedYmlDocument) {
+      cachedYmlJson = ymlDocument.toJSON() as BitriseYml;
+      cachedYmlDocument = ymlDocument;
+    }
+
+    return cachedYmlJson as BitriseYml;
+  }, [ymlDocument]);
+
+  const withBitriseYml = useShallow((state: BitriseYmlStoreState) => ({ ...state, yml: ymlJson }));
   const finalSelector = selector ? (state: BitriseYmlStoreState) => selector(withBitriseYml(state)) : withBitriseYml;
 
   return useStore(bitriseYmlStore, useShallow(finalSelector as any)) as U;
