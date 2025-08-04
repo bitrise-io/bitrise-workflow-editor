d8bc436e7b1ec8c47a78948551c0276077420435
erosdome <jozsef.eros@bitrise.io>
2025-08-04 12:14:31 +0200

    Update machine deprecation banner

--- source/javascripts/components/StacksAndMachine/DeprecatedMachineNotification.tsx
+++ source/javascripts/components/StacksAndMachine/DeprecatedMachineNotification.tsx
@@ -32,9 +32,9 @@ const DeprecatedMachineNotification = (props: Props) => {
   if (currentDate < startDate) {
     return (
       <Notification {...notificationProps}>
-        Upgrading to newer generation Apple Silicon machines, with no extra costs. M1 Large, M1 Max Medium and M1 Max
-        Large machines will be deprecated and your Workflows will automatically run on newer generation Apple Silicon
-        machines from {startDate.toLocaleDateString()}.{' '}
+        Upgrading to newer generation Apple Silicon machines, with no extra costs. M1 Medium and M1 Large machines will
+        be deprecated and your Workflows will automatically run on newer generation Apple Silicon machines from{' '}
+        {startDate.toLocaleDateString()}.{' '}
         <Link
           href="https://docs.google.com/document/d/1DjttMcPHn4c76bVG6_jDQ4RfG08oLTMJ8rusHUA-0gE"
           isExternal
@@ -49,8 +49,8 @@ const DeprecatedMachineNotification = (props: Props) => {
     return (
       <Notification {...notificationProps}>
         Your Workflows were automatically switched to newer generation Apple Silicon machines, with no extra costs. To
-        continue using the deprecated M1 Large, M1 Max Medium and M1 Max Large machines until{' '}
-        {endDate.toLocaleDateString()}, opt to extend the transition period in the {startDate.toLocaleDateString()}.{' '}
+        continue using the deprecated M1 Medium and M1 Large machines until {endDate.toLocaleDateString()}, opt to
+        extend the transition period in the {startDate.toLocaleDateString()}.{' '}
         <Link href={`/workspaces/${GlobalProps.workspaceSlug()}/settings/apps`} isExternal isUnderlined>
           Workspace settings
         </Link>
@@ -60,8 +60,8 @@ const DeprecatedMachineNotification = (props: Props) => {
   if (currentDate > endDate) {
     return (
       <Notification {...notificationProps}>
-        Your Workflows are now running on newer generation Apple Silicon machines, with no extra costs. If you’d like to
-        keep using the deprecated M1 Large, M1 Max Medium and M1 Max Large machines, reach out to support.{' '}
+        Your Workflows are now running on newer generation Apple Silicon machines, with no extra costs. If you'd like to
+        keep using the deprecated M1 Medium and M1 Large machines, reach out to support.{' '}
         <Link
           href="https://docs.google.com/document/d/1DjttMcPHn4c76bVG6_jDQ4RfG08oLTMJ8rusHUA-0gE"
           isExternal
