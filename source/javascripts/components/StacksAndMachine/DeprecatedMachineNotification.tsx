43f610f308c39edbb69040011ce59abcca788faf
erosdome <jozsef.eros@bitrise.io>
2025-09-04 16:49:22 +0200

    Update deprecation wave texts to Linux (#1637)

--- source/javascripts/components/StacksAndMachine/DeprecatedMachineNotification.tsx
+++ source/javascripts/components/StacksAndMachine/DeprecatedMachineNotification.tsx
@@ -32,9 +32,9 @@ const DeprecatedMachineNotification = (props: Props) => {
   if (currentDate < startDate) {
     return (
       <Notification {...notificationProps}>
-        Upgrading to newer generation Apple Silicon machines, with no extra costs. M1 Medium and M1 Large machines will
-        be deprecated and your Workflows will automatically run on newer generation Apple Silicon machines from{' '}
-        {startDate.toLocaleDateString()}.{' '}
+        Upgrading to newer generation AMD EPYC Zen4 Linux machines, with no extra costs. Standard and Elite Linux
+        machines will be deprecated and your Workflows will automatically run on newer generation AMD EPYC Zen4 Linux
+        machines from {startDate.toLocaleDateString()}.{' '}
         <Link
           href="https://docs.google.com/document/d/1DjttMcPHn4c76bVG6_jDQ4RfG08oLTMJ8rusHUA-0gE"
           isExternal
@@ -48,8 +48,8 @@ const DeprecatedMachineNotification = (props: Props) => {
   if (currentDate >= startDate && currentDate <= endDate) {
     return (
       <Notification {...notificationProps}>
-        Your Workflows were automatically switched to newer generation Apple Silicon machines, with no extra costs. To
-        continue using the deprecated M1 Medium and M1 Large machines until {endDate.toLocaleDateString()}, opt to
+        Your Workflows were automatically switched to newer generation AMD EPYC Zen4 Linux machines, with no extra
+        costs. To continue using the deprecated Standard and Elite machines until {endDate.toLocaleDateString()}, opt to
         extend the transition period in the {startDate.toLocaleDateString()}.{' '}
         <Link href={`/workspaces/${GlobalProps.workspaceSlug()}/settings/apps`} isExternal isUnderlined>
           Workspace settings
@@ -60,8 +60,8 @@ const DeprecatedMachineNotification = (props: Props) => {
   if (currentDate > endDate) {
     return (
       <Notification {...notificationProps}>
-        Your Workflows are now running on newer generation Apple Silicon machines, with no extra costs. If you'd like to
-        keep using the deprecated M1 Medium and M1 Large machines, reach out to support.{' '}
+        Your Workflows are now running on newer generation AMD EPYC Zen4 Linux machines, with no extra costs. If you'd
+        like to keep using the deprecated Standard and Elite Linux machines, reach out to support.{' '}
         <Link
           href="https://docs.google.com/document/d/1DjttMcPHn4c76bVG6_jDQ4RfG08oLTMJ8rusHUA-0gE"
           isExternal
