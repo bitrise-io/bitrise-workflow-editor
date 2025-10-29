5c4e69edec15d9986fa69b2b73b7119c609843a8
Laszlo Moczo <laszlo.moczo@bitrise.io>
2025-10-29 13:25:03 +0100

    Show step bundle title [BIVS-3405] (#1648)

--- source/javascripts/components/unified-editor/StepBundleConfig/StepBundleConfig.context.tsx
+++ source/javascripts/components/unified-editor/StepBundleConfig/StepBundleConfig.context.tsx
@@ -1,11 +1,7 @@
-import { toMerged } from 'es-toolkit';
 import { createContext, PropsWithChildren, useContext } from 'react';
 
 import { StepBundleInstance } from '@/core/models/Step';
-import StepBundleService from '@/core/services/StepBundleService';
-import StepService from '@/core/services/StepService';
-import useBitriseYmlStore from '@/hooks/useBitriseYmlStore';
-import useDefaultStepLibrary from '@/hooks/useDefaultStepLibrary';
+import useStepBundle from '@/hooks/useStepBundle';
 
 import StepConfigDrawerProvider from '../StepConfigDrawer/StepConfigDrawer.context';
 
@@ -39,55 +35,9 @@ type UseStepBundleConfigContextResult = Omit<Props, 'children'> & {
 export function useStepBundleConfigContext<U = UseStepBundleConfigContextResult>(
   selector?: (state: UseStepBundleConfigContextResult) => U,
 ): U {
-  const defaultStepLibrary = useDefaultStepLibrary();
-  const { stepBundleId, parentWorkflowId, parentStepBundleId, stepIndex } = useContext(Context);
-
-  return useBitriseYmlStore(({ yml }) => {
-    const result: UseStepBundleConfigContextResult = { stepBundleId, parentWorkflowId, parentStepBundleId, stepIndex };
-
-    if (stepBundleId) {
-      const stepBundle = yml.step_bundles?.[stepBundleId];
-      result.stepBundle = stepBundle ? StepBundleService.ymlInstanceToStepBundle(stepBundleId, stepBundle) : undefined;
-    }
-
-    if (!stepBundleId && parentWorkflowId && stepIndex >= 0) {
-      const stepListItemModel = yml.workflows?.[parentWorkflowId]?.steps?.[stepIndex];
-      const [cvs, stepBundleInWorkflow] = Object.entries(stepListItemModel ?? {})[0] ?? ['', {}];
-
-      const id = StepBundleService.cvsToId(cvs);
-      const stepBundle = toMerged(yml.step_bundles?.[id] ?? {}, stepBundleInWorkflow ?? {});
-
-      result.stepBundle = StepService.isStepBundle(cvs, defaultStepLibrary, stepBundle)
-        ? StepBundleService.ymlInstanceToStepBundle(
-            id,
-            stepBundle,
-            yml.step_bundles?.[id] ?? undefined,
-            stepBundleInWorkflow ?? undefined,
-          )
-        : undefined;
-      result.stepBundleId = id;
-    }
-
-    if (!stepBundleId && !parentWorkflowId && parentStepBundleId && stepIndex >= 0) {
-      const stepListItemModel = yml.step_bundles?.[parentStepBundleId]?.steps?.[stepIndex];
-      const [cvs, stepBundleInStepBundle] = Object.entries(stepListItemModel ?? {})[0] ?? ['', {}];
-
-      const id = StepBundleService.cvsToId(cvs);
-      const stepBundle = toMerged(yml.step_bundles?.[id] ?? {}, stepBundleInStepBundle ?? {});
-
-      result.stepBundle = StepService.isStepBundle(cvs, defaultStepLibrary, stepBundle)
-        ? StepBundleService.ymlInstanceToStepBundle(
-            id,
-            stepBundle,
-            yml.step_bundles?.[id] || undefined,
-            stepBundleInStepBundle || undefined,
-          )
-        : undefined;
-      result.stepBundleId = id;
-    }
-
-    return selector ? selector(result) : result;
-  }) as U;
+  const context = useContext(Context);
+  const result = useStepBundle(context);
+  return selector ? selector(result as UseStepBundleConfigContextResult) : (result as U);
 }
 
 export default StepBundleConfigProvider;
