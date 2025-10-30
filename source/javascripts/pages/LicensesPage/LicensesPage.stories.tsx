e396d57ada46517425c24d229c70a501c859567a
Andras Eszes <andras.eszes@bitrise.io>
2025-10-30 09:56:40 +0100

    fix: eslint errors

--- source/javascripts/pages/LicensesPage/LicensesPage.stories.tsx
+++ source/javascripts/pages/LicensesPage/LicensesPage.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta } from '@storybook/react-webpack5';
+import { Meta } from '@storybook/react-vite';
 
 import LicensePoolsApiMswMocks from '@/core/api/LicensePoolsApi.mswMocks';
 
--- source/javascripts/pages/StepBundlesPage/StepBundlesPage.stories.tsx
+++ source/javascripts/pages/StepBundlesPage/StepBundlesPage.stories.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@bitrise/bitkit';
-import { Meta, StoryObj } from '@storybook/react-webpack5';
+import { Meta, StoryObj } from '@storybook/react-vite';
 import { set } from 'es-toolkit/compat';
 
 import StepBundlesPage from './StepBundlesPage';
