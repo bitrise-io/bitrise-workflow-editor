65b264c7abd01c4d966d62d5829d27761e32f31f
Antal Orcsik <aorcsik@gmail.com>
2025-11-05 15:28:30 +0100

    clean up worker script

--- source/javascripts/proxy-worker.js
+++ source/javascripts/proxy-worker.js
@@ -1,12 +1,12 @@
 export default {
   async fetch(request, env, ctx) {
     const urlObject = new URL(request.url);
-
-    const rssMatch = urlObject.pathname.match(/^\/workflow_editor\/(.*)$/);
-    if (rssMatch) {
+    if (urlObject.pathname.match(/^\/workflow_editor\/.*$/)) {
       urlObject.hostname = 'workflow-editor-cdn.bitrise.io';
-      urlObject.pathname = `/workflow_editor/${rssMatch[1]}`;
-      return await fetch(urlObject);
+      return fetch(urlObject);
     }
+
+    // Return 404 for non-matching requests
+    return new Response('Not Found', { status: 404 });
   }
 };
