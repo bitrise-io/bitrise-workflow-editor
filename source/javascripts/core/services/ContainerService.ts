becd30c970d5b2bb18f0f2975395376270acb324
Luca Kollar <luca.gelencser-kollar@bitrise.io>
2026-01-14 14:31:13 +0100

    Added functions to Container Service

--- source/javascripts/core/services/ContainerService.ts
+++ source/javascripts/core/services/ContainerService.ts
@@ -3,7 +3,7 @@ import { Document } from 'yaml';
 import { updateBitriseYmlDocument } from '@/core/stores/BitriseYmlStore';
 import YmlUtils from '@/core/utils/YmlUtils';
 
-function getContainerOrThrowError(id: string, doc: Document) {
+function getExecutionContainerOrThrowError(id: string, doc: Document) {
   const container = YmlUtils.getMapIn(doc, ['containers', id]);
 
   if (!container) {
@@ -13,18 +13,119 @@ function getContainerOrThrowError(id: string, doc: Document) {
   return container;
 }
 
-function createExecutionContainer(id: string, image: string) {
+function getServiceContainerOrThrowError(id: string, doc: Document) {
+  const service = YmlUtils.getMapIn(doc, ['services', id]);
+
+  if (!service) {
+    throw new Error(`Service ${id} not found. Ensure that the service exists in the 'services' section.`);
+  }
+
+  return service;
+}
+
+function createExecutionContainer(
+  id: string,
+  image: string,
+  credentials?: { username?: string; password?: string; server?: string },
+  ports?: string[],
+  envs?: Record<string, string>[],
+  options?: string,
+) {
   updateBitriseYmlDocument(({ doc }) => {
     if (doc.hasIn(['containers', id])) {
       throw new Error(`Container '${id}' already exists`);
     }
 
-    YmlUtils.setIn(doc, ['containers', id], { image });
+    const container: Record<string, unknown> = { image };
+
+    if (credentials) {
+      const filteredCredentials = Object.entries(credentials)
+        .filter(([_, value]) => value !== undefined && value !== '')
+        .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+
+      if (Object.keys(filteredCredentials).length > 0) {
+        container.credentials = filteredCredentials;
+      }
+    }
+
+    if (ports && ports.length > 0) {
+      container.ports = ports;
+    }
+
+    if (envs && envs.length > 0) {
+      container.envs = envs;
+    }
+
+    if (options) {
+      container.options = options;
+    }
+
+    YmlUtils.setIn(doc, ['containers', id], container);
+    return doc;
+  });
+}
+
+function createServiceContainer(
+  id: string,
+  image: string,
+  ports: string[],
+  credentials?: { username?: string; password?: string; server?: string },
+  envs?: Record<string, string>[],
+  options?: string,
+) {
+  updateBitriseYmlDocument(({ doc }) => {
+    if (doc.hasIn(['services', id])) {
+      throw new Error(`Service '${id}' already exists`);
+    }
+
+    const service: Record<string, unknown> = { image, ports };
+
+    if (credentials) {
+      const filteredCredentials = Object.entries(credentials)
+        .filter(([_, value]) => value !== undefined && value !== '')
+        .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+
+      if (Object.keys(filteredCredentials).length > 0) {
+        service.credentials = filteredCredentials;
+      }
+    }
+
+    if (envs && envs.length > 0) {
+      service.envs = envs;
+    }
+
+    if (options) {
+      service.options = options;
+    }
+
+    YmlUtils.setIn(doc, ['services', id], service);
+    return doc;
+  });
+}
+
+function deleteExecutionContainer(id: string) {
+  updateBitriseYmlDocument(({ doc }) => {
+    getExecutionContainerOrThrowError(id, doc);
+    YmlUtils.deleteByPath(doc, ['containers', id]);
+
+    return doc;
+  });
+}
+
+function deleteServiceContainer(id: string) {
+  updateBitriseYmlDocument(({ doc }) => {
+    getServiceContainerOrThrowError(id, doc);
+    YmlUtils.deleteByPath(doc, ['services', id]);
+
     return doc;
   });
 }
 
 export default {
   createExecutionContainer,
-  getContainerOrThrowError,
+  createServiceContainer,
+  deleteExecutionContainer,
+  deleteServiceContainer,
+  getExecutionContainerOrThrowError,
+  getServiceContainerOrThrowError,
 };
