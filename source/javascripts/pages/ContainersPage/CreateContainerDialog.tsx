bbea85dedaf797c304add55bcbb9f490691d6d00
Luca Kollar <luca.gelencser-kollar@bitrise.io>
2026-01-28 13:49:37 +0100

    Added CreateContainerDialog

---
+++ source/javascripts/pages/ContainersPage/CreateContainerDialog.tsx
@@ -0,0 +1,116 @@
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Checkbox,
+  Collapse,
+  ControlButton,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  DialogProps,
+  Divider,
+  Icon,
+  Input,
+  Link,
+  Text,
+  Textarea,
+  Tooltip,
+  useDisclosure,
+} from '@bitrise/bitkit';
+
+import { EnvVarPopover } from '@/components/VariablePopover';
+
+const CreateContainerDialog = (props: Omit<DialogProps, 'title'>) => {
+  const { isOpen, onClose } = props;
+  const { isOpen: isShowMore, onToggle } = useDisclosure();
+
+  return (
+    <Dialog title="Create execution container" isOpen={isOpen} onClose={onClose}>
+      <DialogBody display="flex" flexDir="column" gap="16">
+        <Input
+          label="Unique ID"
+          helperText="The unique ID is for referencing in YAML. Allowed characters: A-Za-z0-9-_."
+          placeholder="e.g. node, postgres, redis"
+          isRequired
+        />
+        <Input
+          label="Image"
+          helperText="For Docker Hub use the format of [name]:[version], for other registries use [registry server]/[owner]/[name]:[version]."
+          placeholder="e.g. node:18-alpine, ghcr.io/your-github-user/your-private-image:v1.1"
+          isRequired
+        />
+        <Input
+          label="Ports"
+          helperText="List of port mappings in the format of [HostPort01]:[ContainerPort01]. Separate multiple with commas."
+          placeholder="e.g. 3000:3000, 5432:5432"
+        />
+        <Collapse in={isShowMore} style={{ overflow: 'visible' }}>
+          <Box display="flex" flexDir="column" gap="16">
+            <Text textStyle="heading/h3" mt="8">
+              Authentication credentials
+            </Text>
+            <Text textStyle="body/md/regular" color="text/secondary">
+              Set to access your private images with the docker login command.
+            </Text>
+            <Input
+              label="Registry server"
+              helperText="Fully qualified registry server url to used by docker login command."
+              placeholder="e.g. ghcr.io"
+            />
+            <Box display="flex" alignItems="flex-end" gap="8">
+              <Input label="Username" width="100%" />
+              <EnvVarPopover size="lg" onSelect={() => {}} />
+            </Box>
+            <Box display="flex" alignItems="flex-end" gap="8">
+              <Input label="Password" type="password" width="100%" />
+              <EnvVarPopover size="lg" onSelect={() => {}} />
+            </Box>
+            <Divider />
+            <Box display="flex" alignItems="flex-end" gap="10">
+              <Input label="Environment Variables" placeholder="Key" width="100%" />
+              <Input placeholder="Value" width="100%" />
+              <ButtonGroup>
+                <EnvVarPopover size="lg" onSelect={() => {}} />
+                <ControlButton iconName="Trash" size="lg" aria-label="Remove environment variable" />
+              </ButtonGroup>
+            </Box>
+            <Checkbox>
+              Replace variables in input
+              <Tooltip label="Enable this if you want to replace Environment Variables in your input with the variable’s assigned value.">
+                <Icon name="InfoCircle" color="icon/tertiary" size="16" ml="4" />
+              </Tooltip>
+            </Checkbox>
+            <Button variant="tertiary" leftIconName="Plus" size="md" maxW="131">
+              Add Env Var
+            </Button>
+            <Divider />
+            <Textarea
+              label="Docker create options"
+              helperText={
+                <>
+                  Additional parameters passed to docker create command.{' '}
+                  <Link colorScheme="purple" href="#" isExternal>
+                    Learn more about not supported options
+                  </Link>
+                </>
+              }
+              placeholder='e.g. --privileged --health-cmd "redis-cli ping" --health-interval 1s --health-timeout 3s --health-retries 2'
+            />
+          </Box>
+        </Collapse>
+        <Link colorScheme="purple" cursor="pointer" size="2" onClick={onToggle}>
+          {isShowMore ? 'Show less options' : 'Show more options'}
+        </Link>
+      </DialogBody>
+      <DialogFooter>
+        <Button variant="secondary" onClick={onClose}>
+          Cancel
+        </Button>
+        <Button>Create container</Button>
+      </DialogFooter>
+    </Dialog>
+  );
+};
+
+export default CreateContainerDialog;
