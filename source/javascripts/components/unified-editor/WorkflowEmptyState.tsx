dadff7d2ec5eb944d5ee14bde2777daf0eb4b5bf
Luca Kollar <luca.gelencser-kollar@bitrise.io>
2026-03-23 14:24:31 +0100

    Added feature flag

--- source/javascripts/components/unified-editor/WorkflowEmptyState.tsx
+++ source/javascripts/components/unified-editor/WorkflowEmptyState.tsx
@@ -1,34 +1,40 @@
 import { Button, ButtonGroup, EmptyState } from '@bitrise/bitkit';
 
+import useFeatureFlag from '@/hooks/useFeatureFlag';
+
 type Props = {
   isAIButtonDisabled?: boolean;
   onCreateWorkflow: () => void;
   onCreateWorkflowWithAI?: () => void;
 };
 
-const WorkflowEmptyState = ({ isAIButtonDisabled, onCreateWorkflow, onCreateWorkflowWithAI }: Props) => (
-  <EmptyState
-    iconName="Workflow"
-    title="Your Workflow will appear here"
-    description="It looks like you haven't set up any Workflows. Create your first Workflow to automate your CI/CD pipeline."
-  >
-    <ButtonGroup spacing="0" gap="24">
-      {onCreateWorkflowWithAI && (
-        <Button
-          isDisabled={isAIButtonDisabled}
-          leftIconName="SparkleFilled"
-          size="md"
-          variant="ai-primary"
-          onClick={onCreateWorkflowWithAI}
-        >
-          Create Workflow with AI
+const WorkflowEmptyState = ({ isAIButtonDisabled, onCreateWorkflow, onCreateWorkflowWithAI }: Props) => {
+  const enableCiConfigExpertAgent = useFeatureFlag('enable-ci-config-expert-agent');
+
+  return (
+    <EmptyState
+      iconName="Workflow"
+      title="Your Workflow will appear here"
+      description="It looks like you haven't set up any Workflows. Create your first Workflow to automate your CI/CD pipeline."
+    >
+      <ButtonGroup spacing="0" gap="24">
+        {enableCiConfigExpertAgent && onCreateWorkflowWithAI && (
+          <Button
+            isDisabled={isAIButtonDisabled}
+            leftIconName="SparkleFilled"
+            size="md"
+            variant="ai-primary"
+            onClick={onCreateWorkflowWithAI}
+          >
+            Create Workflow with AI
+          </Button>
+        )}
+        <Button leftIconName="Plus" size="md" onClick={onCreateWorkflow} variant="secondary">
+          Create Workflow manually
         </Button>
-      )}
-      <Button leftIconName="Plus" size="md" onClick={onCreateWorkflow} variant="secondary">
-        Create Workflow manually
-      </Button>
-    </ButtonGroup>
-  </EmptyState>
-);
+      </ButtonGroup>
+    </EmptyState>
+  );
+};
 
 export default WorkflowEmptyState;
