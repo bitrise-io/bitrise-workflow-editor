beb6e145534af0e9f8daff418fbda1d760f0206d
Marcell Vida <marcell.vida@bitrise.io>
2026-05-11 17:25:44 +0200

    *

--- source/javascripts/hooks/useTools.ts
+++ source/javascripts/hooks/useTools.ts
@@ -1,17 +1,14 @@
 import { Tools } from '@/core/models/BitriseYml';
 import useBitriseYmlStore from '@/hooks/useBitriseYmlStore';
-import { useShallow } from '@/hooks/useShallow';
 
 export function useTools(): Tools {
-  return useBitriseYmlStore(useShallow(({ yml }) => yml.tools ?? {}));
+  return useBitriseYmlStore(({ yml }) => yml.tools ?? {});
 }
 
 export function useWorkflowTools(workflowId: string): Tools | undefined {
-  return useBitriseYmlStore(
-    useShallow(({ yml }) => {
-      const workflow = yml.workflows?.[workflowId];
-      if (!workflow) return undefined;
-      return workflow.tools ?? {};
-    }),
-  );
+  return useBitriseYmlStore(({ yml }) => {
+    const workflow = yml.workflows?.[workflowId];
+    if (!workflow) return undefined;
+    return workflow.tools ?? {};
+  });
 }
