9ee21f13a34209fb672f36ebd863a8bed5c1b7c8
Marcell Vida <marcell.vida@bitrise.io>
2026-08-26 10:27:15 +0200

    feat: search for prefixes as well

--- source/javascripts/components/DetailedHelperText.tsx
+++ source/javascripts/components/DetailedHelperText.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import { Box, Collapse, Link, MarkdownContent, useDisclosure } from '@bitrise/bitkit';
 
 type Props = {
--- source/javascripts/hooks/useYmlHasChanges.ts
+++ source/javascripts/hooks/useYmlHasChanges.ts
@@ -1,5 +1,3 @@
-/* eslint-disable no-underscore-dangle */
-
 import useBitriseYmlStore from './useBitriseYmlStore';
 
 export default function useYmlHasChanges() {
